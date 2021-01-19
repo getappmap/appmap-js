b955d20cc9c0ac0a057b10cd104228e8628fa0f1
Dustin Byrne <dustinsbyrne@gmail.com>
2021-01-19 14:42:19 -0500

    Add prettier with pre-commit hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': ['error'],
   },
   overrides: [
     {
