533c33b34ca59d792ff66b2e31652d3f6c081016
Dustin Byrne <dustinsbyrne@gmail.com>
2021-01-25 14:08:02 -0500

    Add tests

---
+++ tests/e2e/specs/flowView.js
@@ -0,0 +1,18 @@
+context('Flow View', () => {
+  beforeEach(() => {
+    cy.visit(
+      'http://localhost:6006/iframe.html?id=appland-diagrams--diagram-flow&viewMode=story',
+    );
+  });
+
+  it('renders', () => {
+    cy.get('.appmap__flow-view .node').should('have.length', 248);
+  });
+
+  it('displays poppers', () => {
+    cy.get('.column.left .item.has-data').first().click();
+    cy.get('.appmap__flow-view-popper[data-placement="left"]').should(
+      'be.visible',
+    );
+  });
+});
