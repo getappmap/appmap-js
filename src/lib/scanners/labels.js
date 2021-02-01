0a589e7585273182eb61479d231c3a57d05e3b98
Kevin Gilpin <kgilpin@gmail.com>
2021-02-01 16:13:02 -0500

    Add AppLand appmaps for test purposes

--- src/lib/scanners/labels.js
+++ src/lib/scanners/labels.js
@@ -1,6 +1,6 @@
 export const PUBLIC = 'public';
-export const MVC_MODEL = 'mvc.model';
-export const MVC_VIEW = 'mvc.view';
-export const MVC_CONTROLLER = 'mvc.controller';
+export const MVC_MODEL = 'model';
+export const MVC_VIEW = 'view';
+export const MVC_CONTROLLER = 'controller';
 export const PROVIDER_AUTHENTICATION = 'provider.authentication';
 export const PROVIDER_AUTHORIZATION = 'provider.authorization';
