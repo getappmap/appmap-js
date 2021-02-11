23ba1e7dbba947cb42f6a54caefa78950203516e
Dustin Byrne <dustinsbyrne@gmail.com>
2021-02-11 15:15:10 -0500

    chore: automatically update CHANGELOG.md [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+    '@semantic-release/github',
+    '@semantic-release/npm',
+  ],
 };
