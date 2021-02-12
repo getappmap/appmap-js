5a7710f1e16ed2e316b83c9dcbe9fd6a9ee42ed1
Dustin Byrne <dustinsbyrne@gmail.com>
2021-02-11 19:27:51 -0500

    chore: re-order release plugins [skip ci]

--- release.config.js
+++ release.config.js
@@ -4,8 +4,8 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/github',
-    '@semantic-release/npm',
   ],
 };
