8b9951eef2ebd1868c4d20c3efb2da6cd453d261
Sasha H <sashahdev23@gmail.com>
2021-02-15 22:56:12 +0200

    feat: use Container helper as Vue component

---
+++ src/components/Container.vue
@@ -0,0 +1,37 @@
+<template>
+  <div ref="el">
+    <slot />
+  </div>
+</template>
+
+<script>
+import Container from '@/lib/diagrams/helpers/container';
+
+export default {
+  name: 'v-container',
+
+  props: {
+    options: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  methods: {
+    initContainer() {
+      const childrenNodes = this.$children.map(($child) => $child.$el);
+      const container = new Container(this.$refs.el, this.options);
+
+      childrenNodes.forEach((child) => container.appendChild(child));
+    },
+  },
+
+  mounted() {
+    this.initContainer();
+  },
+
+  updated() {
+    this.initContainer();
+  },
+};
+</script>
