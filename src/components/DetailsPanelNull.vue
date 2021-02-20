6465af0d6e3a4bc52a88e480321012d8aadaca74
Sasha H <sashahdev23@gmail.com>
2021-02-20 21:35:34 +0200

    feat: show instructions for active tab

--- src/components/DetailsPanelNull.vue
+++ src/components/DetailsPanelNull.vue
@@ -2,51 +2,101 @@
   <div>
     <div id="intro-details" class="welcome">
       <div class="content">
-        <div class="intro">
-          <p>
-            Select a component in the diagram to see its details here. Here,
-            we’ll show you any relevant information about your code as you
-            navigate through your appmaps.
-          </p>
+        <div v-if="currentView === VIEW_COMPONENT">
+          <div class="intro">
+            <p>
+              Select a component in the diagram to see its details here. Here,
+              we’ll show you any relevant information about your code as you
+              navigate through your appmaps.
+            </p>
+          </div>
+          <div class="components_events">
+            <h3>Dependency Map</h3>
+            <p>
+              The UML-inspired diagrams display your key application components,
+              and how they are inter-related during application execution.
+            </p>
+          </div>
+          <div class="right_click">
+            <h3>Right-click</h3>
+            <p>Expand, collapse, and reset the diagram elements.</p>
+          </div>
+          <div class="filters">
+            <h3>Filters</h3>
+            <p>Bring focus to a narrow area of interest.</p>
+          </div>
+          <div class="legend">
+            <ul>
+              <li>
+                <div class="icon class-pkg"></div>
+                <p>Class packages. Right-click to expand them to classes.</p>
+              </li>
+              <li>
+                <div class="icon class"></div>
+                <p>Class. Right-click to see source (Ruby only)</p>
+              </li>
+              <li>
+                <div class="icon dynamic"></div>
+                <p>Dynamic dependencies</p>
+              </li>
+              <li>
+                <div class="icon web-service"></div>
+                <p>Web services/endpoints. Right-click to expand.</p>
+              </li>
+              <li>
+                <div class="icon database"></div>
+                <p>Databases that persist application data</p>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="components_events">
-          <h3>Dependency Map & Trace</h3>
-          <p>
-            The UML-inspired diagrams display your key application components,
-            and how they are inter-related during application execution.
-          </p>
-        </div>
-        <div class="right_click">
-          <h3>Right-click</h3>
-          <p>Expand, collapse, and reset the diagram elements.</p>
-        </div>
-        <div class="filters">
-          <h3>Filters</h3>
-          <p>Bring focus to a narrow area of interest.</p>
-        </div>
-        <div class="legend">
-          <ul>
-            <li>
-              <div class="icon class-pkg"></div>
-              <p>Class packages. Right-click to expand them to classes.</p>
-            </li>
-            <li>
-              <div class="icon class"></div>
-              <p>Class. Right-click to see source (Ruby only)</p>
-            </li>
-            <li>
-              <div class="icon dynamic"></div>
-              <p>Dynamic dependencies</p>
-            </li>
-            <li>
-              <div class="icon web-service"></div>
-              <p>Web services/endpoints. Right-click to expand.</p>
-            </li>
-            <li>
-              <div class="icon database"></div>
-              <p>Databases that persist application data</p>
-            </li>
-          </ul>
+        <div v-if="currentView === VIEW_FLOW">
+          <div class="intro">
+            <p>
+              Select a component in the diagram to see its details here. Here,
+              we’ll show you any relevant information about your code as you
+              navigate through your appmaps.
+            </p>
+          </div>
+          <div class="components_events">
+            <h3>Trace</h3>
+            <p>
+              The UML-inspired diagrams display your key application components,
+              and how they are inter-related during application execution.
+            </p>
+          </div>
+          <div class="right_click">
+            <h3>Right-click</h3>
+            <p>Expand, collapse, and reset the diagram elements.</p>
+          </div>
+          <div class="filters">
+            <h3>Filters</h3>
+            <p>Bring focus to a narrow area of interest.</p>
+          </div>
+          <div class="legend">
+            <ul>
+              <li>
+                <div class="icon class-pkg"></div>
+                <p>Class packages. Right-click to expand them to classes.</p>
+              </li>
+              <li>
+                <div class="icon class"></div>
+                <p>Class. Right-click to see source (Ruby only)</p>
+              </li>
+              <li>
+                <div class="icon dynamic"></div>
+                <p>Dynamic dependencies</p>
+              </li>
+              <li>
+                <div class="icon web-service"></div>
+                <p>Web services/endpoints. Right-click to expand.</p>
+              </li>
+              <li>
+                <div class="icon database"></div>
+                <p>Databases that persist application data</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -54,8 +104,26 @@
 </template>
 
 <script>
+import { VIEW_COMPONENT, VIEW_FLOW } from '../store/vsCode';
+
 export default {
   name: 'v-details-panel-null',
+
+  data() {
+    return {
+      VIEW_COMPONENT,
+      VIEW_FLOW,
+    };
+  },
+
+  computed: {
+    currentView() {
+      if (this.$store) {
+        return this.$store.state.currentView;
+      }
+      return null;
+    },
+  },
 };
 </script>
 
