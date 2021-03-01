8d17c77c2184468027195355752a719c3a679187
Kevin Gilpin <kgilpin@gmail.com>
2021-03-01 10:18:23 -0500

    Fingerprint only the HTTP server requests, if they are present

--- src/lib/fingerprint/canonicalize.js
+++ src/lib/fingerprint/canonicalize.js
@@ -1,12 +1,42 @@
 import betaV1Error from './canonicalize/beta_v1_error';
 import betaV1Info from './canonicalize/beta_v1_info';
 import betaV1Debug from './canonicalize/beta_v1_debug';
+import { AppMap } from '../../../dist/appmap.node';
 
 export const algorithms = {
   beta_v1_error: betaV1Error,
   beta_v1_info: betaV1Info,
   beta_v1_debug: betaV1Debug,
 };
+
+function httpServerRequestsOnly(appmap) {
+  const httpServerRequestStack = [];
+  const selectedEvents = [];
+  appmap.events.forEach((event) => {
+    if (event.isCall() && event.httpServerRequest) {
+      httpServerRequestStack.push(event.httpServerRequest);
+    }
+
+    if (httpServerRequestStack.length > 0) {
+      selectedEvents.push(event);
+    }
+
+    if (event.isReturn() && event.httpServerResponse) {
+      httpServerRequestStack.pop();
+    }
+  });
+
+  if (selectedEvents.length === 0) {
+    console.warn(
+      `No http server requests found in appmap ${appmap.data?.metadata?.name}`
+    );
+    return appmap;
+  }
+
+  const data = { ...appmap.data };
+  data.events = selectedEvents;
+  return new AppMap(data);
+}
 
 /**
  * Process an appmap into a canonical form which can be fingerprinted
@@ -27,5 +57,5 @@ export function canonicalize(algorithmName, appmap) {
   // events that lie outside those requests (such as test fixture activity) are not shown
   // at all. If we want to treat the appmap that way for canonicalization purposes, this
   // is the place to do it.
-  return algorithm(appmap);
+  return algorithm(httpServerRequestsOnly(appmap));
 }
