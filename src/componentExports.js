c739a3cace43face693316c9947068aedbe70656
Dustin Byrne <dustinsbyrne@gmail.com>
2021-03-08 12:20:21 -0500

    export the Diff component

--- src/componentExports.js
+++ src/componentExports.js
@@ -2,3 +2,4 @@ export { default as VButton } from '@/components/Button.vue';
 export { default as VDetailsPanel } from '@/components/DetailsPanel.vue';
 export { default as VTabs } from '@/components/Tabs.vue';
 export { default as VVsCodeExtension } from '@/pages/VsCodeExtension.vue';
+export { default as VDiff } from '@/pages/Diff.vue';
