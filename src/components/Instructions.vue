eed1ff0f1859d916a2d65749abacd56ead84a9a5
Ty Paulhus <typaulhus@gmail.com>
2021-03-17 11:34:36 -0400

    combined maps

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -6,29 +6,27 @@
     <section class="instructions__container" v-if="isOpen">
       <div class="instructions__head">
         <h1 class="instructions__title">Welcome to your AppMap!</h1>
+
         <button class="instructions__close" @click="close">
           <CloseIcon />
         </button>
       </div>
-      <div class="instructions__content">
-        <div class="welcome">
-          <p>
-            UML-inspired diagrams display your key application components, and
-            how they are inter-related during application execution.
-          </p>
-          <p>
-            Select a node to see its details in the sidebar. Here, we’ll show
-            you any relevant information about your code as you navigate through
-            your appmaps.
-          </p>
-          <ul>
-            <li>
-              Right-click nodes to expand, collapse & reset the diagram elements
-            </li>
-            <li>Use filters to bring focus to a narrow area of interest</li>
-          </ul>
-          <div class="legend">
-            <h4>Dependency map legend</h4>
+      <div class="instructions__content welcome">
+        <p>
+          UML-inspired diagrams display your key application components, and how
+          they are inter-related during application execution.
+        </p>
+        <ul>
+          <li>Select a node to see its details in the sidebar. .</li>
+          <li>
+            Right-click nodes to expand, collapse & reset the diagram elements
+          </li>
+          <li>Use filters to bring focus to a narrow area of interest</li>
+        </ul>
+        <h3>Map Legend</h3>
+        <section class="legends">
+          <div class="legend dependency-map">
+            <h4>Dependency map</h4>
             <ul>
               <li>
                 <div class="icon class-pkg"></div>
@@ -52,7 +50,17 @@
               </li>
             </ul>
           </div>
-        </div>
+          <div class="legend trace-map">
+            <h4>Trace</h4>
+            <ul>
+              <li class="type-int icon">Integer/Number</li>
+              <li class="type-string icon">String</li>
+              <li class="type-bool icon">Boolean</li>
+              <li class="type-float icon">Float/Decimal</li>
+              <li class="type-object icon">Object/Other</li>
+            </ul>
+          </div>
+        </section>
       </div>
     </section>
   </div>
@@ -131,7 +139,7 @@ export default {
     margin: 0 0.75rem 1rem 0;
     width: max-content;
     height: max-content;
-    max-width: 30vw;
+    //max-width: 30vw;
     max-height: 50vh;
     padding: 1.5rem;
     color: $gray6;
@@ -181,42 +189,119 @@ export default {
     padding-left: 1rem;
     list-style: disc;
   }
-  .legend {
-    ul {
-      margin: 0;
-      padding: 0;
-      list-style-type: none;
-      li {
-        display: flex;
-        flex-direction: row;
-        margin: 0.5rem 0;
-        p {
-          flex: 1;
-          margin: 0;
-        }
-        align-items: flex-start;
+}
+.legends {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  column-gap: 2rem;
+}
+.legend {
+  h4 {
+    margin: 0;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    li {
+      display: flex;
+      flex-direction: row;
+      margin: 0.5rem 0;
+      p {
+        flex: 1;
+        margin: 0;
       }
-      .icon {
-        display: inline-block;
-        height: 1rem;
-        width: 2rem;
-        margin-right: 0.5rem;
-        &.class-pkg {
-          background-color: $teal;
+      align-items: flex-start;
+    }
+  }
+  border: 1px solid $gray2;
+  padding: 1rem;
+  border-radius: $border-radius;
+  &.dependency-map {
+    padding-right: 1.5rem;
+    .icon {
+      display: inline-block;
+      height: 1rem;
+      width: 2rem;
+      margin-right: 0.5rem;
+      &.class-pkg {
+        background-color: $teal;
+      }
+      &.class {
+        background-color: $blue;
+      }
+      &.dynamic {
+        background-image: url("data:image/svg+xml,%3Csvg width='32' height='12' viewBox='0 0 32 12' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M32 5.99999L22 0.226492L22 11.7735L32 5.99999ZM2.62268e-07 7L23 6.99999L23 4.99999L-2.62268e-07 5L2.62268e-07 7Z' fill='%23808B98'/%3E%3C/svg%3E%0A");
+        background-repeat: no-repeat;
+      }
+      &.web-service {
+        background-color: $web-services;
+      }
+      &.database {
+        background-color: $royal;
+      }
+    }
+  }
+
+  &.trace-map {
+    ul {
+      display: flex;
+      flex-direction: column;
+      li {
+        margin: 0.25rem 0;
+      }
+    }
+    .icon {
+      height: unset;
+      width: unset;
+      &.type-int {
+        &:before {
+          content: '●';
+          color: $col-int;
         }
-        &.class {
-          background-color: $blue;
+      }
+      &.type-string:before,
+      &.type-bool:before,
+      &.type-float:before,
+      &.type-object:before,
+      &.type-int:before {
+        font-size: 1rem;
+      }
+      &.type-string {
+        &:before {
+          content: '●';
+          color: $col-string;
         }
-        &.dynamic {
-          background-image: url("data:image/svg+xml,%3Csvg width='32' height='12' viewBox='0 0 32 12' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M32 5.99999L22 0.226492L22 11.7735L32 5.99999ZM2.62268e-07 7L23 6.99999L23 4.99999L-2.62268e-07 5L2.62268e-07 7Z' fill='%23808B98'/%3E%3C/svg%3E%0A");
-          background-repeat: no-repeat;
+      }
+      &.type-bool {
+        &:before {
+          content: '●';
+          color: $col-bool;
         }
-        &.web-service {
-          background-color: $web-services;
+      }
+      &.type-float {
+        &:before {
+          content: '●';
+          color: $col-float;
         }
-        &.database {
-          background-color: $royal;
+      }
+      &.type-object {
+        &:before {
+          content: '●';
+          color: $col-object;
         }
+      }
+    }
+  }
+}
+@media (max-width: 1300px) {
+  section.legends {
+    grid-template-columns: 1fr;
+    .legend {
+      margin-bottom: 1rem;
+      &:last-of-type {
+        margin-bottom: 0;
       }
     }
   }
