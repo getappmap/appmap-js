8358ea8fdf08e4483cef1611f3d2baf260838383
Dustin Byrne <dustinsbyrne@gmail.com>
2021-03-23 18:57:46 -0400

    fixup test

--- tests/unit/components/VsCodeExtension.spec.js
+++ tests/unit/components/VsCodeExtension.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import VsCodeExtension from '@/pages/VsCodeExtension.vue';
 import data from '../fixtures/user_page_scenario.appmap.json';
 
@@ -6,26 +6,18 @@ describe('VsCodeExtension.vue', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(VsCodeExtension, {
-      stubs: {
-        VDiagramComponent: true,
-        VDiagramTrace: true,
-      },
-    });
+    wrapper = shallowMount(VsCodeExtension);
     wrapper.vm.loadData(data);
   });
 
   it('sets the selected object by FQID', () => {
-    // FQID: Header
-    const expected = {
-      'label:json': 'json',
-      'event:44': 'User.find_by_id!',
-      'class:app/models/User': 'User',
-    };
+    wrapper.vm.setSelectedObject('label:json');
+    expect(wrapper.vm.selectedLabel).toMatch('json');
 
-    Object.entries(expected).forEach(([fqid, header]) => {
-      wrapper.vm.setSelectedObject(fqid);
-      expect(wrapper.get('details-label__header-title').text()).toMatch(header);
-    });
+    wrapper.vm.setSelectedObject('event:44');
+    expect(wrapper.vm.selectedObject.toString()).toMatch('User.find_by_id!');
+
+    wrapper.vm.setSelectedObject('class:app/models/User');
+    expect(wrapper.vm.selectedObject.id).toMatch('app/models/User');
   });
 });
