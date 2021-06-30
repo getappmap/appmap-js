0e8fca89070bfe4c41dbbdfbb44c28fc9091446a
Kevin Gilpin <kgilpin@gmail.com>
2021-06-30 12:33:07 -0400

    feat: v1.1.0 AppMap indexing
    Add database_type to CodeObjectType.QUERY and store in metadata.json.
    
    Fix handling of parent assignment in normalization.
    
    sql can contain the analysis (action, tables, columns), and/or the normalized query string.

--- packages/cli/src/fingerprint/canonicalize/base.js
+++ packages/cli/src/fingerprint/canonicalize/base.js
@@ -1,8 +1,19 @@
 const { buildTree, notNull } = require('../algorithms');
 
+const BLACKLISTED_LABELS = new Set([
+  'format.json.generate',
+  'format.yaml.generate',
+  'http.session.read',
+]);
+
 module.exports = class {
   constructor(appmap) {
     this.appmap = appmap;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  whitelistedLabels(labels) {
+    return [...labels].filter((label) => !BLACKLISTED_LABELS.has(label));
   }
 
   execute() {
@@ -10,17 +21,35 @@ module.exports = class {
       .filter((event) => event.isCall())
       .map(this.transform.bind(this))
       .filter(notNull);
+
     return buildTree(events);
   }
 
   transform(event) {
-    if (event.sql) {
-      return this.sql(event);
-    }
-    if (event.httpServerRequest) {
-      return this.httpServerRequest(event);
+    const buildEvent = () => {
+      if (event.sql) {
+        return this.sql(event);
+      }
+      if (event.httpServerRequest) {
+        return this.httpServerRequest(event);
+      }
+      if (event.httpClientRequest) {
+        return this.httpClientRequest(event);
+      }
+
+      return this.functionCall(event);
+    };
+
+    const result = buildEvent();
+    if (!result) {
+      return null;
     }
 
-    return this.functionCall(event);
+    result.id = event.id;
+    if (event.parent) {
+      result.parent_id = event.parent.id;
+    }
+    result.depth = event.depth;
+    return result;
   }
 };
--- packages/cli/src/fingerprint/canonicalize/update_v1.js
+++ packages/cli/src/fingerprint/canonicalize/update_v1.js
@@ -1,10 +1,12 @@
 /* eslint-disable class-methods-use-this */
-const { analyzeQuery } = require('../../database');
+const { analyzeQuery, obfuscate } = require('../../database');
 const Base = require('./base');
 
 /**
- * At ERROR level, the order of events is not important, and the amount of data
- * retained about events is minimal.
+ * At UPDATE level, the order of events is not important, and the amount of data
+ * retained about events is minimal. Function labels are retained. HTTP server
+ * and client request method, route, and status code are retained; but not parameter
+ * names.
  */
 class Canonicalize extends Base {
   /**
@@ -12,59 +14,70 @@ class Canonicalize extends Base {
    * @param {Event} event
    */
   sql(event) {
-    const normalizedSQL = analyzeQuery(event.sql);
-    if (typeof normalizedSQL === 'string') {
+    const analyzedQuery = analyzeQuery(event.sql);
+    if (typeof analyzedQuery === 'string') {
       const sqlLower = event.sqlQuery.toLowerCase();
       if (
         sqlLower.indexOf('insert') !== -1 ||
         sqlLower.indexOf('update') !== -1
       ) {
         return {
-          id: event.id,
-          parent_id: event.parent ? event.parent.id : null,
           kind: 'sql',
-          sql: analyzeQuery(event.sql),
+          sql: {
+            normalized_query: obfuscate(
+              event.sqlQuery,
+              event.sql.database_type
+            ),
+          },
         };
       }
-    } else if (normalizedSQL) {
-      let actions = [];
-      if (normalizedSQL.action) {
-        actions.push(normalizedSQL.action);
-      }
-      if (normalizedSQL.actions) {
-        actions = actions.concat(normalizedSQL.actions);
-      }
+    } else if (analyzedQuery && analyzedQuery.actions) {
       if (
-        ['insert', 'update', 'delete'].filter((x) => actions.includes(x))
-          .length > 0
+        ['insert', 'update', 'delete'].find((x) =>
+          analyzedQuery.actions.includes(x)
+        )
       ) {
-        return normalizedSQL;
+        return {
+          kind: 'sql',
+          sql: {
+            analyzed_query: analyzedQuery,
+          },
+        };
       }
     }
 
     return null;
   }
 
+  httpClientRequest(event) {
+    return {
+      kind: 'http_client_request',
+      route: event.route,
+      status_code: event.httpClientResponse
+        ? event.httpClientResponse.status_code
+        : null,
+    };
+  }
+
   httpServerRequest(event) {
     return {
-      id: event.id,
-      parent_id: event.parent ? event.parent.id : null,
       kind: 'http_server_request',
       route: event.route,
-      status: event.httpServerResponse ? event.httpServerResponse.status : null,
+      status_code: event.httpServerResponse
+        ? event.httpServerResponse.status_code
+        : null,
     };
   }
 
   functionCall(event) {
-    if (event.codeObject.labels.size === 0) {
+    const labels = this.whitelistedLabels(event.codeObject.labels);
+    if (labels.length === 0) {
       return null;
     }
 
     return {
-      id: event.id,
-      parent_id: event.parent ? event.parent.id : null,
       kind: 'function',
-      labels: [...event.codeObject.labels],
+      labels,
     };
   }
 }
