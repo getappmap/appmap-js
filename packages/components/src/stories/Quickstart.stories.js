f806ce28402e213eaaaaa18d669e458a99437f3b
Sasha H <sashahdev23@gmail.com>
2021-07-02 00:25:41 +0300

    feat: switch between quickstart steps

--- packages/components/src/stories/Quickstart.stories.js
+++ packages/components/src/stories/Quickstart.stories.js
@@ -3,6 +3,16 @@ import Quickstart from '@/pages/Quickstart.vue';
 export default {
   title: 'Pages/VS Code',
   component: Quickstart,
+  args: {
+    language: 'ruby',
+    // completedSteps: [1, 2],
+    onAction: () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(true);
+        }, 2000);
+      }),
+  },
 };
 
 export const quickstart = (args, { argTypes }) => ({
