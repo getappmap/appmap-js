a2bbd78216547724d8781a21356e7662e6e2f90f
Sasha H <sashahdev23@gmail.com>
2021-07-31 02:56:49 +1000

    feat: Add Quickstart docs pages 'Install AppMap Agent' and 'Open AppMaps' (#297)
    * feat: quickstart docs pages 'Install AppMap Agent' and 'Open AppMaps'
    
    * fix: small improvements in Quickstart docs pages
    
    * fix: hide languages dropdown for 'Install AppMap Agent' page
    
    * feat: show message when no one language in project was detected
    
    * fix: edit text of 'Open AppMaps' doc page

--- packages/components/src/componentExports.js
+++ packages/components/src/componentExports.js
@@ -4,3 +4,5 @@ export { default as VTabs } from '@/components/Tabs.vue';
 export { default as VVsCodeExtension } from '@/pages/VsCodeExtension.vue';
 export { default as VDiff } from '@/pages/Diff.vue';
 export { default as VQuickstart } from '@/pages/Quickstart.vue';
+export { default as VQuickstartDocsInstallAgent } from '@/pages/InstallAgent.vue';
+export { default as VQuickstartDocsOpenAppmaps } from '@/pages/OpenAppMaps.vue';
---
+++ packages/components/src/stories/OpenAppMaps.stories.js
@@ -0,0 +1,37 @@
+import OpenAppMaps from '@/pages/OpenAppMaps.vue';
+
+export default {
+  title: 'Pages/VS Code',
+  component: OpenAppMaps,
+  args: {
+    appmaps: [
+      {
+        name: 'Appmap 1',
+        path: '/path/to/appmap',
+        requests: 4,
+        sqlQueries: 29,
+        functions: 136,
+      },
+      {
+        name: 'Appmap 2',
+        path: '/another/path/to/appmap',
+        requests: 1,
+        sqlQueries: 15,
+        functions: 22,
+      },
+      {
+        name: 'Appmap 3',
+        path: '/one/more/path/to/appmap',
+        requests: 12,
+        sqlQueries: 7,
+        functions: 279,
+      },
+    ],
+  },
+};
+
+export const openAppMaps = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { OpenAppMaps },
+  template: '<OpenAppMaps v-bind="$props" ref="openAppMaps" />',
+});
