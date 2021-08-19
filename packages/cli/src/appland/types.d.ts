3501ca9219c4a2ebe0a1aaf9ece644261ddb9fa1
Dustin Byrne <dustinsbyrne@gmail.com>
2021-08-19 15:34:54 -0400

    chore: introduce TypeScript

--- packages/cli/src/appland/types.d.ts
+++ packages/cli/src/appland/types.d.ts
@@ -1,3 +1,10 @@
+export interface Fingerprint {
+  appmap_digest: string;
+  canonicalization_algorithm: string;
+  digest: string;
+  fingerprint_algorithm: string;
+}
+
 export interface AppMapMetadata {
   name: string;
   fingerprints: Fingerprint[];
