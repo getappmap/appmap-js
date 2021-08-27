3cb3ce21ecf9fa5e4475f787c73410f83157dbca
Dustin Byrne <dustinsbyrne@gmail.com>
2021-08-27 14:15:57 -0400

    allow user to supply project framework in addition to language
    Instead of installing for `java`, the user can now specify the framework
    which also implies the language, e.g. `maven` implies `java`.

--- packages/cli/src/cmds/agentInstaller/install-agent.ts
+++ packages/cli/src/cmds/agentInstaller/install-agent.ts
@@ -15,6 +15,21 @@ const AGENT_INSTALLERS = {
   python: PythonAgentInstaller,
 };
 
+const AGENT_FRAMEWORK_INSTALLERS = Object.values(AGENT_INSTALLERS)
+  .flatMap((installerLanguage) =>
+    // Take every sub-installer and return it as an array of objects formatted like so:
+    // { maven: { installerFramework: MavenAgentInstaller, installerLanguage: JavaAgentInstaller }}
+    // { gradle: { installerFramework: GradleAgentInstaller, installerLanguage: JavaAgentInstaller }}
+    // etc.
+    installerLanguage.installers.map((installerFramework) => ({
+      [new installerFramework().name.toLowerCase()]: {
+        installerLanguage,
+        installerFramework,
+      },
+    }))
+  )
+  .reduce((acc, entry) => ({ ...acc, ...entry }), {});
+
 export const command = 'install-agent [project-type]';
 export const describe = 'Install and configure an AppMap language agent';
 
@@ -23,8 +38,24 @@ function supportedTargetsMessage(): string {
     .map((name) => chalk.blue(name))
     .join(', ');
 
-  return `Supported languages: ${supportedLanguages}`;
+  const supportedFrameworks = Object.keys(AGENT_FRAMEWORK_INSTALLERS)
+    .map((name) => chalk.blue(name))
+    .join(', ');
+
+  return [
+    `Supported languages: ${supportedLanguages}`,
+    `Supported project frameworks: ${supportedFrameworks}`,
+  ].join('\n');
+}
+
 export const builder = (args) => {
+  args.positional('project-type', {
+    describe: [
+      'Specifies the installation target. This may be a language or project framework. Case-insensitive.',
+      supportedTargetsMessage(),
+    ].join('\n'),
+    type: 'string',
+    default: undefined,
   });
   args.option('dir', {
     describe: 'directory in which to install',
@@ -38,16 +69,39 @@ export const handler = async (argv) => {
   verbose(argv.verbose);
 
   const commandFn = async () => {
-    const { language, dir } = argv;
-    let installerOptions = AGENT_INSTALLERS[language];
+    const { projectType, dir } = argv;
+    let installTarget = projectType;
+    if (!installTarget) {
+      const { result } = await UI.prompt({
+        type: 'list',
+        name: 'result',
+        message: 'Select a target language for agent installation.',
+        choices: Object.values(AGENT_INSTALLERS).map((i) => i.name),
+      });
+      installTarget = result;
+    }
+
+    installTarget = installTarget.toLowerCase();
+
+    let installerOptions = AGENT_INSTALLERS[installTarget];
+    let installerFramework;
+
     if (!installerOptions) {
+      const installerInfo = AGENT_FRAMEWORK_INSTALLERS[installTarget];
+
+      if (!installerInfo) {
         UI.error(
-        [
-          `${chalk.red(installTarget)} is not a supported installer type.`,
-          supportedTargetsMessage(),
-        ].join('\n')
-      );
-      process.exit(1);
+          [
+            `${chalk.red(installTarget)} is not a supported installer type.`,
+            supportedTargetsMessage(),
+          ].join('\n')
+        );
+        process.exit(1);
+      }
+
+      // The user has specified a framework
+      installerOptions = installerInfo.installerLanguage;
+      installerFramework = installTarget;
     }
 
     const installer = new AgentInstallerProcedure(installerOptions, dir);
