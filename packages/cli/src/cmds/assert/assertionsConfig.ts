d3c28346484f166b2e9a1e365a3be895cd8600c2
Kevin Gilpin <kgilpin@gmail.com>
2021-08-30 15:57:35 -0400

    Use Event accessor methods in assertionsConfig

--- packages/cli/src/cmds/assert/assertionsConfig.ts
+++ packages/cli/src/cmds/assert/assertionsConfig.ts
@@ -1,14 +1,18 @@
 // @ts-ignore
 import { Event } from '@appland/models';
-import Assertion from "./assertion";
+import Assertion from './assertion';
 
 export default class AssertionsConfig {
   assertions: Assertion[];
 
   constructor() {
     this.assertions = [
-      new Assertion('http_server_request',(e: Event) => e.elapsed < 1),
-      new Assertion('sql_query', (e: Event) => e.elapsed < 1, (e: Event) => e.sql.sql.match(/SELECT/)),
+      new Assertion('http_server_request', (e: Event) => e.elapsed < 1),
+      new Assertion(
+        'sql_query',
+        (e: Event) => e.elapsedTime < 1,
+        (e: Event) => e.sqlQuery.match(/SELECT/)
+      ),
     ];
   }
 }
