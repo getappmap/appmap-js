4c2752675530be2fc1d0305f99aeb31ca1bf4216
Kevin Gilpin <kgilpin@gmail.com>
2021-08-31 09:25:51 -0400

    Add assertion description via builder function

--- packages/cli/src/cmds/assert/assertion.ts
+++ packages/cli/src/cmds/assert/assertion.ts
@@ -4,13 +4,36 @@ import { Scope } from './types';
 import { AppMapData } from '../../appland/types';
 
 export default class Assertion {
+  public where?: (e: Event, appMap?: AppMapData) => boolean;
+  public description?: string;
+
+  static assert(
+    scope: Scope,
+    assert: (e: Event, appMap: AppMapData) => Boolean,
+    cb?: (assertion: Assertion) => void
+  ) {
+    const assertion = new Assertion(scope, assert);
+    if (cb) {
+      cb(assertion);
+    }
+    return assertion;
+  }
+
   constructor(
     public scope: Scope,
-    public assert: (e: Event, appMap: AppMapData) => Boolean,
-    public where?: (e: Event, appMap: AppMapData) => Boolean,
+    public assert: (e: Event, appMap: AppMapData) => Boolean
   ) {}
 
   toString(): string {
-    return `[${this.scope}] ${this.assert} ${this.where ? `(where ${this.where})` : ''}`
+    const tokens = [`[${this.scope}]`];
+    if (this.description) {
+      tokens.push(this.description);
+    } else {
+      tokens.push(this.assert.toString());
+    }
+    if (this.where) {
+      tokens.push(`(where ${this.where})`);
+    }
+    return tokens.join(' ');
   }
 }
--- packages/cli/src/cmds/assert/defaultAssertions.ts
+++ packages/cli/src/cmds/assert/defaultAssertions.ts
@@ -3,11 +3,28 @@ import { Event } from '@appland/models';
 import Assertion from './assertion';
 
 const assertions: Assertion[] = [
-  new Assertion('http_server_request', (e: Event) => e.elapsed < 1),
-  new Assertion(
+  Assertion.assert(
+    'http_server_request',
+    (e: Event) => e.elapsed < 0.25,
+    (assertion: Assertion): void => {
+      assertion.description = 'Slow HTTP server request';
+    }
+  ),
+  Assertion.assert(
     'sql_query',
-    (e: Event) => e.elapsedTime < 1,
-    (e: Event) => e.sqlQuery.match(/SELECT/)
+    (e: Event) => e.elapsedTime < 0.1,
+    (assertion: Assertion): void => {
+      assertion.where = (e: Event) => e.sqlQuery.match(/SELECT/);
+      assertion.description = 'Slow SQL query';
+    }
+  ),
+  Assertion.assert(
+    'sql_query',
+    (e: Event) =>
+      e.ancestors().every((e: Event) => !e.codeObject.labels.has('mvc.view')),
+    (assertion: Assertion): void => {
+      assertion.description = 'SQL query from the view';
+    }
   ),
 ];
 
