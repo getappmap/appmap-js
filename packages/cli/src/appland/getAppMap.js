0b3388580b0c481a80070f8ccbbbd03a532d53dc
Kevin Gilpin <kgilpin@gmail.com>
2021-09-01 13:45:16 -0400

    feat: Retry failed attempts to download AppMaps

--- packages/cli/src/appland/getAppMap.js
+++ packages/cli/src/appland/getAppMap.js
@@ -2,6 +2,8 @@ const { request: httpRequest } = require('http');
 const { request: httpsRequest } = require('https');
 const { baseURL, apiKey, exists } = require('./settings');
 
+const MAX_ATTEMPTS = 3;
+
 /**
  * Loads AppMap data from UUID.
  *
@@ -14,39 +16,49 @@ const getAppMap = async (uuid) => {
   }
 
   return new Promise((resolve, reject) => {
-    const getScenarioURL = new URL([baseURL, 'api/appmaps', uuid].join('/'));
-    const requestFunction =
-      getScenarioURL.protocol === 'https:' ? httpsRequest : httpRequest;
-    const req = requestFunction(
-      getScenarioURL,
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          Accept: 'application/json',
+    let attempt = 1;
+
+    function makeRequest() {
+      const getScenarioURL = new URL([baseURL, 'api/appmaps', uuid].join('/'));
+      const requestFunction =
+        getScenarioURL.protocol === 'https:' ? httpsRequest : httpRequest;
+      const req = requestFunction(
+        getScenarioURL,
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            Accept: 'application/json',
+          },
         },
-      },
-      // eslint-disable-next-line consistent-return
-      (res) => {
-        if (res.statusCode >= 300) {
-          return reject(res.statusCode);
+        // eslint-disable-next-line consistent-return
+        (res) => {
+          if (res.statusCode >= 300) {
+            return reject(res.statusCode);
+          }
+          let data = '';
+          res.setEncoding('utf8');
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            resolve(JSON.parse(data));
+          });
         }
-        let data = '';
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          resolve(JSON.parse(data));
-        });
-      }
-    );
+      );
 
-    req.on('error', (e) => {
-      reject(e);
-    });
+      req.on('error', (e) => {
+        if (attempt < MAX_ATTEMPTS) {
+          attempt += 1;
+          setTimeout(attempt * attempt * 50, makeRequest);
+        } else {
+          reject(e);
+        }
+      });
 
-    // Write data to request body
-    req.end();
+      req.end();
+    }
+
+    makeRequest();
   });
 };
 
