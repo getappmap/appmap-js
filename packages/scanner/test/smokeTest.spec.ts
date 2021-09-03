452f58d280d43ba16ec17d770aaf29fc8772a105
Alex Torchenko <torchenko@gmail.com>
2021-09-03 14:35:53 -0400

    Add test for default config path

--- packages/scanner/test/smokeTest.spec.ts
+++ packages/scanner/test/smokeTest.spec.ts
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 import AssertCommand from '../src/command';
 
 describe('smoke test', () => {
-  afterAll(() => {
+  afterEach(() => {
     sinon.restore();
   });
 
@@ -13,6 +13,17 @@ describe('smoke test', () => {
       appmapDir: join(__dirname, 'fixtures', 'appmaps'),
       config: join(__dirname, '..', 'src', 'defaultAssertions.ts'),
       format: 'progress',
+    } as any);
+
+    expect(processExit.calledWith(0)).toBe(true);
+  });
+
+  it('runs as expected with default config', async () => {
+    const processExit = sinon.stub(process, 'exit');
+    await AssertCommand.handler({
+      appmapDir: join(__dirname, 'fixtures', 'appmaps'),
+      format: 'progress',
+      config: '../defaultAssertions.ts', // need to pass it explicitly
     } as any);
 
     expect(processExit.calledWith(0)).toBe(true);
