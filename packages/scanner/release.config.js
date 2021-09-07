e1d63106f141bfd2c37d4956802281bab036592c
Alex Torchenko <torchenko@gmail.com>
2021-09-07 12:42:05 -0400

    ci: Configure semantic-release for Github Packages (#4)
    * Configure semantic-release for Github Packages
    
    * add semantic-release deps
    
    Co-authored-by: Dustin Byrne <dustinsbyrne@gmail.com>

---
+++ packages/scanner/release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer', // determine what needs to be released
+    '@semantic-release/release-notes-generator', // generate release notes
+    '@semantic-release/changelog', // append release notes to CHANGELOG.md
+    '@semantic-release/npm', // publish to NPM using current login and config, bump `version`
+    '@semantic-release/git', // commit code changes (CHANGELOG.md, package.json)
+    '@semantic-release/github', // add a GitHub release
+  ],
+};
