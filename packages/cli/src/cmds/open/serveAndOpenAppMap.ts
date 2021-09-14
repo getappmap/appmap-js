250c26d92665c81264a29576c2155409b144ab5c
Kevin Gilpin <kgilpin@gmail.com>
2021-09-14 10:49:22 -0400

    feat: Serve the appmap file and JS map file

--- packages/cli/src/cmds/open/serveAndOpenAppMap.ts
+++ packages/cli/src/cmds/open/serveAndOpenAppMap.ts
@@ -1,50 +1,84 @@
 import { createReadStream } from 'fs';
-import { createServer } from 'http';
-import { AddressInfo } from 'net';
+import { createServer, ServerResponse } from 'http';
+import { AddressInfo, Server } from 'net';
 import open from 'open';
+import { join } from 'path';
 import { parse } from 'url';
+import { exists, verbose } from '../../utils';
 import UI from '../userInteraction';
 
 export default async function serveAndOpenAppMap(appMapFile: string) {
   UI.progress(`Opening ${appMapFile}`);
 
-  return new Promise((resolve) => {
-    const server = createServer((req, res) => {
-      try {
-        var requestUrl = parse(req.url!);
-        const pathname = requestUrl.pathname;
-        if (pathname === '/appmap.html') {
-          res.writeHead(200, 'OK', { 'Content-Type': 'text/html' });
+  const builtDir = join(__dirname, '../../../built');
+  const baseDir = (await exists(builtDir))
+    ? builtDir
+    : join(__dirname, '../..');
 
-          // TODO: Create and serve the HTML page with the AppMap
+  const server = createServer(async (req, res) => {
+    // Once a request is received, the server can be closed.
+    // The socket will remain open for a while as it serves requests.
+    server!.close();
 
-          const fileStream = createReadStream(appMapFile);
-          fileStream.pipe(res);
-          fileStream.on('open', function () {
-            res.writeHead(200);
-          });
-          fileStream.on('error', function (e) {
-            res.writeHead(404);
-            res.end();
-          });
-          fileStream.on('end', function () {
-            // Once the file is served, I can go away.
-            // TODO: Doesn't seem to be working though. The process does not exit.
-            server.close();
-          });
-        } else {
-          console.log(`Not found: ${pathname}`);
-          res.writeHead(404);
-          res.end();
+    const serveStaticFile = (fileName: string, contentType: string) => {
+      res.writeHead(200, 'OK', { 'Content-Type': contentType });
+
+      const fileStream = createReadStream(fileName);
+      fileStream.pipe(res);
+      fileStream.on('open', function () {
+        if (verbose()) {
+          console.log(`${fileName}: 200`);
         }
-      } catch (e) {
-        console.log(e.stack);
-        res.writeHead(500);
-        res.end(); // end the response so browsers don't hang
+        res.writeHead(200);
+      });
+      fileStream.on('error', function (e) {
+        if (verbose()) {
+          console.log(`${fileName}: 404`);
+        }
+        res.writeHead(404);
+        res.end();
+      });
+    };
+
+    try {
+      if (verbose()) {
+        console.log(req.url);
       }
-    }).listen(0, '127.0.0.1', () => {
-      const port = (server.address() as AddressInfo).port;
-      resolve(open(`http://localhost:${port}/appmap.html`));
+
+      var requestUrl = parse(req.url!);
+      const pathname = requestUrl.pathname;
+
+      if (pathname === '/') {
+        return serveStaticFile(join(baseDir, 'appmap.html'), 'text/html');
+      } else if (pathname === '/main.js.map') {
+        return serveStaticFile(join(baseDir, 'main.js.map'), 'text/javascript');
+      } else if (pathname === '/appmap') {
+        const urlParams = new URLSearchParams(requestUrl.query!);
+        const appMapFile = urlParams.get('file');
+        if (appMapFile) {
+          return serveStaticFile(appMapFile!, 'application/json');
+        }
+      }
+
+      if (verbose()) {
+        console.log(`${pathname}: 404`);
+      }
+      res.writeHead(404);
+      res.end();
+    } catch (e) {
+      console.log(e.stack);
+      res.writeHead(500);
+      res.end(); // end the response so browsers don't hang
+    }
+  })
+    .listen(0, '127.0.0.1', () => {
+      const port = (server!.address() as AddressInfo).port;
+      open(
+        `http://localhost:${port}/?appmap=${encodeURIComponent(appMapFile)}`
+      );
+    })
+    .on('connection', function (socket) {
+      // Don't let the open socket keep the process alive.
+      socket.unref();
     });
-  });
 }
--- packages/cli/src/html/page.js
+++ packages/cli/src/html/page.js
@@ -7,26 +7,25 @@ require('@appland/diagrams/dist/style.css');
 Vue.use(Vuex);
 Vue.use(plugin);
 
-// eslint-disable-next-line no-unused-vars
-const app = new Vue({
-  el: '#app',
-  render: (h) =>
-    h(VVsCodeExtension, {
-      ref: 'ui',
-      props: {
-        appMapUploadable: true,
-      },
-    }),
-  mounted() {
-    const { ui } = this.$refs;
+async function initializeApp() {
+  return new Vue({
+    el: '#app',
+    render: (h) =>
+      h(VVsCodeExtension, {
+        ref: 'ui',
+        props: {
+          appMapUploadable: true,
+        },
+      }),
+    async mounted() {
+      const params = new URL(document.location).searchParams;
+      const appmap = params.get('appmap');
+      const res = await fetch(`/appmap?file=${encodeURIComponent(appmap)}`);
+      const { ui } = this.$refs;
 
-    const params = new URL(document.location).searchParams;
-    const appmap = params.get('appmap');
+      ui.loadData((await res.json()) || {});
+    },
+  });
+}
 
-    if (appmap) {
-      ui.loadData(appmap);
-    } else {
-      ui.loadData({});
-    }
-  },
-});
+initializeApp();
