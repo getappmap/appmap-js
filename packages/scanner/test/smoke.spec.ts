35b72d7105f37b831ab023524e0f044bcc25bc60
Kevin Gilpin <kgilpin@gmail.com>
2021-10-01 17:38:41 -0400

    test: Add scanner tests

--- packages/scanner/test/smoke.spec.ts
+++ packages/scanner/test/smoke.spec.ts
@@ -1,27 +1,20 @@
 import { join } from 'path';
 import sinon from 'sinon';
 import AssertCommand from '../src/command';
+import { fixtureAppMap } from './util';
 
 describe('smoke test', () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  it('runs as expected', async () => {
-    const processExit = sinon.stub(process, 'exit');
-    await AssertCommand.handler({
-      appmapDir: join(__dirname, 'fixtures', 'appmaps'),
-      config: join(__dirname, '..', 'src', 'sampleConfig', 'default.ts'),
-      format: 'progress',
-    } as any);
-
-    expect(processExit.calledWith(0)).toBe(true);
-  });
-
   it('runs as expected with default config', async () => {
+    sinon.stub(process.stdout, 'write');
     const processExit = sinon.stub(process, 'exit');
     await AssertCommand.handler({
-      appmapDir: join(__dirname, 'fixtures', 'appmaps'),
+      appmapFile: fixtureAppMap(
+        'org_springframework_samples_petclinic_owner_OwnerControllerTests_testInitCreationForm.appmap.json'
+      ),
       format: 'progress',
       config: join(__dirname, '..', 'src', 'sampleConfig', 'default.ts'), // need to pass it explicitly
     } as any);
