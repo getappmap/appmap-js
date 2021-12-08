f5cb5ac5fcb1d831f9e350af9bfdda835c554867
Kevin Gilpin <kgilpin@gmail.com>
2021-12-08 10:55:16 -0500

    refactor: Remove src/scanner in favor of src/rules

--- packages/scanner/src/scanner/lib/hasParameterOrReceiver.ts
+++
@@ -1,9 +0,0 @@
-import { Event } from '@appland/models';
-
-// Builds a function that returns true if the provided event argument has the specified
-// objectId as the receiver or as a parameter value.
-export default (objectId: number) => {
-  return (event: Event): boolean =>
-    (!!event.receiver && event.receiver!.object_id === objectId) ||
-    (!!event.parameters && event.parameters!.some((param) => param.object_id === objectId));
-};
