75982aab2c9a37aa254de50ef0e53c6b674ddbea
Kevin Gilpin <kgilpin@gmail.com>
2021-12-14 16:48:58 -0500

    feat: Use SqliteParser types

--- packages/models/types/sqlite-parser.d.ts
+++ packages/models/types/sqlite-parser.d.ts
@@ -1,16 +1,14 @@
-declare module 'sqlite-parser' {
-  export namespace SqliteParser {
-    export interface ListStatement {
-      type: 'statement';
-      variant: 'list';
-      statement: Statement[];
-    }
-    export interface TransactionStatement {
-      type: 'statement';
-      variant: 'transaction';
-      action: 'begin' | 'rollback' | 'commit';
-    }
-    export type Statement = ListStatement | TransactionStatement;
+export namespace SqliteParser {
+  export interface ListStatement {
+    type: 'statement';
+    variant: 'list';
+    statement: Statement[];
   }
-  export default function (sql: string): SqliteParser.ListStatement;
+  export interface TransactionStatement {
+    type: 'statement';
+    variant: 'transaction';
+    action: 'begin' | 'rollback' | 'commit';
+  }
+  export type Statement = ListStatement | TransactionStatement;
 }
+export default function (sql: string): SqliteParser.ListStatement;
