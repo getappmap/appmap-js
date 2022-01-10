2331cf982a9cda8a8f05cfebfd1ae0330d7acfbf
Rafał Rzepecki <rafal@app.land>
2022-01-10 15:55:17 +0100

    Improve code style in insecure-compare rule
    Remove some redundant types and simplify some code.

--- packages/scanner/src/rules/insecureCompare.ts
+++ packages/scanner/src/rules/insecureCompare.ts
@@ -7,12 +7,12 @@ const BCRYPT_REGEXP = /^[$]2[abxy]?[$](?:0[4-9]|[12][0-9]|3[01])[$][./0-9a-zA-Z]
 
 const secrets: Set<string> = new Set();
 
-function stringEquals(e: Event): string | boolean | import('../types').MatchResult[] | undefined {
+function stringEquals(e: Event): boolean {
   if (!e.parameters || !e.receiver || e.parameters!.length !== 1) {
-    return;
+    return false;
   }
 
-  const args = [e.receiver!.value, e.parameters![0].value];
+  const args = [e.receiver.value, e.parameters[0].value];
 
   function isBcrypt(str: string): boolean {
     return BCRYPT_REGEXP.test(str);
@@ -22,9 +22,7 @@ function stringEquals(e: Event): string | boolean | import('../types').MatchResu
     if (secrets.has(str)) {
       return true;
     }
-    return !!Object.keys(SecretsRegexes).find(
-      (key): boolean => !!SecretsRegexes[key].find((re: RegExp): boolean => re.test(str))
-    );
+    return Object.values(SecretsRegexes).some((res) => res.some((re) => re.test(str)));
   }
 
   // BCrypted strings are safe to compare using equals()
@@ -36,7 +34,7 @@ function build(): RuleLogic {
     if (e.codeObject.labels.has(Secret)) {
       recordSecrets(secrets, e);
     }
-    if (e.parameters && e.codeObject.labels.has(StringEquals)) {
+    if (e.codeObject.labels.has(StringEquals)) {
       return stringEquals(e);
     }
   }
