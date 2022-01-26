85d7a754a2da457aa39719ebdd0f75416f04e3dd
Kevin Gilpin <kgilpin@gmail.com>
2022-01-26 14:19:51 -0500

    feat: Shortcut the record 'open' step when running in the VSCode terminal

--- packages/cli/src/cmds/open/showAppMap.ts
+++ packages/cli/src/cmds/open/showAppMap.ts
@@ -1,5 +1,6 @@
+import { spawn } from 'child_process';
 import UI from '../userInteraction';
-import { openInBrowser, openInTool, openInVSCode } from './openers';
+import { abspath, openInBrowser, openInTool, openInVSCode } from './openers';
 
 interface Opener {
   name: string;
@@ -16,8 +17,16 @@ const Openers: Opener[] = [
 ];
 
 export default async function showAppMap(appMapFile: string) {
+  const termProgram = process.env.TERM_PROGRAM;
+  if (process.platform === 'darwin') {
+    if (termProgram === 'vscode') {
+      spawn('code', [abspath(appMapFile)]);
+      return;
+    }
+  }
+
   UI.progress(
-    `There are three options for viewing the AppMap. Please choose one (you can do this as many times as you'd like).`
+    `There are multiple options for viewing the AppMap. Please choose one (you can do this as many times as you'd like).`
   );
   const { outputTool } = await UI.prompt({
     type: 'list',
@@ -28,7 +37,11 @@ export default async function showAppMap(appMapFile: string) {
 
   const opener = Openers.find((o) => o.name === outputTool);
   if (opener) {
+    console.log();
+    console.log(`Click this link to open the AppMap in ${outputTool}:`);
+    console.log();
     await opener.fn(appMapFile, opener.toolId);
+    console.log();
     await showAppMap(appMapFile);
   }
 }
