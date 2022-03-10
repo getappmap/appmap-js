055d742b83d66fe6acc9550fa8f2f966a7d9351d
Rafał Rzepecki <rafal@app.land>
2022-03-10 15:56:53 -0500

    Split scope tests

--- packages/scanner/src/scope/rootScope.ts
+++ packages/scanner/src/scope/rootScope.ts
@@ -4,7 +4,7 @@ import ScopeImpl from './scopeImpl';
 import ScopeIterator from './scopeIterator';
 
 export default class RootScope extends ScopeIterator {
-  *scopes(events: Generator<Event>): Generator<Scope> {
+  *scopes(events: IterableIterator<Event>): Generator<Scope> {
     for (const event of events) {
       if (event.isCall() && !event.parent) {
         yield new ScopeImpl(event);
