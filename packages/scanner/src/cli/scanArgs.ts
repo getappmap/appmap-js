dea963101ac6b93cae3a87a57d8b3dac49e714b5
Kevin Gilpin <kgilpin@gmail.com>
2022-03-23 17:53:26 -0400

    feat: 'a' is an alias for 'app'

--- packages/scanner/src/cli/scanArgs.ts
+++ packages/scanner/src/cli/scanArgs.ts
@@ -23,5 +23,6 @@ export default function (args: Argv): void {
   args.option('app', {
     describe:
       'name of the app to publish the findings for. By default, this is determined by looking in appmap.yml',
+    alias: 'a',
   });
 }
