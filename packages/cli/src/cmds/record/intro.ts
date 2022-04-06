c70532b7480e2f310e67481922620ed637bdef41
Pete Cheslock <petecheslock@gmail.com>
2022-04-06 11:14:45 -0400

    fix: correct typo in the recording intro message

--- packages/cli/src/cmds/record/intro.ts
+++ packages/cli/src/cmds/record/intro.ts
@@ -4,7 +4,7 @@ import ready from './ready';
 export default async function () {
   UI.progress(`To create a recording, the first thing you need to do is run your app using
   the instructions in the AppMap documentation. Choose the most suitable link here,
-  then configure and launch your app process. Press enter wen you're ready to continue.`);
+  then configure and launch your app process. Press enter when you're ready to continue.`);
   UI.progress(`
   Rails:      https://appland.com/docs/reference/appmap-ruby.html#remote-recording
   Django:     https://appland.com/docs/reference/appmap-python.html#django
