5c25abdf4c3c223113e7923a66caed53771f369b
Kevin Gilpin <kgilpin@gmail.com>
2022-04-26 16:46:46 -0400

    feat: Boldly print if the agent is available or not

--- packages/cli/src/cmds/record/test/isAgentAvailable.ts
+++ packages/cli/src/cmds/record/test/isAgentAvailable.ts
@@ -4,13 +4,16 @@ import RemoteRecording from '../remoteRecording';
 
 export default async function isAgentAvailable(): Promise<boolean> {
   const ro = await requestOptions();
-  UI.status = `Checking if the AppMap agent is available at ${ro.protocol}//${ro.hostname}:${ro.port}${ro.path}`;
+  const locationStr = `${ro.protocol}//${ro.hostname}:${ro.port}${ro.path}`;
+  UI.status = `Checking if the AppMap agent is available at ${locationStr}`;
   try {
     await new RemoteRecording(ro).status();
-    UI.success();
+    UI.success(`AppMap agent is available at ${locationStr}`);
     return true;
-  } catch (e) {
-    UI.error();
+  } catch (e: any) {
+    UI.error(
+      `AppMap agent is not avaliable at ${locationStr}: ${e.toString()}`
+    );
     return false;
   }
 }
