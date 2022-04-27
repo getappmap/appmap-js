afc19a953c43f74736d63949ad6a866e3bf5ee15
Kevin Gilpin <kgilpin@gmail.com>
2022-04-27 12:20:32 -0400

    feat: Telemetry for 'record'

--- packages/cli/src/cmds/runCommand.ts
+++ packages/cli/src/cmds/runCommand.ts
@@ -2,28 +2,76 @@ import { AbortError, ValidationError } from './errors';
 import { verbose } from '../utils';
 import UI from './userInteraction';
 import chalk from 'chalk';
-import Yargs from 'yargs';
+import Yargs, { command } from 'yargs';
+import { ExitCode } from './record/types/exitCode';
+import Telemetry from '../telemetry';
 
-export default async function runCommand(fn: () => Promise<any>) {
-  return fn().catch((err) => {
+async function invokeCommand() {}
+
+function flushTelemetry(exitCode: ExitCode, err?: Error) {
+  Telemetry.flush(() => {
+    // @types/yargs defines:
+    //   exit(code: number, err: Error): void;
+    // but in the implementation, err appears to be optional.
+    Yargs.exit(exitCode, err as any);
+  });
+}
+
+export default async function runCommand(
+  commandPrefix: string,
+  fn: () => Promise<any>
+): Promise<void> {
+  try {
+    await fn();
+
+    Telemetry.sendEvent({
+      name: `${commandPrefix}:exit-ok`,
+    });
+
+    flushTelemetry(0);
+  } catch (err) {
     if (err instanceof ValidationError) {
       console.warn(err.message);
-      return Yargs.exit(1, err);
-    }
-    if (err instanceof AbortError) {
-      return Yargs.exit(2, err);
-    }
 
-    if (verbose()) {
-      UI.error(err);
+      Telemetry.sendEvent({
+        name: `${commandPrefix}:validation-error`,
+        properties: {
+          error: err.message,
+        },
+      });
+
+      return flushTelemetry(ExitCode.Error, err);
+    } else if (err instanceof AbortError) {
+      Telemetry.sendEvent({
+        name: `${commandPrefix}:abort`,
+        properties: {
+          error: err.message,
+        },
+      });
+
+      return flushTelemetry(ExitCode.Quit, err);
+    } else if (err instanceof Error) {
+      Telemetry.sendEvent({
+        name: `${commandPrefix}:error`,
+        properties: {
+          error: err.message,
+        },
+      });
+
+      if (verbose()) {
+        UI.error(err);
+      } else {
+        UI.error(`An error occurred: ${err.toString().split('\n')[0]}`);
+        UI.error(
+          `Try re-running the command with the ${chalk.red(
+            'verbose'
+          )} flag (${chalk.red('-v')}).`
+        );
+      }
+      flushTelemetry(ExitCode.Error, err);
     } else {
-      UI.error(`An error occurred: ${err.toString().split('\n')[0]}`);
-      UI.error(
-        `Try re-running the command with the ${chalk.red(
-          'verbose'
-        )} flag (${chalk.red('-v')}).`
-      );
+      // idk why this wouldn't be an Error ^^
+      throw err;
     }
-    Yargs.exit(3, err);
-  });
+  }
 }
