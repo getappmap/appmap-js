ea0b958a672ddb55894cce3f8d4eb376030849eb
Kevin Gilpin <kgilpin@gmail.com>
2022-04-27 18:11:41 -0400

    refactor: Metadata scope and enumerateScope are optional

--- packages/scanner/src/rules/lib/metadata.ts
+++ packages/scanner/src/rules/lib/metadata.ts
@@ -2,9 +2,9 @@ import { ImpactDomain, ScopeName } from '../../types';
 
 export type Metadata = {
   title: string;
-  scope: ScopeName;
-  enumerateScope: boolean;
   impactDomain: ImpactDomain;
   references: Record<string, string>;
   labels?: string[];
+  scope?: ScopeName;
+  enumerateScope?: boolean;
 };
