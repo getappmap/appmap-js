ec9766ef17ace1e4ac8cb128a30c320dc7ae202b
Kevin Gilpin <kgilpin@gmail.com>
2022-04-27 17:00:18 -0400

    feat: 'record' command finishes by prompting with next steps

--- packages/cli/src/cmds/record/action/printAppMapCount.ts
+++ packages/cli/src/cmds/record/action/printAppMapCount.ts
@@ -1,8 +1,13 @@
+import { config } from 'yargs';
 import { listAppMapFiles, verbose } from '../../../utils';
 import UI from '../../userInteraction';
+import { readConfig } from '../configuration';
 
-export default async function printAppMapCount(appMapDir: string) {
+export default async function printAppMapCount() {
   let fileCount = 0;
+
+  const configuredAppMapDir = (await readConfig())?.appmap_dir;
+  const appMapDir = configuredAppMapDir || '.';
 
   // This function is too verbose to be useful in this context.
   const v = verbose();
@@ -10,7 +15,24 @@ export default async function printAppMapCount(appMapDir: string) {
   await listAppMapFiles(appMapDir, (_fileName: string) => (fileCount += 1));
   verbose(v);
 
-  UI.progress(
-    `There are now ${fileCount} AppMap files in directory ${appMapDir}`
+  UI.success(
+    `There are now ${fileCount} AppMap files in directory '${appMapDir}'.`
+  );
+  UI.success(
+    `To generate OpenAPI from the AppMaps, run:
+
+    npx @appland/appmap openapi --appmap-dir ${appMapDir} -o openapi.yml
+
+To scan the AppMaps for potential code design issues, run:
+
+    npx @appland/scanner scan --appmap-dir ${appMapDir} --all
+
+To search the AppMaps for a class or package, run a command like:
+
+    npx @appland/appmap inspect --appmap-dir ${appMapDir} -i package:com/example/app
+    npx @appland/appmap inspect --appmap-dir ${appMapDir} -i package:app/models
+    npx @appland/appmap inspect --appmap-dir ${appMapDir} -i class:app/models/User
+  `,
+    'left'
   );
 }
