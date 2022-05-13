fb8df829074454237259b2c6bf48d7f631de51d9
Rafał Rzepecki <rafal@app.land>
2022-05-13 06:02:58 +0200

    Remove duplicate OpenAPI code from packages/scanner

--- packages/scanner/src/openapi/index.ts
+++ packages/scanner/src/openapi/index.ts
@@ -1,41 +1,11 @@
-import provider from './provider';
-import OpenApiDiff from 'openapi-diff';
-import Model from './model';
-import SecuritySchemes from './securitySchemes';
-import { rpcRequestForEvent } from './rpcRequest';
-import { Event } from '@appland/models';
-import { OpenAPIV3 } from 'openapi-types';
 import { writeFileSync } from 'fs';
+import OpenApiDiff from 'openapi-diff';
+import { OpenAPIV3 } from 'openapi-types';
 import { verbose } from '../rules/lib/util';
-import { URL } from 'url';
 
-interface OpenAPIV3Fragment {
-  paths: OpenAPIV3.PathItemObject;
-  securitySchemes: Record<string, OpenAPIV3.SecuritySchemeObject>;
-}
+export * from '@appland/openapi';
 
-const forClientRequest = (event: Event): OpenAPIV3Fragment | undefined => {
-  const rpcRequest = rpcRequestForEvent(event);
-  if (!rpcRequest) {
-    return;
-  }
-
-  const securitySchemes = new SecuritySchemes();
-  const model = new Model();
-  securitySchemes.addRpcRequest(rpcRequest);
-  model.addRpcRequest(rpcRequest);
-
-  return { paths: model.openapi(), securitySchemes: securitySchemes.openapi() };
-};
-
-const forURL = async (
-  url: string,
-  openapiSchemata: Record<string, string>
-): Promise<OpenAPIV3.Document> => {
-  return provider(new URL(url).host, openapiSchemata);
-};
-
-const breakingChanges = async (
+export const breakingChanges = async (
   schemaHead: OpenAPIV3.Document,
   schemaBase: OpenAPIV3.Document
 ): Promise<Array<OpenApiDiff.DiffResult<'breaking'>>> => {
@@ -63,5 +33,3 @@ const breakingChanges = async (
 
   return [];
 };
-
-export { forClientRequest, forURL, breakingChanges };
