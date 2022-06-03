7d16f0329d54d2968c696261d04d1a3238f43f12
Rafał Rzepecki <rafal@app.land>
2022-06-04 00:24:40 +0200

    Add smoke test for cli commands

---
+++ packages/cli/smoketest.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ex
+
+PKGDIR="$PWD"
+TESTDIR="`mktemp -d`"
+
+yarn pack --out "$TESTDIR"/package.tgz
+
+cp -r tests/unit/fixtures/ruby "$TESTDIR"
+
+cd "$TESTDIR"
+echo '{}' > package.json
+yarn add ./package.tgz
+
+
+yarn run appmap index --appmap-dir ruby
+yarn run appmap depends --appmap-dir ruby
+yarn run appmap inventory --appmap-dir ruby
+yarn run appmap openapi -d ruby -o /dev/null
+
+
+cd "$PKGDIR"
+rm -rf "$TESTDIR"
