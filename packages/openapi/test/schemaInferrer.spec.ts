4ef46743fad691414c88238047a3f0dee20a15f6
Kevin Gilpin <kgilpin@gmail.com>
2022-06-15 11:11:32 -0400

    fix: Merge succeeds when examples are missing 'items'
    Fixes #624

--- packages/openapi/test/schemaInferrer.spec.ts
+++ packages/openapi/test/schemaInferrer.spec.ts
@@ -1,4 +1,4 @@
-import SchemaInferrer from "../src/schemaInferrer";
+import SchemaInferrer from '../src/schemaInferrer';
 
 describe(SchemaInferrer, () => {
   it('returns undefined when no examples', () => {
@@ -9,8 +9,44 @@ describe(SchemaInferrer, () => {
     const inferrer = new SchemaInferrer();
     inferrer.addExample({
       class: 'nilclass',
-      value: 'nil'
+      value: 'nil',
     });
     expect(inferrer.openapi()).toBeUndefined();
+  });
+
+  it(`can merge arrays that don't have items`, () => {
+    const inferrer = new SchemaInferrer();
+    inferrer.addExample({
+      class: 'Hash',
+      value: '{products=>[]}',
+      object_id: 70132031886120,
+      size: 1,
+      properties: [
+        {
+          name: 'products',
+          class: 'Array',
+        },
+      ],
+    });
+    inferrer.addExample({
+      class: 'Hash',
+      value: '{products=>[]}',
+      object_id: 70131584403400,
+      size: 1,
+      properties: [
+        {
+          name: 'products',
+          class: 'Array',
+        },
+      ],
+    });
+    expect(inferrer.openapi()).toEqual({
+      properties: {
+        products: {
+          type: 'array',
+        },
+      },
+      type: 'object',
+    });
   });
 });
