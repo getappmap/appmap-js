29f15a01e6589fdbd9548e6aecaae74b11e96d50
Kevin Gilpin <kgilpin@gmail.com>
2022-08-03 13:10:53 -0400

    feat: Command scope falls back on root events
    If no events qualify as commands (@command, @job, or
    http_server_request), 'command' scope will enumerate root events.

--- packages/scanner/src/scope/commandScope.ts
+++ packages/scanner/src/scope/commandScope.ts
@@ -25,16 +25,30 @@ const Job = 'job.perform';
 
 export default class CommandScope extends ScopeIterator {
   *scopes(events: IterableIterator<Event>): Generator<Scope> {
+    let found = false;
+    const roots: Event[] = [];
     for (const event of events) {
+      if (event.isCall() && !event.parent) {
+        roots.push(event);
+      }
+
       if (
         event.isCall() &&
         (event.codeObject.labels.has(Command) ||
           event.codeObject.labels.has(Job) ||
           event.httpServerRequest)
       ) {
+        found = true;
         yield new ScopeImpl(event);
 
         this.advanceToReturnEvent(event, events);
+      }
+    }
+    // If no true command is found, yield all root events.
+    if (!found) {
+      for (let index = 0; index < roots.length; index++) {
+        const event = roots[index];
+        yield new ScopeImpl(event);
       }
     }
   }
