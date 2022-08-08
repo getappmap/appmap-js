527896cb3fab5988006c4ba205dd64009e1f0c6d
Kevin Gilpin <kgilpin@gmail.com>
2022-08-08 11:50:05 -0400

    fix: Only check if non-nil String arguments are sanitized

--- packages/scanner/src/rules/eval-of-untrusted-code/rule.ts
+++ packages/scanner/src/rules/eval-of-untrusted-code/rule.ts
@@ -1,4 +1,5 @@
 import { Event, EventNavigator } from '@appland/models';
+import { classNameToOpenAPIType } from '@appland/openapi';
 import { MatchResult, RuleLogic } from '../../types';
 import precedingEvents from '../lib/precedingEvents';
 import sanitizesData from '../lib/sanitizesData';
@@ -6,23 +7,13 @@ import { Eval, EvalSafe, EvalSanitize } from './metadata';
 
 function allArgumentsSanitized(rootEvent: Event, event: Event): boolean {
   return (event.parameters || [])
-    .filter((parameter) => parameter.object_id)
+    .filter(
+      (parameter) =>
+        parameter.value &&
+        parameter.object_id &&
+        classNameToOpenAPIType(parameter.class) === 'string'
+    )
     .every((parameter): boolean => {
-      /**
-       * Here we encounter a problem with the way that Ruby Kernel.eval is recorded.
-       *
-       * > event
-       * Kernel.eval
-       * > parameter
-       * {name: 'arg', class: 'Array', object_id: 2460, value: '[puts 'Hello, I am safe!']', kind: 'rest', …}
-       *
-       * The eval parameters are recorded as 'rest' parameters (a list), so even though Kernel.eval is invoked
-       * with a single argument - the code string to evaluate - the AppMap contains an array of size 1
-       * which contains that code string. The object_id of the parameter array is not the same as the object_id of
-       * the code string, so any search for the code string being passed through a sanitize function will
-       * fail if an exact object_id  match isrequired.
-       */
-
       for (const candidate of precedingEvents(rootEvent, event)) {
         if (sanitizesData(candidate.event, parameter.object_id!, EvalSanitize)) {
           return true;
