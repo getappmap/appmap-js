53ac465281c1ae22142bd717010b43e19a0c5b0e
Rafał Rzepecki <rafal@app.land>
2022-09-29 21:26:16 +0200

    feat: Send telemetry events when indexing AppMaps
    The telemetry data sent includes number of AppMaps seen and some
    common metadata about them. An event is not sent more often than
    once per second.
    
    Note this only applies to watch mode. The intention is to let us
    see when our users are succeeding with AppMap creation, and as live
    indexing automatically happens when using IDEs, this solution
    avoids having to call home from every agent. Telemetry can also
    be easily disabled in the editors.

---
+++ packages/cli/src/lib/flattenMetadata.ts
@@ -0,0 +1,37 @@
+import { Metadata } from '@appland/models';
+
+/** Flattens metadata into a string-string map suitable for use in telemetry.
+ * Ignores git, exception and fingerprints.
+ */
+export default function flattenMetadata(metadata: Metadata): Map<string, string> {
+  const result = new Map<string, string>();
+
+  if (metadata.app) result.set('app', metadata.app);
+
+  if (metadata.client) {
+    result.set('client.name', metadata.client.name);
+    result.set('client.url', metadata.client.url);
+    if (metadata.client.version) result.set('client.version', metadata.client.version);
+  }
+
+  if (metadata.frameworks)
+    for (const { name, version } of metadata.frameworks) result.set(`framework.${name}`, version);
+
+  if (metadata.labels) result.set('labels', metadata.labels.join(', '));
+
+  if (metadata.language) {
+    result.set('language.name', metadata.language.name);
+    result.set('language.version', metadata.language.version);
+    if (metadata.language.engine) result.set('language.engine', metadata.language.engine);
+  }
+
+  if (metadata.name) result.set('name', metadata.name);
+  if (metadata.recorder) {
+    result.set('recorder.name', metadata.recorder.name);
+    if (metadata.recorder.type) result.set('recorder.type', metadata.recorder.type);
+  }
+
+  if (metadata.testStatus) result.set('testStatus', metadata.testStatus);
+
+  return result;
+}
--- packages/cli/tests/unit/fingerprint/fingerprintWatchCommand.spec.ts
+++ packages/cli/tests/unit/fingerprint/fingerprintWatchCommand.spec.ts
@@ -1,11 +1,17 @@
 import { retry } from 'async';
 import { existsSync, copyFileSync } from 'fs';
 import { stat } from 'fs/promises';
-import { join } from 'path';
+import { basename, join } from 'path';
 import { restore, stub } from 'sinon';
 import { dirSync } from 'tmp';
 import FingerprintWatchCommand from '../../../src/fingerprint/fingerprintWatchCommand';
 import { verbose } from '../../../src/utils';
+import OriginalTelemetry from '../../../src/telemetry';
+import { once } from 'events';
+import Fingerprinter from '../../../src/fingerprint/fingerprinter';
+
+jest.mock('../../../src/telemetry');
+const Telemetry = jest.mocked(OriginalTelemetry);
 
 describe(FingerprintWatchCommand, () => {
   let cmd: FingerprintWatchCommand | null;
@@ -43,11 +49,9 @@ describe(FingerprintWatchCommand, () => {
   });
 
   describe('indexing', () => {
-    function placeMap() {
-      copyFileSync(
-        join(__dirname, '../fixtures/ruby/revoke_api_key.appmap.json'),
-        join(appMapDir, 'revoke_api_key.appmap.json')
-      );
+    function placeMap(path = '../fixtures/ruby/revoke_api_key.appmap.json', name?: string) {
+      name ||= basename(path);
+      copyFileSync(join(__dirname, path), join(appMapDir, name));
     }
 
     async function verifyIndexSuccess(interval = 100, times = 10) {
@@ -68,6 +72,57 @@ describe(FingerprintWatchCommand, () => {
       cmd.watcher?.removeAllListeners();
       placeMap();
       return verifyIndexSuccess(200, 20);
+    });
+
+    describe('telemetry', () => {
+      let handler: Fingerprinter;
+
+      beforeEach(async () => {
+        cmd = new FingerprintWatchCommand(appMapDir);
+        handler = cmd.fpQueue.handler;
+        await cmd.execute();
+        jest.useFakeTimers();
+        Telemetry.sendEvent.mockClear();
+      });
+
+      afterEach(() => {
+        jest.runAllTimers();
+        jest.useRealTimers();
+      });
+
+      it('aggregates indexes that occur less than one second apart', async () => {
+        placeMap();
+        await once(handler, 'index');
+        jest.advanceTimersByTime(512);
+        placeMap('../fixtures/ruby/user_page_scenario.appmap.json');
+        await once(handler, 'index');
+        jest.advanceTimersByTime(2048);
+
+        expect(Telemetry.sendEvent).toHaveBeenCalledTimes(1);
+        const data = Telemetry.sendEvent.mock.calls[0][0];
+        expect(data).toMatchObject({
+          name: 'appmap:index',
+          properties: {
+            app: 'appland/AppLand',
+            'language.name': 'ruby',
+          },
+          metrics: { appmaps: 2 },
+        });
+        expect(data.properties?.name).toBeUndefined();
+      });
+
+      it('send separate events for indexes that occur more than one second apart', async () => {
+        placeMap();
+        await once(handler, 'index');
+        jest.advanceTimersByTime(1024);
+        placeMap('../fixtures/ruby/user_page_scenario.appmap.json');
+        await once(handler, 'index');
+        jest.advanceTimersByTime(2048);
+
+        expect(Telemetry.sendEvent).toHaveBeenCalledTimes(2);
+        for (const [data] of Telemetry.sendEvent.mock.calls)
+          expect(data).toMatchObject({ name: 'appmap:index', metrics: { appmaps: 1 } });
+      });
     });
   });
 });
