ac39fe6e10694d8f3f809c5e6f0c8d47090a3c26
Adam Trotta <ahtrotta@gmail.com>
2022-11-18 15:20:47 -0500

    feat: move findings pages and dynamically render data

--- packages/components/src/componentExports.js
+++ packages/components/src/componentExports.js
@@ -4,4 +4,6 @@ export { default as VTabs } from '@/components/Tabs.vue';
 export { default as VVsCodeExtension } from '@/pages/VsCodeExtension.vue';
 export { default as VDiff } from '@/pages/Diff.vue';
 export { default as VInstallGuide } from '@/pages/InstallGuide.vue';
+export { default as VAnalysisFindings } from '@/pages/AnalysisFindings.vue';
+export { default as VFindingDetails } from '@/pages/FindingDetails.vue';
 export { default as VOpenAppmaps } from '@/pages/install-guide/OpenAppMaps.vue';
