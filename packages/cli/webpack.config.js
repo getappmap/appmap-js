dd8407b402f3a9db98522826ccab67f2385ef8e1
Rafał Rzepecki <divided.mind@gmail.com>
2022-12-01 13:02:37 -0500

    Don't use node:crypto in @appmap/models
    @appmap/models package is also used in the browser. Using
    node:crypto requires using crypto-browserify implementation which
    bloats the build unnecessarily. We're already using crypto-js in
    other places, it can be used instead.

--- packages/cli/webpack.config.js
+++ packages/cli/webpack.config.js
@@ -36,7 +36,6 @@ module.exports = {
   resolve: {
     fallback: {
       buffer: require.resolve('buffer'),
-      crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
     },
   },
