c1b5b89896b180d71b8f2c16daae5db61769b6b2
Dustin Byrne <dustinsbyrne@gmail.com>
2022-12-06 15:27:10 -0500

    chore: Update Storybook v6.2 -> 6.5 and Webpack to v4 -> v5
    Our dated versions of Storybook and Webpack stopped working in Node 18

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config, { configType }) => {
@@ -25,11 +28,8 @@ module.exports = {
       include: path.resolve(__dirname, '../'),
     });
 
-    let rule = config.module.rules.find(
-      (r) =>
-        r.test && r.test.toString().includes('svg') && r.loader && r.loader.includes('file-loader')
-    );
-    rule.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+    let rule = config.module.rules.find((r) => r.test && r.test.toString().includes('svg'));
+    rule.test = new RegExp(rule.test.toString().replace(/(^\/)|(\|svg|svg\|)|(\/$)/g, ''));
 
     config.module.rules.push({
       test: /\.svg$/,
