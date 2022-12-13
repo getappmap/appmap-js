d8c17afd054ea564a45ea91f65a2d71a61229907
symwell <111290954+symwell@users.noreply.github.com>
2022-12-13 16:20:08 -0500

    fix: Don't use posix on win32 when testing increaseFileLimit

--- packages/cli/tests/unit/lib/increaseFileLimit.spec.ts
+++ packages/cli/tests/unit/lib/increaseFileLimit.spec.ts
@@ -1,28 +1,30 @@
-const posix = require('posix');
-
+import os from 'os';
 import * as increaseFileLimitLib from '../../../src/lib/increaseFileLimit';
 
+if (os.platform() !== 'win32') {
+    const posix = require('posix');
 
-describe('increaseFileLimit', () => {
+    describe('increaseFileLimit', () => {
 
-    it(`increaseFileLimitTo works the first time`, async () => {
-        jest.clearAllMocks();
-        const spySetRlimit = jest.spyOn(posix, 'setrlimit').mockReturnValue(true);
-        increaseFileLimitLib.increaseFileLimit();
-        expect(spySetRlimit).toBeCalledTimes(1);
-    });
+        it(`increaseFileLimitTo works the first time`, async () => {
+            jest.clearAllMocks();
+            const spySetRlimit = jest.spyOn(posix, 'setrlimit').mockReturnValue(true);
+            increaseFileLimitLib.increaseFileLimit();
+            expect(spySetRlimit).toBeCalledTimes(1);
+        });
  
-    it(`increaseFileLimit works with multiple calls to increaseFileLimitTo`, async () => {
-        jest.clearAllMocks();
-        const spySetRlimit = jest.spyOn(posix, 'setrlimit')
-        .mockImplementationOnce( () => {
-            throw new Error("EPERM, Operation not permitted");
-        })
-        .mockImplementationOnce( () => {
-            throw new Error("EPERM, Operation not permitted");
-        })
-        .mockReturnValue(true);
-        increaseFileLimitLib.increaseFileLimit();
-        expect(spySetRlimit).toBeCalledTimes(3);
-      });
-})
+        it(`increaseFileLimit works with multiple calls to increaseFileLimitTo`, async () => {
+            jest.clearAllMocks();
+            const spySetRlimit = jest.spyOn(posix, 'setrlimit')
+            .mockImplementationOnce( () => {
+                throw new Error("EPERM, Operation not permitted");
+            })
+            .mockImplementationOnce( () => {
+                throw new Error("EPERM, Operation not permitted");
+            })
+            .mockReturnValue(true);
+            increaseFileLimitLib.increaseFileLimit();
+            expect(spySetRlimit).toBeCalledTimes(3);
+        });
+    })
+}
