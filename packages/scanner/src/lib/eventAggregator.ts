6e20d7305fd86bd077c88f674615edb31854a11a
Kevin Gilpin <kgilpin@gmail.com>
2022-12-15 17:03:30 -0500

    fix: Enable scan:completed telemetry

--- packages/scanner/src/lib/eventAggregator.ts
+++ packages/scanner/src/lib/eventAggregator.ts
@@ -8,8 +8,13 @@ export type PendingEvent<E> = {
 
 export const MaxMSBetween = 10 * 1000;
 
+export type CancelFn = () => void;
+
 // TODO: Unify with the code in packages/cli - find a way to make a common import.
 export default class EventAggregator<E> {
+  private pending: PendingEvent<E>[] = [];
+  private timeout?: NodeJS.Timeout;
+
   constructor(
     private callback: (events: PendingEvent<E>[]) => void,
     private maxMsBetween = MaxMSBetween
@@ -22,13 +27,11 @@ export default class EventAggregator<E> {
     });
   }
 
-  private pending: PendingEvent<E>[] = [];
   private push(emitter: EventEmitter, event: string, arg: E) {
     this.pending.push({ emitter, event, arg });
     this.refresh();
   }
 
-  private timeout?: NodeJS.Timeout;
   private refresh() {
     if (this.timeout) clearTimeout(this.timeout);
     this.timeout = setTimeout(this.emitPending.bind(this), this.maxMsBetween);
@@ -40,7 +43,11 @@ export default class EventAggregator<E> {
     this.pending = [];
   }
 
-  attach(emitter: EventEmitter, event: string): void {
-    emitter.on(event, (...args) => this.push(emitter, event, args[0]));
+  attach(emitter: EventEmitter, event: string): CancelFn {
+    const listenerFn = (...args: any[]) => {
+      this.push(emitter, event, args[0]);
+    };
+    emitter.addListener(event, listenerFn);
+    return () => emitter.removeListener(event, listenerFn);
   }
 }
