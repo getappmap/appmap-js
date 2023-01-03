c221bb45842b162cff0f324fa3c11a4a45c0e864
Adam Trotta <ahtrotta@gmail.com>
2023-01-03 13:34:52 -0500

    fix: correctly show events with multiple findings
    Events with multiple associated findings were displaying only one
    of the findings in the details panel because findings didn't have
    an `id` property.

--- packages/components/src/lib/finding.js
+++ packages/components/src/lib/finding.js
@@ -3,6 +3,7 @@ import { CodeObjectType } from '@appland/models';
 export default function toListItem(resolvedFinding) {
   return {
     type: CodeObjectType.ANALYSIS_FINDING,
+    id: resolvedFinding.finding.hash_v2 || resolvedFinding.finding.hash,
     resolvedFinding,
     name: `${resolvedFinding.rule.title}: ${resolvedFinding.finding.message}`,
     shortName: resolvedFinding.rule.title,
