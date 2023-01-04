0082db3d7cdc19628f5157449c765ad0d1c4f8dc
Kevin Gilpin <kgilpin@gmail.com>
2023-01-04 12:15:39 -0500

    refactor: Reorganize sequence diagram cmd

--- packages/cli/src/cmds/sequenceDiagramDiff.ts
+++ packages/cli/src/cmds/sequenceDiagramDiff.ts
@@ -1,31 +1,20 @@
 import { queue } from 'async';
 import { existsSync, statSync } from 'fs';
-import { mkdir, readFile, writeFile } from 'fs/promises';
+import { mkdir, writeFile } from 'fs/promises';
 import { glob } from 'glob';
 import { promisify } from 'util';
 import yargs from 'yargs';
 import { handleWorkingDirectory } from '../lib/handleWorkingDirectory';
 import {
-  Actor,
-  DiffOptions,
-  MoveType,
   format as formatDiagram,
   Formatters,
   Diagram,
-  diff,
-  buildDiffDiagram,
-  setParent,
-  actionActors,
-  isFunction,
   FormatType,
-  buildDiagram,
-  Specification,
-  Action,
-  nodeName,
 } from '@appland/sequence-diagram';
 import { verbose } from '../utils';
 import { join } from 'path';
-import { buildAppMap } from '@appland/models';
+import { DiffDiagrams } from '../sequenceDiagramDiff/DiffDiagrams';
+import { readDiagramFile } from './sequenceDiagram/readDiagramFile';
 
 export const command = 'sequence-diagram-diff base-diagram head-diagram';
 export const describe = 'Diff sequence diagrams that are represented as JSON';
@@ -64,130 +53,6 @@ export const builder = (args: yargs.Argv) => {
   return args.strict();
 };
 
-type SerializedAction = {
-  caller?: string | Actor;
-  callee?: string | Actor;
-  children?: SerializedAction[];
-};
-
-class IncludeFilter {
-  expressions: RegExp[] = [];
-
-  addExpression(expression: string) {
-    this.expressions.push(new RegExp(expression));
-  }
-
-  test(action: Action): boolean {
-    if (this.expressions.length === 0) return true;
-
-    let ancestor: Action | undefined = action;
-    while (ancestor) {
-      const actionName = buildActionName(ancestor);
-      if (actionName && this.expressions.find((expr) => expr.test(actionName))) {
-        return true;
-      }
-      ancestor = ancestor.parent;
-    }
-
-    return false;
-  }
-}
-
-class ExcludeFilter {
-  expressions: RegExp[] = [];
-
-  addExpression(expression: string) {
-    this.expressions.push(new RegExp(expression));
-  }
-
-  test(action: Action): boolean {
-    const actionName = buildActionName(action);
-    if (!actionName) return true;
-
-    return this.expressions.find((expr) => expr.test(actionName)) === undefined;
-  }
-}
-
-class DiffDiagrams {
-  includeFilter: IncludeFilter;
-  excludeFilter: ExcludeFilter;
-
-  constructor() {
-    this.includeFilter = new IncludeFilter();
-    this.excludeFilter = new ExcludeFilter();
-  }
-
-  include(expr: string): void {
-    this.includeFilter.addExpression(expr);
-  }
-
-  exclude(expr: string): void {
-    this.excludeFilter.addExpression(expr);
-  }
-
-  diff(base: Diagram, head: Diagram): Diagram | undefined {
-    return this.diffDiagrams(this.filterDiagram(base), this.filterDiagram(head));
-  }
-
-  protected filterDiagram(diagram: Diagram): Diagram {
-    const filterActions = (actions: Action[]): Action[] => {
-      const result = actions.filter(
-        (action) => this.includeFilter.test(action) && this.excludeFilter.test(action)
-      );
-      result.forEach((action) => {
-        action.children = filterActions(action.children);
-      });
-      return result;
-    };
-
-    diagram.rootActions = filterActions(diagram.rootActions);
-    return diagram;
-  }
-
-  protected diffDiagrams(base: Diagram, head: Diagram): Diagram | undefined {
-    const result = diff(base, head, {} as DiffOptions);
-
-    const changes = result.moves.filter((move) => move.moveType !== MoveType.AdvanceBoth);
-    if (changes.length === 0) {
-      return;
-    }
-
-    return buildDiffDiagram(result);
-  }
-}
-
-function buildSequenceDiagramFromAppMap(fileName: string, appMapData: any): Diagram {
-  const appmap = buildAppMap(appMapData).build();
-  return buildDiagram(fileName, appmap, Specification.build(appmap, {}));
-}
-
-async function readDiagramFile(fileName: string): Promise<Diagram> {
-  const jsonData = JSON.parse(await readFile(fileName, 'utf-8')) as any;
-
-  let diagram: Diagram;
-  if (jsonData.actors && jsonData.rootActions) {
-    diagram = jsonData as Diagram;
-  } else if (jsonData.classMap && jsonData.events && jsonData.metadata) {
-    diagram = buildSequenceDiagramFromAppMap(fileName, jsonData);
-  } else {
-    throw new Error(`File '${fileName}' must be AppMap or sequence diagram JSON`);
-  }
-
-  const actors = new Map<string, Actor>();
-  diagram.actors.forEach((actor) => actors.set(actor.id, actor));
-
-  const resolveActors = (action: SerializedAction): void => {
-    if (action.caller) action.caller = actors.get(action.caller as string);
-    if (action.callee) action.callee = actors.get(action.callee as string);
-    if (action.children) action.children.forEach((child) => resolveActors(child));
-  };
-
-  diagram.rootActions.forEach((action) => setParent(action));
-  diagram.rootActions.forEach((action) => resolveActors(action as SerializedAction));
-
-  return diagram;
-}
-
 async function printDiff(
   diagram: Diagram,
   format: FormatType,
@@ -202,14 +67,6 @@ async function printDiff(
   console.log(`Printed diagram ${outputPath}`);
 
   return diagram;
-}
-
-function buildActionName(action: Action): string | undefined {
-  const actor: Actor | undefined = actionActors(action)[1];
-  if (!actor) return;
-
-  const separator = isFunction(action) ? (action.static ? '.' : '#') : '#';
-  return [actor.id, nodeName(action).replace(/\n/g, ' ')].join(separator);
 }
 
 export const handler = async (argv: any) => {
---
+++ packages/cli/src/sequenceDiagramDiff/DiffDiagrams.ts
@@ -0,0 +1,95 @@
+import {
+  DiffOptions,
+  MoveType,
+  Diagram,
+  diff,
+  buildDiffDiagram,
+  Action,
+} from '@appland/sequence-diagram';
+import { buildActionName } from './buildActionName';
+
+class IncludeFilter {
+  expressions: RegExp[] = [];
+
+  addExpression(expression: string) {
+    this.expressions.push(new RegExp(expression));
+  }
+
+  test(action: Action): boolean {
+    if (this.expressions.length === 0) return true;
+
+    let ancestor: Action | undefined = action;
+    while (ancestor) {
+      const actionName = buildActionName(ancestor);
+      if (actionName && this.expressions.find((expr) => expr.test(actionName))) {
+        return true;
+      }
+      ancestor = ancestor.parent;
+    }
+
+    return false;
+  }
+}
+
+export class ExcludeFilter {
+  expressions: RegExp[] = [];
+
+  addExpression(expression: string) {
+    this.expressions.push(new RegExp(expression));
+  }
+
+  test(action: Action): boolean {
+    const actionName = buildActionName(action);
+    if (!actionName) return true;
+
+    return this.expressions.find((expr) => expr.test(actionName)) === undefined;
+  }
+}
+
+export class DiffDiagrams {
+  includeFilter: IncludeFilter;
+  excludeFilter: ExcludeFilter;
+
+  constructor() {
+    this.includeFilter = new IncludeFilter();
+    this.excludeFilter = new ExcludeFilter();
+  }
+
+  include(expr: string): void {
+    this.includeFilter.addExpression(expr);
+  }
+
+  exclude(expr: string): void {
+    this.excludeFilter.addExpression(expr);
+  }
+
+  diff(base: Diagram, head: Diagram): Diagram | undefined {
+    return this.diffDiagrams(this.filterDiagram(base), this.filterDiagram(head));
+  }
+
+  protected filterDiagram(diagram: Diagram): Diagram {
+    const filterActions = (actions: Action[]): Action[] => {
+      const result = actions.filter(
+        (action) => this.includeFilter.test(action) && this.excludeFilter.test(action)
+      );
+      result.forEach((action) => {
+        action.children = filterActions(action.children);
+      });
+      return result;
+    };
+
+    diagram.rootActions = filterActions(diagram.rootActions);
+    return diagram;
+  }
+
+  protected diffDiagrams(base: Diagram, head: Diagram): Diagram | undefined {
+    const result = diff(base, head, {} as DiffOptions);
+
+    const changes = result.moves.filter((move) => move.moveType !== MoveType.AdvanceBoth);
+    if (changes.length === 0) {
+      return;
+    }
+
+    return buildDiffDiagram(result);
+  }
+}
