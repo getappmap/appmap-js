83ab8bdab586a2b0d89732f4fdd704956eb2655c
Kevin Gilpin <kgilpin@gmail.com>
2023-01-04 16:23:04 -0500

    fix: Fix accidental call to verbose(b)

--- packages/cli/src/fingerprint/index.js
+++ packages/cli/src/fingerprint/index.js
@@ -1,13 +1,8 @@
-const { verbose } = require('../utils');
 const { algorithms, canonicalize } = require('./canonicalize');
 const FingerprintDirectoryCommand = require('./fingerprintDirectoryCommand');
 const FingerprintWatchCommand = require('./fingerprintWatchCommand').default;
 
 async function fingerprintDirectory(dir, watch = false) {
-  if (verbose) {
-    verbose(true);
-  }
-
   let cmd;
   if (watch) {
     cmd = new FingerprintWatchCommand(dir);
