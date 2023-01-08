dd55fbb858a54aeb357b3a11d8eac91802314d6f
Rafał Rzepecki <rafal@app.land>
2023-01-08 14:13:11 -0500

    test: Refactor the data-driven test framework a bit further

--- packages/scanner/test/scanner/updateInGetRequest.spec.ts
+++ packages/scanner/test/scanner/updateInGetRequest.spec.ts
@@ -1,28 +1,20 @@
 import updateInGetRequest from '../../src/rules/updateInGetRequest';
-import testRule from '../support/testRule';
+import describeRule from '../support/describeRule';
 
-describe(updateInGetRequest.title, () => {
-  it('correctly recognizes an update in a GET request', async () => {
-    const findings = await testRule(updateInGetRequest, [
-      { http: 'GET /foobar' },
-      [{ sql: 'UPDATE things SET col=4' }],
-    ]);
-    expect(findings[0].checkId).toEqual(updateInGetRequest.id);
-  });
-
-  it('correctly recognizes an update deeper in a GET request', async () => {
-    const findings = await testRule(updateInGetRequest, [
-      { http: 'GET /foobar' },
-      [[{ fn: 'Foo.check' }, [{ sql: 'UPDATE things SET col=4' }]]],
-    ]);
-    expect(findings[0].checkId).toEqual(updateInGetRequest.id);
-  });
-
-  it('does not flag an update in a POST request', async () => {
-    const findings = await testRule(updateInGetRequest, [
-      { http: 'POST /foobar' },
-      [{ sql: 'UPDATE things SET col=4' }],
-    ]);
-    expect(findings.length).toEqual(0);
-  });
-});
+describeRule(updateInGetRequest, [
+  [
+    'correctly recognizes an update in a GET request',
+    [{ http: 'GET /foobar' }, [{ sql: 'UPDATE things SET col=4' }]],
+    1,
+  ],
+  [
+    'correctly recognizes an update deeper in a GET request',
+    [{ http: 'GET /foobar' }, [[{ fn: 'Foo.check' }, [{ sql: 'UPDATE things SET col=4' }]]]],
+    1,
+  ],
+  [
+    'does not flag an update in a POST request',
+    [{ http: 'POST /foobar' }, [{ sql: 'UPDATE things SET col=4' }]],
+    0,
+  ],
+]);
