e5475017b29847f06df0c0429e7d87b489404411
Kevin Gilpin <kgilpin@gmail.com>
2023-01-23 13:50:41 -0500

    feat: Seq diagram diff include and exclude
    Support --include and --exclude options for diffing seq diagram

--- packages/cli/src/cmds/sequenceDiagramDiff.ts
+++ packages/cli/src/cmds/sequenceDiagramDiff.ts
@@ -15,10 +15,17 @@ import {
   diff,
   buildDiffDiagram,
   setParent,
+  actionActors,
+  isFunction,
   FormatType,
+  buildDiagram,
+  Specification,
+  Action,
+  nodeName,
 } from '@appland/sequence-diagram';
 import { verbose } from '../utils';
 import { join } from 'path';
+import { buildAppMap } from '@appland/models';
 
 export const command = 'sequence-diagram-diff base-diagram head-diagram';
 export const describe = 'Diff sequence diagrams that are represented as JSON';
@@ -42,14 +49,20 @@ export const builder = (args: yargs.Argv) => {
   });
   args.option('format', {
     describe: 'output format',
-    choices: ['plantuml', 'json'],
+    alias: 'f',
+    choices: ['plantuml', 'json', 'text'],
     default: 'plantuml',
+  });
+
+  args.option('include', {
+    describe: 'code objects to include in the diagram (inclusive of descendants)',
+  });
+  args.option('exclude', {
+    describe: 'code objects to exclude from the diagram',
   });
 
   return args.strict();
 };
-
-type Formatter = (diagram: Diagram, source: string) => string;
 
 type SerializedAction = {
   caller?: string | Actor;
@@ -57,8 +70,108 @@ type SerializedAction = {
   children?: SerializedAction[];
 };
 
+class IncludeFilter {
+  expressions: RegExp[] = [];
+
+  addExpression(expression: string) {
+    this.expressions.push(new RegExp(expression));
+  }
+
+  test(action: Action): boolean {
+    if (this.expressions.length === 0) return true;
+
+    let ancestor: Action | undefined = action;
+    while (ancestor) {
+      const actionName = buildActionName(ancestor);
+      if (actionName && this.expressions.find((expr) => expr.test(actionName))) {
+        return true;
+      }
+      ancestor = ancestor.parent;
+    }
+
+    return false;
+  }
+}
+
+class ExcludeFilter {
+  expressions: RegExp[] = [];
+
+  addExpression(expression: string) {
+    this.expressions.push(new RegExp(expression));
+  }
+
+  test(action: Action): boolean {
+    const actionName = buildActionName(action);
+    if (!actionName) return true;
+
+    return this.expressions.find((expr) => expr.test(actionName)) === undefined;
+  }
+}
+
+class DiffDiagrams {
+  includeFilter: IncludeFilter;
+  excludeFilter: ExcludeFilter;
+
+  constructor() {
+    this.includeFilter = new IncludeFilter();
+    this.excludeFilter = new ExcludeFilter();
+  }
+
+  include(expr: string): void {
+    this.includeFilter.addExpression(expr);
+  }
+
+  exclude(expr: string): void {
+    this.excludeFilter.addExpression(expr);
+  }
+
+  diff(base: Diagram, head: Diagram): Diagram | undefined {
+    return this.diffDiagrams(this.filterDiagram(base), this.filterDiagram(head));
+  }
+
+  protected filterDiagram(diagram: Diagram): Diagram {
+    const filterActions = (actions: Action[]): Action[] => {
+      const result = actions.filter(
+        (action) => this.includeFilter.test(action) && this.excludeFilter.test(action)
+      );
+      result.forEach((action) => {
+        action.children = filterActions(action.children);
+      });
+      return result;
+    };
+
+    diagram.rootActions = filterActions(diagram.rootActions);
+    return diagram;
+  }
+
+  protected diffDiagrams(base: Diagram, head: Diagram): Diagram | undefined {
+    const result = diff(base, head, {} as DiffOptions);
+
+    const changes = result.moves.filter((move) => move.moveType !== MoveType.AdvanceBoth);
+    if (changes.length === 0) {
+      return;
+    }
+
+    return buildDiffDiagram(result);
+  }
+}
+
+function buildSequenceDiagramFromAppMap(fileName: string, appMapData: any): Diagram {
+  const appmap = buildAppMap(appMapData).build();
+  return buildDiagram(fileName, appmap, Specification.build(appmap, {}));
+}
+
 async function readDiagramFile(fileName: string): Promise<Diagram> {
-  const diagram = JSON.parse(await readFile(fileName, 'utf-8')) as Diagram;
+  const jsonData = JSON.parse(await readFile(fileName, 'utf-8')) as any;
+
+  let diagram: Diagram;
+  if (jsonData.actors && jsonData.rootActions) {
+    diagram = jsonData as Diagram;
+  } else if (jsonData.classMap && jsonData.events && jsonData.metadata) {
+    diagram = buildSequenceDiagramFromAppMap(fileName, jsonData);
+  } else {
+    throw new Error(`File '${fileName}' must be AppMap or sequence diagram JSON`);
+  }
 
   const actors = new Map<string, Actor>();
   diagram.actors.forEach((actor) => actors.set(actor.id, actor));
@@ -75,34 +188,12 @@ async function readDiagramFile(fileName: string): Promise<Diagram> {
   return diagram;
 }
 
-async function performDiff(
+async function printDiff(
+  diagram: Diagram,
   format: FormatType,
   outputFileName: string,
-  baseFileName: string,
-  base: Diagram,
-  head: Diagram,
-  headFileName?: string
+  description: string
 ): Promise<Diagram | undefined> {
-  const result = diff(base, head, {} as DiffOptions);
-
-  const changes = result.moves.filter((move) => move.moveType !== MoveType.AdvanceBoth);
-  if (changes.length === 0) {
-    if (headFileName) {
-      console.log(`${baseFileName} and ${headFileName} are identical`);
-    } else {
-      console.log(`${baseFileName} are identical`);
-    }
-    return;
-  }
-
-  const diagram = buildDiffDiagram(result);
-  let description: string;
-  if (headFileName) {
-    description = `Diff ${baseFileName} with ${headFileName}`;
-  } else {
-    description = `Diff ${baseFileName}`;
-  }
-
   const template = formatDiagram(format, diagram, description);
 
   const outputPath = [outputFileName, template.extension].join('');
@@ -111,6 +202,14 @@ async function performDiff(
   console.log(`Printed diagram ${outputPath}`);
 
   return diagram;
+}
+
+function buildActionName(action: Action): string | undefined {
+  const actor: Actor | undefined = actionActors(action)[1];
+  if (!actor) return;
+
+  const separator = isFunction(action) ? (action.static ? '.' : '#') : '#';
+  return [actor.id, nodeName(action).replace(/\n/g, ' ')].join(separator);
 }
 
 export const handler = async (argv: any) => {
@@ -127,6 +226,17 @@ export const handler = async (argv: any) => {
 
   const { baseDiagram: baseDiagramFile, headDiagram: headDiagramFile } = argv;
 
+  const diffDiagrams = new DiffDiagrams();
+
+  if (argv.include)
+    (Array.isArray(argv.include) ? argv.include : [argv.include]).forEach((expr: string) =>
+      diffDiagrams.include(expr)
+    );
+  if (argv.exclude)
+    (Array.isArray(argv.exclude) ? argv.exclude : [argv.exclude]).forEach((expr: string) =>
+      diffDiagrams.exclude(expr)
+    );
+
   [baseDiagramFile, headDiagramFile].forEach((fileName) => {
     if (!existsSync(fileName)) throw new Error(`${fileName} does not exist`);
   });
@@ -135,13 +245,17 @@ export const handler = async (argv: any) => {
     const baseDiagram = await readDiagramFile(baseDiagramFile);
     const headDiagram = await readDiagramFile(headDiagramFile);
 
-    performDiff(
+    const diffDiagram = diffDiagrams.diff(baseDiagram, headDiagram);
+    if (!diffDiagram) {
+      console.log(`${baseDiagramFile} and ${headDiagramFile} are identical`);
+      return;
+    }
+
+    printDiff(
+      diffDiagram,
       argv.format,
       join(argv.outputDir, 'diff'),
-      baseDiagramFile,
-      baseDiagram,
-      headDiagram,
-      headDiagramFile
+      `Diff ${baseDiagramFile} with ${headDiagramFile}`
     );
   };
 
@@ -157,7 +271,7 @@ export const handler = async (argv: any) => {
     await Promise.all(
       [...diagramData.keys()].map(async (dirName) => {
         const data = diagramData.get(dirName)!;
-        await promisify(glob)(`${dirName}/**/*.json`).then(async (matches) => {
+        await promisify(glob)(`${dirName}/**/*.sequence.json`).then(async (matches) => {
           const loader = queue(async (matchName: string) => {
             const diagram = await readDiagramFile(matchName);
             const relativePath = matchName.slice(dirName.length + 1);
@@ -176,22 +290,25 @@ export const handler = async (argv: any) => {
       diagramNames.add(fileName);
 
     for (const fileName of diagramNames) {
-      if (diagramData.get(baseDiagramFile)!.diagrams.get(fileName) === undefined) {
+      const baseDiagram = diagramData.get(baseDiagramFile)!.diagrams.get(fileName);
+      if (baseDiagram === undefined) {
         console.log(`Diagram ${fileName} exists only in ${headDiagramFile}`);
         continue;
       }
-      if (diagramData.get(headDiagramFile)!.diagrams.get(fileName) === undefined) {
+
+      const headDiagram = diagramData.get(headDiagramFile)!.diagrams.get(fileName);
+      if (headDiagram === undefined) {
         console.log(`Diagram ${fileName} exists only in ${baseDiagramFile}`);
         continue;
       }
 
-      performDiff(
-        argv.format,
-        [fileName, 'diff'].join('.'),
-        fileName,
-        diagramData.get(baseDiagramFile)!.diagrams.get(fileName)!,
-        diagramData.get(headDiagramFile)!.diagrams.get(fileName)!
-      );
+      const diffDiagram = diffDiagrams.diff(baseDiagram, headDiagram);
+      if (!diffDiagram) {
+        console.log(`${fileName} is identical`);
+        continue;
+      }
+
+      printDiff(diffDiagram, argv.format, [fileName, 'diff'].join('.'), `Diff ${fileName}`);
     }
   };
 
