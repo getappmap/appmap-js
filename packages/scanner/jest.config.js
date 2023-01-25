609fdbd42c948d262d55e32c9e682c593bb63fc9
Dustin Byrne <dustinsbyrne@gmail.com>
2023-01-25 13:49:34 -0500

    chore: Don't run tests in `built` by default
    When running locally, tests can be run twice (once in `tests`, again in
    `built`). Tests in `built` aren't guaranteed to pass.

--- packages/scanner/jest.config.js
+++ packages/scanner/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: parseInt(process.env.TEST_TIMEOUT, 10) || 5000,
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.ts?$',
   setupFilesAfterEnv: ['./test/setup.ts'],
 };
