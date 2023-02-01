efd5f31594f20f01db812704b478fb30dfd1a74e
Kevin Gilpin <kgilpin@gmail.com>
2023-02-01 16:55:47 -0500

    feat: Proper shape and description for Loop

--- packages/components/src/components/sequence/LoopAction.vue
+++ packages/components/src/components/sequence/LoopAction.vue
@@ -6,18 +6,32 @@
       'grid-row': gridRows,
     }"
   >
-    <div class="label">Loop</div>
+    <div class="label-container">
+      <div class="label">
+        Loop
+        <div class="rhs-effect">
+          <SequenceGroupLabelRHS />
+        </div>
+      </div>
+      <div class="description">
+        [
+        <div class="count">{{ actionSpec.action.count }} times</div>
+        <div class="elapsed">{{ actionSpec.elapsedTimeMs }}ms</div>
+        ]
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 // @ts-nocheck
 import { ActionSpec } from './ActionSpec';
+import SequenceGroupLabelRHS from '@/assets/sequence-group-label-rhs.svg';
 
 export default {
   name: 'v-sequence-loop',
 
-  components: {},
+  components: { SequenceGroupLabelRHS },
 
   props: {
     actionSpec: {
@@ -48,11 +62,41 @@ export default {
   margin: 15px -20px 10px -20px;
   border: 1px solid magenta;
   position: relative;
+  z-index: 3;
+  display: inline-block;
+  font-weight: bold;
 
-  .label {
-    padding: 5px;
-    background: magenta;
-    width: fit-content;
+  .label-container {
+    white-space: nowrap;
+    background-color: rgba(200, 200, 200, 0.25);
+
+    div {
+      display: inline-block;
+    }
+
+    .label {
+      padding: 5px;
+      height: 28px;
+      background-color: #6fddd6;
+      color: black;
+      border-bottom: 2px solid magenta;
+      position: relative;
+
+      .rhs-effect {
+        position: absolute;
+        top: 0;
+        left: 100%;
+      }
+    }
+
+    .description {
+      font-size: 9pt;
+      margin-left: 20px;
+
+      .elapsed {
+        color: $sequence-elapsed-time-color;
+      }
+    }
   }
 }
 </style>
