92cda57ae085b41d0c221a5324849fbb2ae107ea
Kevin Gilpin <kgilpin@gmail.com>
2023-02-07 16:50:14 -0500

    feat: Move AppMapFilter to @appland/models

--- packages/models/src/index.js
+++ packages/models/src/index.js
@@ -12,4 +12,5 @@ export { CodeObjectType } from './codeObjectType';
 export { default as analyzeSQL, abstractSqlAstJSON } from './sql/analyze';
 export { default as normalizeSQL } from './sql/normalize';
 export { default as parseSQL } from './sql/parse';
+export { default as AppMapFilter } from './appMapFilter';
 export * from './util';
