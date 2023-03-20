5b0fc1643baf72a76f0009967caa99c9d91455bd
Adam Trotta <ahtrotta@gmail.com>
2023-03-20 15:43:36 -0400

    feat: use base64url-encoded filter state for pruning

--- packages/cli/src/cmds/prune/pruneAppMap.ts
+++ packages/cli/src/cmds/prune/pruneAppMap.ts
@@ -1,4 +1,4 @@
-import { AppMapFilter, buildAppMap } from '@appland/models';
+import { AppMapFilter, buildAppMap, deserializeAppmapState } from '@appland/models';
 
 // Simplified AppMap interface, just for pruning
 export interface AppMap {
@@ -10,10 +10,15 @@ export const pruneAppMap = (appMap: AppMap, size: number): any => {
   return buildAppMap().source(appMap).prune(size).normalize().build();
 };
 
-export const removeEventsByFqid = (appMap: AppMap, fqids: Array<any>): any => {
+export const pruneWithFilter = (appMap: AppMap, serializedFilter: string): any => {
   const fullMap = buildAppMap().source(appMap).normalize().build();
+
+  const filterOrState = deserializeAppmapState(serializedFilter);
+  const filters = 'filters' in filterOrState ? filterOrState.filters : filterOrState;
+
   const appmapFilter = new AppMapFilter();
-  appmapFilter.declutter.hideName.on = true;
-  appmapFilter.declutter.hideName.names = fqids;
+  appmapFilter.declutter.limitRootEvents.on = false;
+  appmapFilter.declutter.hideMediaRequests.on = false;
+  appmapFilter.apply(filters);
   return appmapFilter.filter(fullMap, []);
 };
