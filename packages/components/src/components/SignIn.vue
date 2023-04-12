03a3c89ce2bc13b6e8732d1bb6955417494594ad
sintaxera <typaulhus@gmail.com>
2023-04-12 17:02:34 -0400

    feat: update sidebar sign-in messaging
    Co-authored-by: Adam Trotta <ahtrotta@gmail.com>
    Co-authored-by: Daniel Warner <dan@danielwarner.net>

--- packages/components/src/components/SignIn.vue
+++ packages/components/src/components/SignIn.vue
@@ -2,14 +2,36 @@
   <div class="signin-sidebar">
     <AppMapLogo width="120" />
     <div class="content-wrap">
-      <h1 data-cy="title">Activate</h1>
-      <p>To begin using AppMap for free, please sign in using your GitHub or GitLab account.</p>
-      <a href="/" class="btn btn-primary" data-cy="sign-in-button" @click="signIn">Get Started</a>
+      <div class="sidebar-title">
+        <h1 data-cy="title">Sign in</h1>
+        <p>
+          Start visualizing your runtime code, identifying problems, and finding solutions. Sign in
+          via GitHub or GitLab to start automatically generating:
+        </p>
+      </div>
+
+      <ul class="signin-ctas">
+        <li><a href="https://appmap.io/docs/diagrams/sequence-diagrams">Sequence diagrams</a></li>
+        <li>
+          <a
+            href="https://appmap.io/docs/diagrams/how-to-use-appmaps.html#view-and-interact-with-the-trace-diagram"
+            >Trace views</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://appmap.io/docs/diagrams/how-to-use-appmaps.html#view-and-interact-with-the-dependency-map"
+            >Dependency maps</a
+          >
+        </li>
+        <li><a href="https://appmap.io/docs/openapi">OpenAPI definitions</a></li>
+      </ul>
+      <a href="/" class="btn btn-primary" data-cy="sign-in-button" @click="signIn">Sign in</a>
       <div class="your-data">
         <shieldIcon />
         <p data-cy="your-data-text">
-          Authentication with GitHub or GitLab is used solely for verification purposes. AppMap will
-          not be granted access to source code.
+          Authentication with GitHub or GitLab is used solely for issuing a license. AppMap runs in
+          your code editor, so your AppMaps and your source code stay on your machine.
         </p>
       </div>
     </div>
@@ -49,8 +71,10 @@ export default {
     display: flex;
     flex-direction: column;
     align-content: center;
-    gap: 1rem;
-    h1,
+    gap: 2rem;
+    h1 {
+      margin: 0 0 0.75rem 0;
+    }
     p {
       margin: 0;
     }
@@ -64,11 +88,39 @@ export default {
     text-decoration: none;
     text-align: center;
     &.btn-primary {
-      background-color: desaturate($powderblue, 20);
+      background-color: desaturate($powderblue, 20); //$hotpink;
       font-weight: 600;
       margin: 0 1.25rem;
       &:hover {
         background-color: darken($powderblue, 15);
+      }
+    }
+  }
+}
+
+.signin-ctas {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  list-style-type: none;
+  font-family: $appland-text-font-family;
+  padding: 0;
+  margin: 0;
+  li {
+    background-color: #091118;
+    border-radius: 0.25rem;
+    padding: 0.8rem 1rem;
+    font-weight: 600;
+    transition: $transition;
+    a {
+      color: desaturate($powderblue, 30);
+      text-decoration: none;
+      display: flex;
+    }
+    &:hover {
+      background-color: #12222f;
+      a {
+        text-decoration: underline;
       }
     }
   }
@@ -83,8 +135,7 @@ export default {
   border: 1px solid $gray3;
   border-radius: 0.5rem;
   font-size: 0.9rem;
-  line-height: 1.2rem;
-  margin-top: 0.5rem;
+  line-height: 1.4rem;
   svg {
     width: 100%;
     max-width: 33px;
