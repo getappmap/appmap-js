a87219038c00227b5b7ce9bf1eb4c8fd5f5e7248
Kevin Gilpin <kgilpin@gmail.com>
2023-04-27 09:42:38 -0400

    feat: Write sql parse errors to a file
    This should be safe to use in the browser, where it will continue
    to print to console.debug

--- packages/models/src/sql/nopErrorCallback.js
+++ packages/models/src/sql/nopErrorCallback.js
@@ -1,8 +1,42 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const SqlErrors = new Set();
+const SqlParseErrorFileName = 'sql_parse_error.txt';
+let SqlParseErrorFile;
+let writeError;
+
+async function writeErrorToFile(error) {
+  const { open } = require('fs/promises');
+
+  if (!SqlParseErrorFile) SqlParseErrorFile = await open(SqlParseErrorFileName, 'w');
+
+  SqlParseErrorFile.write([error.toString(), ''].join('\n'), (err) => {
+    if (err) console.debug(err);
+  });
+}
+
+function writeErrorToConsole(error) {
+  console.debug(error);
+}
+
+function isProcessAvailable() {
+  return typeof process !== 'undefined' && process && typeof process.on === 'function';
+}
+
+if (isProcessAvailable()) {
+  process.on('exit', () => {
+    if (SqlParseErrorFile) SqlParseErrorFile.close();
+
+    SqlParseErrorFile = undefined;
+  });
+  writeError = writeErrorToFile;
+} else {
+  writeError = writeErrorToConsole;
+}
 
 export function nopErrorCallback(parseError) {
   if (!SqlErrors.has(parseError.sql)) {
-    console.debug(`Unable to parse SQL: ${parseError}`);
+    writeError(parseError);
     SqlErrors.add(parseError.sql);
   }
 }
