421e238f5718bff759f1d68f4dffa7e2486644ad
Rafał Rzepecki <divided.mind@gmail.com>
2023-06-04 17:37:08 +0200

    test: Use standard Jest method of suppressing log messages
    There's no need to use Sinon to mock console; Jest already controls
    the console and provides a way to silence the messages in tests.
    
    This change was needed also because using Sinon to stub console like
    that was problematic with AppMap. Using Jest's built-in spies works
    perfectly fine. It's also cleaner IMO.

--- packages/cli/tests/unit/fingerprint/fingerprintQueue.spec.ts
+++ packages/cli/tests/unit/fingerprint/fingerprintQueue.spec.ts
@@ -1,19 +1,17 @@
 import FingerprintQueue from '../../../src/fingerprint/fingerprintQueue';
-import sinon, { SinonStub } from 'sinon';
 import { verbose } from '../../../src/utils';
 
 describe(FingerprintQueue, () => {
   beforeEach(() => verbose(process.env.DEBUG === 'true'));
-  afterEach(() => {
-    sinon.restore();
-  });
 
   it('gracefully handles files which cannot be read', async () => {
+    const logSpy = jest.spyOn(console, 'log');
+
     const queue = new FingerprintQueue();
     queue.push('missing-file.appmap.json');
     await queue.process();
 
-    expect((console.log as SinonStub).callCount).toBe(1);
-    expect((console.log as SinonStub).getCall(0).args[0]).toMatch(/does not exist/);
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy.mock.calls[0][0]).toMatch(/does not exist/);
   });
 });
