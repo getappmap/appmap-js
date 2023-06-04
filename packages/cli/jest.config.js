9dc992c740d175f538fc30df9a4f42cc8cd6a1c6
Rafał Rzepecki <divided.mind@gmail.com>
2023-06-04 17:37:08 +0200

    test: Use standard Jest method of suppressing log messages
    There's no need to use Sinon to mock console; Jest already controls
    the console and provides a way to silence the messages in tests.
    
    This change was needed also because using Sinon to stub console like
    that was problematic with AppMap. Using Jest's built-in spies works
    perfectly fine. It's also cleaner IMO.

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  setupFilesAfterEnv: ['./jest-setup'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: parseInt(process.env.TEST_TIMEOUT, 10) || 5000,
+  silent: true,
+  restoreMocks: true,
 };
