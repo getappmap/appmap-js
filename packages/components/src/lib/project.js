fcbbba49d2153c986ddc230e4f1efe0c6680ba74
Kevin Gilpin <kgilpin@gmail.com>
2023-06-06 14:42:49 -0400

    feat: Update install instructions
    Provide an overview of install instructions for each language + ide.
    Provide manual instructions for all platforms except Java.
    Provide a Next button below each instruction set (automated and manual).
    Explain the advantages of the installer.

--- packages/components/src/lib/project.js
+++ packages/components/src/lib/project.js
@@ -1,7 +1,7 @@
 export function isFeatureSupported(feature) {
-  return feature && feature.score > 0;
+  return feature && (feature.score || 0) > 0;
 }
 
 export function isProjectSupported(project) {
-  return project && project.score > 0;
+  return project && (project.score || 0) > 0;
 }
