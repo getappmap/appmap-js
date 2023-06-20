a83ecab851d38f40022371b63223f0fd01741d5c
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-06-20 15:34:56 +0200

    refactor: re-implement zoom slider as vue component
    The original implementation of the zoom slider is:
    https://github.com/getappmap/appmap-js/blob/main/packages/diagrams/src/helpers/container/zoom.js
    
    The problem with reusing the original implementation is that it is
    entangled with functionalities like drag and drop panel, which we don't
    need here. Also it is not a vue component, so it is less consistent with
    the packages/components codebase. In the future this component could be
    reused to implement the zoom slider of the dependency diagram and trace
    diagram.

---
+++ packages/components/src/components/Slider.vue
@@ -0,0 +1,136 @@
+<!--
+  This is re-implementation of:
+  https://github.com/getappmap/appmap-js/blob/main/packages/diagrams/src/helpers/container/zoom.js
+  The problem with reusing the original implementation is that it is tangled with functionalities
+  like drag and drop panel, which we don't need here. Also it is not a vue component, so it is less
+  consistent with the packages/components codebase. In the future this component could be reused to
+  implement the zoom slider of the dependency diagram and trace diagram.
+-->
+
+<template>
+  <div class="slider">
+    <button class="button" @click="increase">&plus;</button>
+    <div ref="handle" class="handle" @click="set">
+      <div class="bar"></div>
+      <div class="grab" @mousedown="startDragging" :style="grabStyle"></div>
+    </div>
+    <button class="button" @click="decrease">&minus;</button>
+  </div>
+</template>
+
+<script>
+const clamp = (val, { min, max }) => Math.min(Math.max(val, min), max);
+export default {
+  name: 'v-slider',
+  emits: ['slide'],
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+    step: {
+      type: Number,
+      default: 0.1,
+    },
+  },
+  data() {
+    return {
+      dragging: false,
+    };
+  },
+  computed: {
+    grabStyle() {
+      return {
+        bottom: `${100 * this.value}%`,
+      };
+    },
+  },
+  methods: {
+    set(event) {
+      const rect = this.$refs.handle.getBoundingClientRect();
+      this.$emit(
+        'slide',
+        1 - (clamp(event.clientY, { min: rect.top, max: rect.bottom }) - rect.top) / rect.height
+      );
+    },
+    increase() {
+      this.$emit('slide', Math.min(1, this.value + this.step));
+    },
+    decrease() {
+      this.$emit('slide', Math.max(0, this.value - this.step));
+    },
+    startDragging() {
+      this.dragging = true;
+    },
+    drag(event) {
+      if (this.dragging) {
+        this.set(event);
+      }
+    },
+    stopDragging() {
+      this.dragging = false;
+    },
+  },
+  mounted() {
+    document.addEventListener('mousemove', this.drag);
+    document.addEventListener('mouseup', this.stopDragging);
+  },
+  beforeUnmount() {
+    document.removeEventListener('mousemove', this.drag);
+    document.removeEventListener('mouseup', this.stopDragging);
+  },
+};
+</script>
+
+<style scoped lang="css">
+.slider {
+  position: absolute;
+  top: 50%;
+  right: 0px;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.button {
+  border: none;
+  border-radius: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  appearance: none;
+  background: #242c41;
+  color: #808b98;
+  font-weight: bold;
+  cursor: pointer;
+  opacity: 0.8;
+}
+
+.handle {
+  margin: 3px 6px;
+  position: relative;
+  width: 20px;
+  height: 90px;
+  cursor: pointer;
+}
+
+.bar {
+  position: absolute;
+  top: -3px;
+  left: 50%;
+  bottom: -3px;
+  margin-left: -3px;
+  width: 6px;
+  background: #242c41;
+  opacity: 0.8;
+}
+
+.grab {
+  transform: translateY(3px);
+  position: absolute;
+  border-radius: 3px;
+  left: 0;
+  width: 100%;
+  height: 6px;
+  background: #808b98;
+  cursor: grab;
+}
+</style>
---
+++ packages/components/src/stories/Slider.stories.js
@@ -0,0 +1,22 @@
+import VSlider from '@/components/Slider.vue';
+
+export default {
+  title: 'AppLand/UI/Slider',
+  component: VSlider,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { VSlider },
+  data() {
+    return { state: 0.5 };
+  },
+  template: `
+    <v-slider
+      :value="state"
+      @slide="(v) => { state = v; }"
+    />
+  `,
+});
+
+export const slider = Template.bind({});
