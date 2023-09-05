1b7d08c4a7d7a291f6e6dd4c562ecb8cab6a0d65
Mohit Sahoo <git@mohitsahoo.com>
2023-09-06 02:54:58 +0530

    refactor(expectedReport.md): refactor microposts response and path syntax feat(expected-change-report.json): update locations in sourceSpecEntityDetails to reflect syntax changes feat(tests): add unit tests for formatEndpoint function in ReportFieldCalculator.spec.ts

--- packages/cli/src/cmds/compare/ChangeReporter.ts
+++ packages/cli/src/cmds/compare/ChangeReporter.ts
@@ -501,12 +501,12 @@ export class ReportFieldCalculator {
 
       // Check if the input only has the 'paths.' and endpoint
       if (parts.length === 2) {
-        return parts[1].replace('.', '/');
+        return parts[1];
       }
 
       const statusCode = parts[parts.length - 1];
       const method = parts[parts.length - 3].toUpperCase();
-      const path = parts[1].replace('.', '/');
+      const path = parts[1];
 
       return `${statusCode} ${method} ${path}`;
     }
