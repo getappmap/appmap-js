200e7d7090266e205a8efc0a0e256992c198c5e8
Kevin Gilpin <kgilpin@gmail.com>
2023-09-21 18:28:41 -0400

    feat: Run each unique diff command once

--- packages/cli/src/cmds/compare/DiffLoader.ts
+++ packages/cli/src/cmds/compare/DiffLoader.ts
@@ -1,89 +1,125 @@
 import parseDiff from 'parse-diff';
-import { executeCommand } from '../../lib/executeCommand';
 import { warn } from 'console';
 import assert from 'assert';
-import { exists } from '../../utils';
+
+import { executeCommand } from '../../lib/executeCommand';
+import { QueueObject, queue } from 'async';
+import { existsSync } from 'fs';
 
 export const MAX_DIFF_LENGTH = 1000 * 1000;
 
+export type Diff = {
+  command: string;
+  files: parseDiff.File[];
+};
+
+export class DiffLoaderQueue {
+  private diffQueue: QueueObject<string>;
+  private diffByRoots = new Map<string, Diff>();
+
+  constructor(public baseRevision: string, public headRevision: string) {
+    this.diffQueue = queue(this.executeDiffCommand.bind(this), 1);
+  }
+
+  async diff(sourcePathRoots: Set<string>): Promise<Diff | undefined> {
+    // Process only one diff at a time.
+
+    if (this.diffQueue.length()) await this.diffQueue.drain();
+
+    const roots = [...sourcePathRoots].sort().join(' ');
+
+    this.diffQueue.push(roots);
+    await this.diffQueue.drain();
+
+    return this.diffByRoots.get(roots);
+  }
+
+  private async executeDiffCommand(roots: string) {
+    if (roots === '') return;
+
+    if (this.diffByRoots.get(roots)) return;
+
+    const command = `git diff ${this.baseRevision}..${this.headRevision} -- ${roots}`;
+    let diff: Diff = { command, files: [] };
+    try {
+      const diffStr = await executeCommand(command, true);
+      if (diffStr.length > MAX_DIFF_LENGTH) {
+        warn(`Diff is too large to parse. Skipping...`);
+      } else {
+        const files = parseDiff(diffStr);
+        diff = { command, files };
+      }
+    } catch (err) {
+      // There's no point in trying this command again.
+      warn(err);
+    } finally {
+      this.diffByRoots.set(roots, diff);
+    }
+  }
+}
+
 export default class DiffLoader {
   private sourcePathRoots = new Set<string>();
-  private diffByFile: Map<string, parseDiff.File[]> | undefined;
-  private reloadQueue: Array<string[]> = [];
+  private diffByFile: Map<string, Diff> | undefined;
+  private diffLoaderQueue: DiffLoaderQueue;
 
-  constructor(public baseRevision: string, public headRevision: string) {}
+  constructor(public baseRevision: string, public headRevision: string) {
+    this.diffLoaderQueue = new DiffLoaderQueue(baseRevision, headRevision);
+  }
 
   lookupDiff(path: string): string | undefined {
-    assert(this.diffByFile);
+    if (!this.diffByFile) return;
 
-    const diffFiles = this.diffByFile.get(path);
-    if (!diffFiles) return;
+    const diff = this.diffByFile.get(path);
+    if (!diff) return;
 
-    return diffFiles
-      .map((diffFile) =>
-        diffFile.chunks
+    const { files: diffFiles } = diff;
+    const content = diffFiles
+      .map((diffFile) => {
+        const fromLine = ['---', diffFile.from].join(' ');
+        const toLine = ['+++', diffFile.to].join(' ');
+        const content = diffFile.chunks
           .map((chunk) =>
             [chunk.content, chunk.changes.map((change) => change.content).join('\n')].join('\n')
           )
-          .join('\n')
-      )
+          .join('\n');
+        return [fromLine, toLine, content, ''].join('\n');
+      })
       .join('\n');
+    return [content].join('\n');
   }
 
   async update(sourcePathRoots: Set<string>) {
-    let reloadDiff = false;
+    const diffRoots = new Set<string>();
     for (const root of sourcePathRoots) {
-      if (['vendor', 'node_modules'].includes(root)) continue;
-
-      if (!(await exists(root))) continue;
-
       if (!this.sourcePathRoots.has(root)) {
         this.sourcePathRoots.add(root);
-        reloadDiff = true;
+        if (DiffLoader.isEligibleFile(root)) diffRoots.add(root);
       }
     }
 
-    if (this.diffByFile && !reloadDiff) return;
-
-    this.reloadQueue.push([...this.sourcePathRoots]);
-
-    await this.reloadDiff();
+    const diff = await this.diffLoaderQueue.diff(diffRoots);
+    if (diff) this.parseDiffStr(diff);
   }
 
-  private async reloadDiff() {
-    const diffCommands = new Set<string>();
+  static isEligibleFile(root: string): boolean {
+    return !['vendor', 'node_modules'].includes(root) && existsSync(root);
+  }
 
-    while (this.reloadQueue.length > 0) {
-      const sourcePathRoots = this.reloadQueue.shift()!.sort();
-      const diffCommand = `git diff ${this.baseRevision}..${
-        this.headRevision
-      } -- ${sourcePathRoots.join(' ')}`;
-      if (diffCommands.has(diffCommand)) continue;
+  private parseDiffStr(diff: Diff) {
+    if (!this.diffByFile) this.diffByFile = new Map<string, Diff>();
 
-      diffCommands.add(diffCommand);
+    diff.files.reduce((memo, diffFile) => {
+      [...new Set([diffFile.to, diffFile.from])]
+        .filter(Boolean)
+        .sort()
+        .forEach((path) => {
+          assert(path);
+          if (!memo.has(path)) memo.set(path, { command: diff.command, files: [] });
 
-      try {
-        const diffStr = await executeCommand(diffCommand, true);
-        if (diffStr.length > MAX_DIFF_LENGTH) {
-          warn(`Diff is too large to parse. Skipping...`);
-          continue;
-        }
-
-        const diffFiles = parseDiff(diffStr);
-        this.diffByFile = diffFiles.reduce((memo, diffFile) => {
-          [...new Set([diffFile.to, diffFile.from])]
-            .filter(Boolean)
-            .sort()
-            .forEach((path) => {
-              assert(path);
-              if (!memo.has(path)) memo.set(path, []);
-              memo.get(path)?.push(diffFile);
-            });
-          return memo;
-        }, new Map<string, parseDiff.File[]>());
-      } catch (err) {
-        warn(err);
-      }
-    }
+          memo.get(path)!.files.push(diffFile);
+        });
+      return memo;
+    }, this.diffByFile);
   }
 }
