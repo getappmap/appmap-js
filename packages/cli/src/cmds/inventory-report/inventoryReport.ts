caa2afdb83cd6e25cc28b15bb83f4f492654d93f
Kevin Gilpin <kgilpin@gmail.com>
2023-10-13 17:34:03 -0400

    feat: Delete un-maintained report

--- packages/cli/src/cmds/inventory-report/inventoryReport.ts
+++ packages/cli/src/cmds/inventory-report/inventoryReport.ts
@@ -21,8 +21,8 @@ export const builder = (args: yargs.Argv) => {
   args.positional('template-name', {
     type: 'string',
     describe: `Template name.`,
-    default: 'default',
-    choices: ['default', 'welcome'],
+    default: 'welcome',
+    choices: ['welcome'],
     alias: 't',
   });
 
