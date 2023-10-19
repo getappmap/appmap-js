7c74b127fead746cab5d452d9676d49bcdb5ebaf
Adam Trotta <ahtrotta@gmail.com>
2023-10-19 13:38:26 -0400

    test: Add tests for appmap_url_with_finding helper

--- packages/cli/tests/unit/compareReport/helpers.spec.ts
+++ packages/cli/tests/unit/compareReport/helpers.spec.ts
@@ -1,4 +1,9 @@
 import helpers from '../../../src/cmds/compare-report/helpers';
+import assert from 'assert';
+import sinon from 'sinon';
+import ReportSection, { ReportOptions } from '../../../src/cmds/compare-report/ReportSection';
+import { base64UrlEncode } from '@appland/models';
+import { SafeString } from 'handlebars';
 
 describe('pluralize', () => {
   const pluralize = (helpers as any).pluralize;
@@ -17,4 +22,55 @@ describe('pluralize', () => {
     expect(pluralize(0, 'test', {})).toEqual('tests'));
   it(`ignores the 'plural' argument if it's a function`, () =>
     expect(pluralize(0, 'test', () => true)).toEqual('tests'));
+});
+
+describe('ReportSection', () => {
+  describe('appmap_url_with_finding helper', () => {
+    const appmapURL = new URL('https://getappmap.com');
+    const sourceURL = new URL('http://fake.com');
+    const mockHash = 'a0b1c2d3e4f5g6h7i8j9k';
+    const pathToMap = 'path/to/map';
+    const revision = 'head';
+    let sandbox: sinon.SinonSandbox;
+
+    beforeEach(() => (sandbox = sinon.createSandbox()));
+    afterEach(() => sandbox.restore());
+
+    it('creates the expected URL when appmapURL is set', () => {
+      const helpers = ReportSection.helpers({ appmapURL, sourceURL } as ReportOptions);
+      assert(helpers);
+      expect(helpers).toHaveProperty('appmap_url_with_finding');
+      const result = helpers.appmap_url_with_finding(revision, { id: pathToMap }, mockHash);
+      expect(result).toBeInstanceOf(SafeString);
+
+      const expectedPath = `${revision}%2F${pathToMap.replace(/\//g, '%2F')}.appmap.json`;
+      const expectedStateObject = { selectedObject: `analysis-finding:${mockHash}` };
+      const expectedState = base64UrlEncode(JSON.stringify(expectedStateObject));
+      expect(result.string).toBe(
+        `${appmapURL.toString()}?path=${expectedPath}&state=${expectedState}`
+      );
+    });
+
+    it('creates the expected URL when state is not created', () => {
+      sinon.stub(JSON, 'stringify').throws(new Error('mock error'));
+      const helpers = ReportSection.helpers({ appmapURL, sourceURL } as ReportOptions);
+      assert(helpers);
+      expect(helpers).toHaveProperty('appmap_url_with_finding');
+      const result = helpers.appmap_url_with_finding(revision, { id: pathToMap }, mockHash);
+      expect(result).toBeInstanceOf(SafeString);
+
+      const expectedPath = `${revision}%2F${pathToMap.replace(/\//g, '%2F')}.appmap.json`;
+      expect(result.string).toBe(`${appmapURL.toString()}?path=${expectedPath}`);
+    });
+
+    it('creates the expected URL when appmapURL is not set', () => {
+      const helpers = ReportSection.helpers({ sourceURL } as ReportOptions);
+      assert(helpers);
+      expect(helpers).toHaveProperty('appmap_url_with_finding');
+      const result = helpers.appmap_url_with_finding('head', { id: pathToMap }, mockHash);
+      expect(result).toBeInstanceOf(SafeString);
+
+      expect(result.string).toBe(`${revision}/${pathToMap}.appmap.json`);
+    });
+  });
 });
