025d9740c430d0464f55ab1095e3545d054834ca
Brian Kelly <brikelly@users.noreply.github.com>
2023-10-26 20:53:22 -0400

    Change mentions of "Runnable recording" to "code block recording" in install instructions (#1449)
    * fix: change runnable to code block in Java recording instructions
    
    * fix: changing runnable to code block instructions and doc link for IntelliJ

--- packages/components/src/components/install-guide/record-instructions/Java.vue
+++ packages/components/src/components/install-guide/record-instructions/Java.vue
@@ -114,21 +114,21 @@
     <section class="recording-method">
       <h3>
         <i class="header-icon"><PlayIcon /></i>
-        Runnable recording
+        Code Block recording
       </h3>
       <p>
         You can use the AppMap Java library directly to record a specific span of code. With this
         method, you can control exactly what code is recorded, and where the recording is saved. To
-        use runnable recording, add an AppMap code snippet to the section of code you want to
+        use code block recording, add an AppMap code snippet to the section of code you want to
         record, then run your application using the Debug Configuration with AppMap.
       </p>
       <p>
         Visit
         <a
-          href="https://appmap.io/docs/reference/appmap-java.html#runnable-recording"
+          href="https://appmap.io/docs/reference/appmap-java.html#code-block-recording"
           target="_blank"
         >
-          AppMap Docs - Java Runnable recording
+          AppMap Docs - Java Code Block recording
         </a>
         for more information.
       </p>
