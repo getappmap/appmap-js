15f64a2ddd5197530baf4cac248de7aad7c832b6
Adam Trotta <ahtrotta@gmail.com>
2023-11-10 16:55:01 -0500

    feat: Export sequence diagram with the store

--- packages/components/src/stories/DiagramSequence.stories.js
+++ packages/components/src/stories/DiagramSequence.stories.js
@@ -1,36 +1,36 @@
-import VDiagramSequence from '@/components/DiagramSequence.vue';
+import VSequenceDiagram from '@/pages/SequenceDiagram.vue';
 import micropost_diff from '@/stories/data/sequence/Users_profile_profile_display.diff.sequence.json';
 import create_api_key from '@/stories/data/sequence/create_api_key.sequence.json';
 import list_users from '@/stories/data/sequence/list_users.sequence.json';
 import list_users_prefetch from '@/stories/data/sequence/list_users_prefetch.sequence.json';
 import show_user from '@/stories/data/sequence/show_user.sequence.json';
 import user_not_found from '@/stories/data/sequence/user_not_found.sequence.json';
-import { buildStore, SELECT_CODE_OBJECT } from '@/store/vsCode';
+import { SELECT_CODE_OBJECT } from '@/store/vsCode';
 import { Event } from '@appland/models';
 
-const store = buildStore();
-
-// Build a mock Event that will trick the instanceof operator
-const mockEvent = Object.create(Event.prototype);
-Object.assign(mockEvent, { id: 536 });
-
-store.commit(SELECT_CODE_OBJECT, mockEvent);
+const mockEvent = new Event({ id: 536, event: 'call' });
 
 export default {
   title: 'AppLand/Diagrams/Sequence',
-  component: VDiagramSequence,
+  component: VSequenceDiagram,
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { VDiagramSequence },
-  template: '<v-diagram-sequence v-bind="$props"/>',
-  store,
+  components: { VSequenceDiagram },
+  template: '<v-sequence-diagram v-bind="$props" ref="seqDiagram" />',
+  mounted() {
+    const mockEvent = args.mockEvent;
+    if (mockEvent) {
+      this.$refs.seqDiagram.$store.commit(SELECT_CODE_OBJECT, mockEvent);
+    }
+  },
 });
 
 export const MicropostUserProfileDiff = Template.bind({});
 MicropostUserProfileDiff.args = {
   serializedDiagram: micropost_diff,
+  mockEvent,
 };
 export const Empty = Template.bind({});
 
