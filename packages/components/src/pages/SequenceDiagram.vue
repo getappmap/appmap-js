0ddd029ef13b631db4b35873ead2e85ffee431c1
Adam Trotta <ahtrotta@gmail.com>
2023-11-10 16:55:01 -0500

    feat: Export sequence diagram with the store

---
+++ packages/components/src/pages/SequenceDiagram.vue
@@ -0,0 +1,30 @@
+<template>
+  <VDiagramSequence :serialized-diagram="serializedDiagram" :interactive="interactive" />
+</template>
+
+<script>
+import VDiagramSequence from '@/components/DiagramSequence.vue';
+import { store } from '@/store/vsCode';
+
+export default {
+  name: 'v-sequence-diagram',
+
+  components: {
+    VDiagramSequence,
+  },
+
+  props: {
+    serializedDiagram: {
+      type: Object,
+    },
+    interactive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  store,
+};
+</script>
+
+<style scoped lang="scss"></style>
