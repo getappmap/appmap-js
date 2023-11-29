db7ab097eeb4c1859f0c31610e018d49f764c930
Dustin Byrne <dustinsbyrne@gmail.com>
2023-11-29 15:24:03 -0500

    feat: Websocket AI client

--- packages/client/src/ai.ts
+++ packages/client/src/ai.ts
@@ -1,54 +1,27 @@
-import { Observable } from 'rxjs';
-import { ServiceEndpoint } from './configuration';
-import buildRequest from './buildRequest';
-
-type StreamResponse = {
-  threadId: string;
-  userMessageId: string;
-  systemMessageId: string;
-  response: Observable<string>;
-};
+import { io } from 'socket.io-client';
+import AIClient, { Callbacks } from './aiClient';
+import { getConfiguration } from './loadConfiguration';
 
 // eslint-disable-next-line unicorn/no-static-only-class
 export default class AI {
-  static async conversation(input: string, threadId?: string): Promise<StreamResponse> {
-    const requestBuilder = buildRequest('v1/ai/conversation', {
-      service: ServiceEndpoint.ServiceApi,
-      requireApiKey: true,
-    });
-
-    const body = JSON.stringify({ input, threadId });
-    const response = await fetch(requestBuilder.url.toString(), {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        ...requestBuilder.headers,
+  static async connect(callbacks: Callbacks): Promise<AIClient> {
+    const configuration = getConfiguration();
+    const socket = io(configuration.apiURL.replace(/^http/, 'ws'), {
+      transports: ['websocket'],
+      path: '/v1/ai/conversation',
+      reconnection: false,
+      auth: {
+        token: configuration.apiKey,
       },
-      body,
     });
 
-    if (!response.body) {
-      throw new Error('Received an empty response');
-    }
-
-    const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
-    return {
-      response: new Observable<string>((subscriber) => {
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        setImmediate(async () => {
-          for (;;) {
-            // eslint-disable-next-line no-await-in-loop
-            const { value, done } = await reader.read();
-            if (done) break;
-            subscriber.next(value);
-          }
-
-          subscriber.complete();
-        });
-      }),
-      threadId: response.headers.get('X-Thread-ID') || '',
-      userMessageId: response.headers.get('x-user-message-id') || '',
-      systemMessageId: response.headers.get('x-system-message-id') || '',
-    };
+    return new Promise((resolve, reject) => {
+      socket.on('connect', () => {
+        resolve(new AIClient(socket, callbacks));
+      });
+      socket.on('connect_error', (error) => {
+        reject(error);
+      });
+    });
   }
 }
