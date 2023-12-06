8b3a9dd8515483cad0273585626b61a7bb9185a5
Kevin Gilpin <kgilpin@gmail.com>
2023-12-06 02:05:39 -0500

    feat: ChatSearch component

---
+++ packages/components/src/pages/ChatSearch.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="chat-search-container">
+    <v-chat class="chat" :api-key="apiKey" :api-url="apiUrl"> </v-chat>
+    <vs-code-extension-vue class="appmap"> </vs-code-extension-vue>
+  </div>
+</template>
+
+<script lang="ts">
+//@ts-nocheck
+import VChat from '@/pages/Chat.vue';
+import VsCodeExtensionVue from './VsCodeExtension.vue';
+import { DefaultApiURL } from '@appland/client';
+
+export default {
+  name: 'v-chat-search',
+  components: {
+    VChat,
+    VsCodeExtensionVue,
+  },
+  props: {
+    apiKey: {
+      type: String,
+    },
+    apiUrl: {
+      type: String,
+      default: DefaultApiURL,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.chat-search-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.chat {
+}
+
+.appmap {
+}
+</style>
