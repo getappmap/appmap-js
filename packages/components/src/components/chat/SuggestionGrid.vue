4c173546c0a2bfcd1b055207cea04cab4ea682ca
Kevin Gilpin <kgilpin@gmail.com>
2023-12-07 06:18:51 -0500

    feat: Update suggested questions

--- packages/components/src/components/chat/SuggestionGrid.vue
+++ packages/components/src/components/chat/SuggestionGrid.vue
@@ -3,27 +3,27 @@
     <p>Not sure? Here are some useful starting points:</p>
     <div class="grid">
       <v-suggestion-card
-        title="Fix a performance issue."
-        sub-title="AppMap identified 1 performance issue"
-        prompt="You have one performance issue pending. Would you like me to describe it, or do you have a specific issue in mind?"
+        title="Document the architecture of a feature."
+        sub-title="Get AppMaps and a detailed text description of the code design."
+        prompt="Describe a particular feature or system within your application and I'll describe how it works."
+        @suggest="onSuggestion"
+      />
+      <v-suggestion-card
+        title="Identify the usage of personal data."
+        sub-title="Enumerate the user's personal data that's being accessed."
+        prompt="Describe a feature and ask which user data it accesses. I'll tell you what I find."
         @suggest="onSuggestion"
       />
       <v-suggestion-card
         title="Find the root cause of a performance issue."
         sub-title="Describe the issue and let AppMap help you fix it."
-        prompt="Sure! Let's get to the bottom of a performance issue. Can you describe what the issue is, or how it's manifesting itself in your application?"
+        prompt="Let's get to the bottom of a performance issue. Can you describe what the issue is, or how it's manifesting itself in your application?"
         @suggest="onSuggestion"
       />
       <v-suggestion-card
         title="Look for security issues."
-        sub-title="Create AppMap data for your entire application to surface runtime security flaws."
+        sub-title="Use AppMap data to surface runtime security flaws."
         prompt="I can help you identify any potential security issues. Do you have a particular concern in mind, or would you like me to make a suggestion?"
-        @suggest="onSuggestion"
-      />
-      <v-suggestion-card
-        title="Document the behavior of a specific feature."
-        sub-title="Visualize a specific behavior in your application."
-        prompt="Describe a particular feature or system within your application and I'll help you document or understand it."
         @suggest="onSuggestion"
       />
     </div>
