9f8309a04d1f40949368b0b4d6e59e7eb30cb502
Adam Trotta <ahtrotta@gmail.com>
2023-12-08 14:27:09 -0500

    feat: Enable panel resizing

--- packages/components/src/pages/ChatSearch.vue
+++ packages/components/src/pages/ChatSearch.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="chat-search-container">
-    <div class="chat-container">
+  <div
+    class="chat-search-container"
+    @mousemove="makeResizing"
+    @mouseup="stopResizing"
+    @mouseleave="stopResizing"
+  >
+    <div class="chat-container" ref="chatContainer">
       <v-chat class="chat-search-chat" ref="vchat" :send-message="sendMessage" @clear="clear" />
     </div>
+    <div class="chat-search-container--drag" @mousedown="startResizing"></div>
     <div class="search-container">
       <div class="search-results-container">
         <h2>AppMap search results</h2>
@@ -105,6 +111,9 @@ export default {
       searchId: 0,
       selectedSearchResultId: undefined,
       showDiagnostics: false,
+      isPanelResizing: false,
+      initialPanelWidth: 0,
+      initialClientX: 0,
     };
   },
   watch: {
@@ -199,11 +208,36 @@ export default {
     newIndex(): Index {
       return new Index(this.indexFn ? { request: this.indexFn } : this.indexPort || 30101);
     },
+    startResizing(event) {
+      console.log('startResizing');
+      document.body.style.userSelect = 'none';
+      this.isPanelResizing = true;
+      this.initialPanelWidth = this.$refs.chatContainer.offsetWidth;
+      this.initialClientX = event.clientX;
+    },
+    makeResizing(event) {
+      if (this.isPanelResizing) {
+        const MIN_PANEL_WIDTH = 280;
+        const MAX_PANEL_WIDTH = window.innerWidth * 0.75;
+
+        let newWidth = this.initialPanelWidth + (event.clientX - this.initialClientX);
+        newWidth = Math.max(MIN_PANEL_WIDTH, newWidth);
+        newWidth = Math.min(MAX_PANEL_WIDTH, newWidth);
+
+        this.$refs.chatContainer.style.width = `${newWidth}px`;
+      }
+    },
+    stopResizing() {
+      document.body.style.userSelect = '';
+      this.isPanelResizing = false;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+$border-color: darken($gray4, 10%);
+
 .chat-search-container {
   display: flex;
   flex-direction: row;
@@ -222,8 +256,16 @@ export default {
       min-width: auto;
       border-right: 1px solid $border-color;
       margin: 0;
-      width: 95%;
-      flex: 1;
+    }
+  }
+  .chat-search-container--drag {
+    width: 2px;
+    background: transparent;
+    cursor: col-resize;
+    z-index: 100;
+
+    &:hover {
+      background: $gray5;
     }
   }
   .search-container {
