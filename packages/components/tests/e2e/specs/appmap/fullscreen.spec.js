d713d2b11d7c7332e781c4f9d17f9d4f93f91c53
Dustin Byrne <dustinsbyrne@gmail.com>
2023-12-13 17:29:37 -0500

    test: Verify full screen behavior

--- packages/components/tests/e2e/specs/appmap/fullscreen.spec.js
+++ packages/components/tests/e2e/specs/appmap/fullscreen.spec.js
@@ -3,8 +3,47 @@ context('Fullscreen toggle', () => {
     cy.visit('http://localhost:6006/iframe.html?id=pages-vs-code--extension&viewMode=story');
   });
 
-  it('toggles on and off', () => {
-    cy.get('[data-cy="fullscreen-button"]:not([data-enabled])').click();
-    cy.get('[data-cy="fullscreen-button"][data-enabled]').click();
+  it('toggles on and off via click', async () => {
+    const requestFullScreen = cy.stub();
+    const exitFullscreen = cy.stub();
+
+    let appElement = null;
+    cy.get('[data-cy="app"]').then(([el]) => {
+      appElement = el;
+      appElement.requestFullScreen = requestFullScreen;
+    });
+
+    cy.get('[data-cy="fullscreen-button"]')
+      .click()
+      .then(() => {
+        expect(requestFullScreen).to.be.called;
+      });
+
+    // Manually set the fullscreenElement property on the document
+    // to simulate the browser entering fullscreen mode.
+    cy.document().then((doc) => {
+      cy.stub(doc, 'fullscreenElement', appElement);
+      cy.stub(doc, 'exitFullscreen', exitFullscreen);
+    });
+
+    // Similarly, manually trigger the fullscreenchange event
+    // The browser won't let an automated test trigger fullscreen without user interaction
+    cy.get('[data-cy="app"]').trigger('fullscreenchange');
+
+    cy.get('[data-cy="fullscreen-button"][data-enabled]')
+      .click()
+      .then(() => {
+        expect(exitFullscreen).to.be.called;
+      });
+
+    // Manually clear the fullscreenElement
+    cy.document().then((doc) => {
+      cy.stub(doc, 'fullscreenElement', undefined);
+    });
+
+    // Fire the fullscreenchange event again so the frontend can be notified
+    cy.get('[data-cy="app"]').trigger('fullscreenchange');
+
+    cy.get('[data-cy="fullscreen-button"]:not([data-enabled])').should('exist');
   });
 });
