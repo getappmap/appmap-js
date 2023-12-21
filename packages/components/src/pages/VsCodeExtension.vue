5da92168305d7463c306168550f478077152f61a
Kevin Gilpin <kgilpin@gmail.com>
2023-12-21 18:27:07 -0500

    fix: Remove conflicting and unused property

--- packages/components/src/pages/VsCodeExtension.vue
+++ packages/components/src/pages/VsCodeExtension.vue
@@ -504,10 +504,6 @@ export default {
       type: Array,
       default: () => [],
     },
-    hideDetailsPanel: {
-      type: Boolean,
-      default: false,
-    },
     allowFullscreen: {
       type: Boolean,
       default: false,
@@ -591,7 +587,6 @@ export default {
       return {
         'main-column': true,
         'main-column--left': true,
-        'main-column--left--hidden': this.hideDetailsPanel,
         'manual-resizing': this.isPanelResizing,
       };
     },
