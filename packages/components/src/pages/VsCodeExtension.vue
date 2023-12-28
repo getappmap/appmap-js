339f38ced13a80846cd63a94997c9581651c9aa1
Dustin Byrne <dustinsbyrne@gmail.com>
2023-12-28 12:19:33 -0500

    fixup! feat: Add ChatSearch UI

--- packages/components/src/pages/VsCodeExtension.vue
+++ packages/components/src/pages/VsCodeExtension.vue
@@ -568,6 +568,16 @@ export default {
         this.selectCurrentHighlightedEvent();
       },
     },
+    filteredAppMap: {
+      handler() {
+        if (this.selectedObject) {
+          // Keep the currently selected object only, discard the rest
+          const selection = this.selectedObject;
+          this.$store.commit(CLEAR_SELECTION_STACK);
+          this.setSelectedObject(selection.fqid);
+        }
+      },
+    },
     isActive: {
       handler() {
         clearTimeout(this.seqDiagramTimeoutId);
