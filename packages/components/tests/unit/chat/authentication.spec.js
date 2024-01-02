d96eadb3d9d7355ffeffd6803f352840e2e80324
Dustin Byrne <dustinsbyrne@gmail.com>
2024-01-02 14:02:07 -0500

    fix: Authenticate Chat/ChatSearch frontend clients

---
+++ packages/components/tests/unit/chat/authentication.spec.js
@@ -0,0 +1,36 @@
+import VChat from '@/pages/Chat.vue';
+import VChatSearch from '@/pages/ChatSearch.vue';
+import { mount } from '@vue/test-utils';
+import { setConfiguration, loadConfiguration } from '@appland/client';
+
+describe('Authentication', () => {
+  const apiKey = 'apiKey';
+  const apiUrl = 'apiUrl';
+
+  beforeEach(() => {
+    setConfiguration({
+      apiKey: undefined,
+      apiURL: undefined,
+    });
+  });
+
+  it('authenticates Chat', async () => {
+    const wrapper = mount(VChat, { propsData: { apiKey, apiUrl } });
+    await wrapper.vm.$nextTick();
+    expect(loadConfiguration()).toStrictEqual({
+      apiKey,
+      apiURL: apiUrl,
+      baseURL: expect.any(String),
+    });
+  });
+
+  it('authenticates ChatSearch', async () => {
+    const wrapper = mount(VChatSearch, { propsData: { apiKey, apiUrl } });
+    await wrapper.vm.$nextTick();
+    expect(loadConfiguration()).toStrictEqual({
+      apiKey,
+      apiURL: apiUrl,
+      baseURL: expect.any(String),
+    });
+  });
+});
