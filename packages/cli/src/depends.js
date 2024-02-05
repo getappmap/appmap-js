009abe418e104443f5b9bfda5af71303db87ce98
Kevin Gilpin <kgilpin@gmail.com>
2024-02-05 14:35:00 -0500

    refactor: Move UpToDate checking into its own class

--- packages/cli/src/depends.js
+++ packages/cli/src/depends.js
@@ -1,10 +1,6 @@
-const fsp = require('fs').promises;
-const { dirname, join: joinPath, isAbsolute, basename } = require('path');
-const { verbose, mtime, processNamedFiles } = require('./utils');
-
-// Gets the file path of a location. Location may include a line number or other info
-// in addition to the file path.
-const parseFilePath = (location) => location.split(':')[0];
+const { dirname } = require('path');
+const { verbose, processNamedFiles } = require('./utils');
+const { default: UpToDate } = require('./lib/UpToDate');
 
 class Depends {
   /**
@@ -13,8 +9,6 @@ class Depends {
   constructor(appMapDir) {
     this.appMapDir = appMapDir;
     this.baseDir = '.';
-    this.mtimes = new Map();
-    this.mtimeCacheHitCount = 0;
   }
 
   /**
@@ -47,20 +41,6 @@ class Depends {
   }
 
   /**
-   * Prepend the baseDir to filePath, unless filePath is absolute.
-   *
-   * @param {string} filePath
-   * @returns string
-   */
-  makeAbsolutePath(filePath) {
-    if (isAbsolute(filePath)) {
-      return filePath;
-    }
-
-    return joinPath(this._baseDir, filePath);
-  }
-
-  /**
    * Compute the AppMaps which are out of date with regard to dependency files.
    * Each result is the name of the AppMap file with the suffix 'appmap.json' stripped.
    * If a callback is provided, the AppMaps names are yielded as they are detected.
@@ -71,106 +51,25 @@ class Depends {
   async depends(callback = undefined) {
     const outOfDateNames = new Set();
 
-    const checkClassMap = async (fileName) => {
-      const indexDir = dirname(fileName);
-      if (basename(indexDir) === 'Inventory') {
-        return;
+    const isClientProvidedFile = (filePath) => this.testLocations.has(filePath);
+
+    const upToDate = new UpToDate();
+    if (this._baseDir) upToDate.baseDir = this._baseDir;
+    if (this.testLocations) upToDate.testFunction = isClientProvidedFile;
+
+    const uptodate = async (classMapFile) => {
+      const appmapName = dirname(classMapFile);
+      const outOfDateCodeLocations = await upToDate.isOutOfDate(appmapName);
+      if (outOfDateCodeLocations) {
+        if (callback) callback(appmapName);
+
+        outOfDateNames.add(appmapName);
       }
-
-      let appmapUpdatedAtStr;
-      try {
-        appmapUpdatedAtStr = await fsp.readFile(joinPath(indexDir, 'mtime'));
-      } catch (err) {
-        if (err.code !== 'ENOENT') console.warn(err);
-        return;
-      }
-      const appmapUpdatedAt = parseFloat(appmapUpdatedAtStr);
-
-      if (verbose()) {
-        console.log(`Checking AppMap ${indexDir} with timestamp ${appmapUpdatedAt}`);
-      }
-
-      const reportOutOfDate = (filePath) => {
-        if (verbose()) {
-          console.log(`${filePath} requires rebuild of AppMap ${indexDir}`);
-        }
-        if (!outOfDateNames.has(indexDir)) {
-          if (callback) {
-            callback(indexDir);
-          }
-          outOfDateNames.add(indexDir);
-        }
-      };
-
-      const classMap = JSON.parse(await fsp.readFile(fileName));
-      const codeLocations = new Set();
-
-      const collectFilePaths = (item) => {
-        if (item.location) {
-          const filePath = parseFilePath(item.location);
-          codeLocations.add(filePath);
-        }
-        if (item.children) {
-          item.children.forEach(collectFilePaths);
-        }
-      };
-      classMap.forEach(collectFilePaths);
-
-      const isClientProvidedFile = (filePath) => this.testLocations.has(filePath);
-
-      const modifiedTime = async (filePath) => {
-        const result = this.mtimes.get(filePath);
-        if (result || result === false) {
-          this.mtimeCacheHitCount += 1;
-          return result;
-        }
-
-        const dependencyFilePath = this.makeAbsolutePath(filePath);
-        let dependencyModifiedAt = await mtime(dependencyFilePath);
-
-        // TODO: Actually, if the dependency file does not exist, we should return true.
-        // However, at this time we can't tell the difference between a dependency file that has
-        // been deleted, and one that never existed in the first place. What does 'never existed in the first place'
-        // mean? Well, unfortunately the Ruby agent (and perhaps others?) will write file locations
-        // like 'JSON' for native functions that don't actually correspond to real files.
-        if (!dependencyModifiedAt) {
-          dependencyModifiedAt = false;
-          if (verbose()) console.log(`[depends] ${dependencyFilePath} does not exist`);
-        }
-
-        this.mtimes.set(filePath, dependencyModifiedAt);
-        return dependencyModifiedAt;
-      };
-
-      const isFileModifiedSince = async (filePath) => {
-        const dependencyModifiedAt = await modifiedTime(filePath);
-        if (dependencyModifiedAt === false) return false;
-
-        const uptodate = appmapUpdatedAt < dependencyModifiedAt;
-        if (!uptodate && verbose()) {
-          console.log(
-            `[depends] ${filePath} timestamp is ${dependencyModifiedAt}, NOT up to date with ${indexDir} (${appmapUpdatedAt})`
-          );
-        }
-
-        return uptodate;
-      };
-
-      const testFunction = this.testLocations ? isClientProvidedFile : isFileModifiedSince;
-
-      await Promise.all(
-        [...codeLocations].map(async (filePath) => {
-          if (await testFunction(filePath)) {
-            if (verbose()) console.debug(`[depends] ${indexDir} is out of date due to ${filePath}`);
-            reportOutOfDate(filePath, indexDir);
-          }
-        })
-      );
     };
 
     await processNamedFiles(this.appMapDir, 'classMap.json', async (fileName) => {
       try {
-        await checkClassMap(fileName);
+        await uptodate(fileName);
       } catch (e) {
         console.log(e.code);
         console.warn(`Error checking uptodate ${fileName}: ${e}`);
@@ -178,7 +77,6 @@ class Depends {
     });
 
     if (verbose()) {
-      console.debug(`File mtime cache hit count: ${this.mtimeCacheHitCount}`);
       console.debug(`Out of date count: ${outOfDateNames.size}`);
     }
 
---
+++ packages/cli/tests/unit/lib/UpToDate.spec.ts
@@ -0,0 +1,83 @@
+import UpToDate, { AppMapIndex } from '../../../src/lib/UpToDate';
+import { verbose } from '../../../src/utils';
+
+describe('UpToDate', () => {
+  beforeAll(() => verbose(process.env.DEBUG === 'true'));
+  afterEach(() => jest.resetAllMocks());
+
+  const aSecondAgo = Date.now() - 1000;
+
+  let appMapIndex: AppMapIndex;
+  let upToDate: UpToDate;
+
+  describe('when the AppMap file mtime cannot be determined', () => {
+    beforeEach(() => {
+      const updatedAt = jest.fn().mockResolvedValue(undefined);
+      appMapIndex = {
+        updatedAt,
+      } as any;
+      upToDate = new UpToDate();
+      upToDate.appMapIndex = appMapIndex;
+    });
+
+    it('should return undefined', async () => {
+      const result = await upToDate.isOutOfDate('appmap');
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('with ClassMap', () => {
+    beforeEach(() => {
+      const updatedAt = jest.fn().mockResolvedValue(aSecondAgo);
+      appMapIndex = {
+        updatedAt,
+        classMap: jest.fn().mockResolvedValue([
+          {
+            location: 'file1.rb',
+            children: [
+              {
+                location: 'file2.rb',
+              },
+            ],
+          },
+        ]),
+      } as any;
+      upToDate = new UpToDate();
+      upToDate.appMapIndex = appMapIndex;
+    });
+
+    it('determines the modification time of each file once', async () => {
+      // It doesn't matter for this test what the result is
+      upToDate.mtime = jest.fn().mockResolvedValue(aSecondAgo - 1000);
+
+      await upToDate.isOutOfDate('appmap');
+      expect(upToDate.mtime).toHaveBeenCalledTimes(2);
+      expect(upToDate.mtimeCacheHits).toBe(0);
+
+      await upToDate.isOutOfDate('appmap');
+      expect(upToDate.mtime).toHaveBeenCalledTimes(2);
+      expect(upToDate.mtimeCacheHits).toBe(2);
+    });
+
+    describe('when source files are older than the AppMap file', () => {
+      beforeEach(() => {
+        upToDate.mtime = jest.fn().mockResolvedValue(aSecondAgo - 1000);
+      });
+
+      it('should return undefined', async () => {
+        const result = await upToDate.isOutOfDate('appmap');
+        expect(result).toBeUndefined();
+      });
+    });
+    describe('when a source file is newer than the AppMap file', () => {
+      beforeEach(() => {
+        upToDate.mtime = jest.fn().mockResolvedValue(aSecondAgo + 1000);
+      });
+
+      it('should return the relevant set of source files', async () => {
+        const result = await upToDate.isOutOfDate('appmap');
+        expect(result).toEqual(new Set(['file1.rb', 'file2.rb']));
+      });
+    });
+  });
+});
