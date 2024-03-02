634fb7ee3f47f10543bf343063295fbdec4ed39e
Kevin Gilpin <kgilpin@gmail.com>
2024-03-02 12:12:09 -0500

    feat: Save chat history for local Navie

--- packages/cli/src/rpc/explain/navie/navie-local.ts
+++ packages/cli/src/rpc/explain/navie/navie-local.ts
@@ -1,23 +1,59 @@
 import EventEmitter from 'events';
-import { Context, Explain, ProjectInfo, explain } from '@appland/navie';
+import { Context, Explain, Message, ProjectInfo, explain } from '@appland/navie';
 
 import INavie from './inavie';
 import { randomUUID } from 'crypto';
-import { verbose } from '../../../utils';
+import { join } from 'path';
+import { homedir } from 'os';
+import { mkdir, readFile, readdir, writeFile } from 'fs/promises';
+
+class LocalHistory {
+  constructor(public readonly threadId: string) {}
+
+  async saveMessage(message: Message) {
+    await this.initHistory();
+    const timestampNumber = Date.now();
+    const historyFile = join(this.historyDir, `${timestampNumber}.json`);
+    await writeFile(historyFile, JSON.stringify(message, null, 2));
+  }
+
+  async restoreMessages(): Promise<Message[]> {
+    await this.initHistory();
+    const historyFiles = (await readdir(this.historyDir)).sort();
+    const history: Message[] = [];
+    for (const historyFile of historyFiles) {
+      const historyPath = join(this.historyDir, historyFile);
+      const historyString = await readFile(historyPath, 'utf-8');
+      const message = JSON.parse(historyString);
+      history.push(message);
+    }
+    return history;
+  }
+
+  protected async initHistory() {
+    await mkdir(this.historyDir, { recursive: true });
+  }
+
+  protected get historyDir() {
+    return join(homedir(), '.appmap', 'navie', 'history', this.threadId);
+  }
+}
 
 export default class LocalNavie extends EventEmitter implements INavie {
+  public history: LocalHistory;
+
   constructor(
     public threadId: string | undefined,
     private readonly contextProvider: Context.ContextProvider,
     private readonly projectInfoProvider: ProjectInfo.ProjectInfoProvider
   ) {
     super();
+
+    this.threadId = threadId || randomUUID();
+    this.history = new LocalHistory(this.threadId);
   }
 
   async ask(question: string, codeSelection: string | undefined): Promise<void> {
-    if (!this.threadId) {
-      this.threadId = randomUUID();
-    }
     const messageId = randomUUID();
     this.emit('ack', messageId, this.threadId);
 
@@ -25,24 +61,24 @@ export default class LocalNavie extends EventEmitter implements INavie {
       question,
       codeSelection,
     };
-    // TODO: Make configurable
     const options = new Explain.ExplainOptions();
-    // TODO: Provide
-    const chatHistory = [];
+    const history = await this.history.restoreMessages();
+    this.history.saveMessage({ content: question, role: 'user' });
 
     const explainFn = explain(
       clientRequest,
       this.contextProvider,
       this.projectInfoProvider,
       options,
-      chatHistory
+      history
     );
-    explainFn.on('event', (event) => {
-      if (verbose()) console.log(event.message);
-    });
+    explainFn.on('event', (event) => this.emit('event', event));
+    const response = new Array<string>();
     for await (const token of explainFn.execute()) {
+      response.push(token);
       this.emit('token', token);
     }
+    this.history.saveMessage({ content: response.join(''), role: 'system' });
     this.emit('complete');
   }
 }
