c67755fb74e21e4457dd91c40e450318ded84e40
Adam Trotta <ahtrotta@gmail.com>
2024-03-05 17:20:45 -0500

    feat: Users can activate with an email

--- packages/components/src/components/SignIn.vue
+++ packages/components/src/components/SignIn.vue
@@ -1,39 +1,117 @@
 <template>
   <div class="signin-sidebar">
     <AppMapLogo width="120" />
-    <div class="content-wrap">
+    <div v-if="!submitted" class="content-wrap">
       <div class="sidebar-title">
-        <h1 data-cy="title">Sign in</h1>
-        <p>
-          Start visualizing your runtime code, identifying problems, and finding solutions. Sign in
-          via GitHub or GitLab to start automatically generating:
-        </p>
+        <h1 data-cy="title">Activate AppMap</h1>
       </div>
 
-      <ul class="signin-ctas">
-        <li>
-          <a href="https://appmap.io/docs/navie">AppMap Navie AI</a>
-        </li>
-        <li>
-          <a href="https://appmap.io/docs/diagrams">Code visualizations</a>
-        </li>
-        <li>
-          <a href="https://appmap.io/docs/analysis/in-ci.html">AppMap in CI</a>
-        </li>
-        <li>
-          <a href="https://appmap.io/docs/analysis/in-your-code-editor.html">Runtime analysis</a>
-        </li>
-      </ul>
       <div class="signin-buttons">
-        <a href="/" class="btn btn-primary" data-cy="sign-in-button" @click="signIn">Sign in</a>
+        <p v-if="error" class="error">{{ error }}</p>
+        <input
+          id="email-input"
+          type="email"
+          placeholder="email address"
+          v-model="email"
+          @keydown="keyDown"
+          required
+        />
+        <a
+          href="/"
+          class="btn btn-primary"
+          data-cy="email-activation-button"
+          @click.prevent="activateWithEmail"
+        >
+          Activate with<span><EmailIcon class="small-logo" /> Email</span>
+        </a>
+
+        <div class="or-divider-container">
+          <div class="line"></div>
+          <p class="or-divider">OR</p>
+          <div class="line"></div>
+        </div>
+
+        <a
+          class="btn btn-primary"
+          data-cy="github-activation-button"
+          href="/"
+          @click.prevent="signIn"
+        >
+          Activate with <span><GitHubLogo class="small-logo" /> GitHub</span>
+        </a>
+        <a
+          class="btn btn-primary"
+          data-cy="gitlab-activation-button"
+          href="/"
+          @click.prevent="signIn"
+        >
+          Activate with <span><GitLabLogo class="small-logo" /> GitLab</span>
+        </a>
+
+        <div class="accept-tos">
+          <p>
+            By activating AppMap, I agree to the
+            <a
+              class="link"
+              target="_blank"
+              href="https://appmap.io/community/terms-and-conditions.html"
+            >
+              AppMap Terms of Service<ExternalLinkIcon />
+            </a>
+          </p>
+        </div>
       </div>
 
       <div class="your-data">
-        <shieldIcon />
-        <p data-cy="your-data-text">
-          Authentication with GitHub or GitLab is used solely for issuing a license. AppMap runs in
-          your code editor, so your AppMaps and your source code stay on your machine.
-          <a href="https://appmap.io/security">Security FAQ<VExternalLinkIcon /></a>
+        <ShieldIcon />
+        <div data-cy="your-data-text" class="your-data-text">
+          <p>
+            Your email address is used solely to issue you an AppMap license. Your source code and
+            AppMaps stay on your machine when you use AppMap diagrams and runtime analysis. Consult
+            the
+            <a
+              class="link"
+              target="_blank"
+              href="https://appmap.io/community/terms-and-conditions.html"
+            >
+              Terms of Service<ExternalLinkIcon />
+            </a>
+            for data privacy terms of Navie AI.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div v-else class="finish-activation">
+      <div class="sidebar-title">
+        <h1 data-cy="title">Complete Activation</h1>
+        <p>
+          Check your inbox! We've sent you an email with a 6-digit verification code. Enter the code
+          below.
+        </p>
+      </div>
+
+      <div class="signin-buttons">
+        <p v-if="error" class="error">{{ error }}</p>
+        <input
+          id="verification-code-input"
+          v-model="verificationCode"
+          type="text"
+          placeholder="verification code"
+          @keydown="keyDown"
+        />
+        <a
+          href="/"
+          class="btn btn-primary"
+          data-cy="verify-button"
+          @click.prevent="completeActivation"
+        >
+          Complete Activation
+        </a>
+
+        <p class="no-email">
+          Didn't receive the email? Check your spam folder or
+          <a class="link" href="/" @click.prevent="reset"> try again</a>.
         </p>
       </div>
     </div>
@@ -42,20 +120,107 @@
 
 <script>
 import AppMapLogo from '@/assets/appmap-full-logo.svg';
-import shieldIcon from '@/assets/shield-icon.svg';
-import VExternalLinkIcon from '@/assets/external-link.svg';
+import ShieldIcon from '@/assets/shield-icon.svg';
+import GitHubLogo from '@/assets/github-logo.svg';
+import GitLabLogo from '@/assets/gitlab-logo.svg';
+import EmailIcon from '@/assets/email.svg';
+import ExternalLinkIcon from '@/assets/external-link.svg';
+
+const GENERIC_ERROR_MSG = 'Something went wrong, please try again later.';
 
 export default {
   name: 'v-sign-in',
+
   components: {
     AppMapLogo,
-    shieldIcon,
-    VExternalLinkIcon,
+    ShieldIcon,
+    GitHubLogo,
+    GitLabLogo,
+    ExternalLinkIcon,
+    EmailIcon,
+  },
+
+  props: {
+    appmapServerUrl: {
+      type: String,
+      default: 'https://getappmap.com',
+    },
+  },
+
+  data() {
+    return {
+      email: '',
+      submitted: false,
+      verificationCode: '',
+      error: '',
+    };
   },
 
   methods: {
+    keyDown(event) {
+      if (event.key === 'Enter') {
+        if (event.srcElement.id === 'email-input') {
+          this.activateWithEmail();
+        } else if (event.srcElement.id === 'verification-code-input') {
+          this.completeActivation();
+        }
+      }
+    },
     signIn() {
       this.$root.$emit('sign-in');
+    },
+    reset() {
+      this.email = '';
+      this.error = '';
+      this.verificationCode = '';
+      this.submitted = false;
+    },
+    async activateWithEmail() {
+      const url = new URL('/api/activations', this.appmapServerUrl);
+      this.addParamsToUrl(url, { email: this.email });
+      const response = await fetch(url, { method: 'POST' });
+
+      if (response.status === 201) {
+        this.error = '';
+        this.submitted = true;
+      } else if (response.status < 500 && response.status >= 400) {
+        this.email = '';
+        this.error = this.generateInvalidFieldMsg('email address');
+      } else {
+        this.email = '';
+        this.error = GENERIC_ERROR_MSG;
+      }
+    },
+    async completeActivation() {
+      const url = new URL('/api/activations/verify', this.appmapServerUrl);
+      this.addParamsToUrl(url, {
+        email: this.email,
+        code: this.verificationCode,
+      });
+      const response = await fetch(url, { method: 'POST' });
+
+      if (response.status === 200) {
+        try {
+          const data = await response.json();
+          this.$root.$emit('activate', data.api_key);
+        } catch (error) {
+          this.submitted = false;
+          this.error = GENERIC_ERROR_MSG;
+        }
+      } else {
+        this.verificationCode = '';
+        this.submitted = false;
+        this.error =
+          response.status < 500 && response.status >= 400
+            ? this.generateInvalidFieldMsg('verification code')
+            : (this.error = GENERIC_ERROR_MSG);
+      }
+    },
+    addParamsToUrl(url, params) {
+      Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]));
+    },
+    generateInvalidFieldMsg(field) {
+      return `Invalid ${field}, please try again.`;
     },
   },
 };
@@ -66,23 +231,38 @@ export default {
   background-color: $black;
   font-family: $appland-text-font-family;
   color: $white;
-  font-family: $appland-text-font-family;
+  font-size: 1rem;
   width: auto;
   height: 100%;
   padding: 1.5rem;
   line-height: 1.4rem;
-  .content-wrap {
-    margin: 2rem 0;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    gap: 2rem;
-    h1 {
-      margin: 0 0 0.75rem 0;
+
+  h1 {
+    line-height: 2rem;
+  }
+
+  a.link {
+    color: $brightblue;
+    text-decoration: none;
+    transition: $transition;
+
+    svg {
+      transition: $transition;
+      margin: 0 0.3rem;
+      width: 90%;
+      max-width: 14px;
+      height: 14px;
+      fill: $brightblue;
     }
-    p {
-      margin: 0;
+
+    &:hover {
+      transition: $transition;
+      color: lighten($brightblue, 15%);
+
+      svg {
+        transition: $transition;
+        fill: lighten($brightblue, 15%);
+      }
     }
   }
 
@@ -93,133 +273,147 @@ export default {
     color: $white;
     text-decoration: none;
     text-align: center;
-    width: 80%;
+    width: 90%;
     justify-content: center;
+    display: flex;
+
+    span {
+      display: flex;
+      align-items: center;
+
+      .small-logo {
+        width: 17px;
+        margin: 0 0.5rem;
+      }
+    }
+
     &.btn-primary {
       background-color: desaturate($powderblue, 20);
       font-weight: 600;
       margin: 0 1.25rem;
+
       &:hover {
         background-color: darken($powderblue, 15);
+        transition: $transition;
       }
     }
   }
-}
 
-.signin-not-ready {
-  &:hover {
-    cursor: pointer;
-  }
-  .accordion-toggle {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    margin-bottom: 0.5rem;
-    justify-content: center;
-    &.closed svg {
-      transform: rotate(-90deg);
-    }
-    svg {
-      transition: $transition;
-      width: 1rem;
-      path {
-        fill: desaturate($powderblue, 20);
-      }
+  @media (max-width: 300px) {
+    .btn {
+      flex-direction: column;
+      align-items: center;
     }
   }
-  a {
-    color: desaturate($powderblue, 20);
-    text-decoration: none;
-    &:hover {
-      text-decoration: underline;
-    }
-    svg {
-      path {
-        fill: desaturate($powderblue, 20);
-      }
-      margin-left: 0.5rem;
-    }
-  }
-}
 
-.signin-buttons {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  align-items: center;
-  a {
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
-    align-items: center;
-  }
-}
-.signin-ctas {
-  display: flex;
-  flex-direction: column;
-  gap: 0.25rem;
-  list-style-type: none;
-  font-family: $appland-text-font-family;
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-  li {
-    background-color: #091118;
-    border-radius: 0.5rem;
-    padding: 0.8rem 1rem;
-    font-weight: 600;
-    transition: $transition;
-    a {
-      color: desaturate($powderblue, 30);
-      text-decoration: none;
-      display: flex;
-    }
-    &:hover {
-      background-color: #12222f;
-      a {
-        text-decoration: underline;
-      }
-    }
-  }
-}
-
-.your-data {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 1rem;
-  padding: 1rem;
-  border: 1px solid $gray3;
-  border-radius: 0.5rem;
-  font-size: 0.9rem;
-  line-height: 1.4rem;
-  svg {
+  .content-wrap {
+    margin: 2rem 0;
     width: 100%;
-    max-width: 33px;
-    height: 42px;
-  }
-  p {
-    margin: 0;
-  }
-  a {
-    color: $brightblue;
     display: flex;
-    align-items: center;
-    gap: 0.25rem;
-    svg {
-      width: 100%;
-      max-width: 14px;
-      height: 14px;
-      fill: $brightblue;
-    }
-  }
-}
-
-@media (max-width: 365px) {
-  .your-data {
     flex-direction: column;
     align-content: center;
-    text-align: center;
+    gap: 1.2rem;
+
+    .sidebar-title {
+      h1 {
+        margin: 0 0 0.5rem 0;
+      }
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+
+  .signin-buttons {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    margin-bottom: 1rem;
+
+    .or-divider-container {
+      display: flex;
+      width: 90%;
+
+      .or-divider {
+        margin: 0.5rem;
+        color: $gray4;
+      }
+
+      .line {
+        align-self: center;
+        height: 0px;
+        width: 90%;
+        border-top: 1px solid $gray4;
+      }
+    }
+
+    .error {
+      color: $hotpink;
+      font-size: 0.9rem;
+    }
+
+    .accept-tos {
+      font-size: 0.9rem;
+      width: 90%;
+    }
+  }
+
+  input[type='email'],
+  input[type='text'] {
+    width: 90%;
+    color: $white;
+    font-size: 1rem;
+    background-color: inherit;
+    border: 1px solid $gray5;
+    padding: 0.75rem;
+    border-radius: 10px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .your-data {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+    border: 1px solid $gray3;
+    border-radius: 0.5rem;
+    font-size: 0.9rem;
+    line-height: 1.2rem;
+
+    svg {
+      width: 100%;
+      max-width: 33px;
+      height: 42px;
+    }
+
+    .your-data-text p {
+      margin: 0.5rem 0.2rem;
+    }
+  }
+
+  @media (max-width: 365px) {
+    .your-data {
+      flex-direction: column;
+      align-content: center;
+      text-align: center;
+    }
+  }
+
+  .finish-activation {
+    .sidebar-title {
+      margin-bottom: 2rem;
+    }
+
+    .no-email {
+      font-size: 0.9rem;
+      line-height: 1rem;
+    }
   }
 }
 </style>
