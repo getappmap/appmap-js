3368d5fcd06f10c97ccff32bae9241a703fab40f
Adam Trotta <ahtrotta@gmail.com>
2024-03-06 14:37:07 -0500

    feat: Users can activate with an email (#1620)

--- packages/components/tests/e2e/specs/sidebarSignIn.spec.js
+++ packages/components/tests/e2e/specs/sidebarSignIn.spec.js
@@ -1,24 +1,83 @@
-describe('Sidebar sign-in page', () => {
+describe('Sidebar activation page', () => {
+  const INVALID_EMAIL_MSG = 'Invalid email address, please try again.';
+  const GENERIC_ERROR_MSG = 'Something went wrong, please try again later.';
+  const TEST_EMAIL = 'fake@test.net';
+
   beforeEach(() => {
     cy.visit('http://localhost:6006/iframe.html?args=&id=pages-vs-code--sign-in&viewMode=story');
   });
 
   it('displays a title', () => {
-    cy.get('[data-cy="title"]').should('have.text', 'Sign in');
+    cy.get('[data-cy="title"]').should('have.text', 'Activate AppMap');
   });
 
-  it('has a sign-in button', () => {
-    cy.get('[data-cy="sign-in-button"]').should('have.text', 'Sign in');
+  it('has activation buttons', () => {
+    cy.get('[data-cy="github-activation-button"]')
+      .should('contain.text', 'Activate with')
+      .should('contain.text', 'GitHub');
+    cy.get('[data-cy="gitlab-activation-button"]')
+      .should('contain.text', 'Activate with')
+      .should('contain.text', 'GitLab');
+    cy.get('[data-cy="email-activation-button"]')
+      .should('contain.text', 'Activate with')
+      .should('contain.text', 'Email');
   });
 
   it('informs the user that we do not access their source code', () => {
     const expectedText1 =
-      'Authentication with GitHub or GitLab is used solely for issuing a license. AppMap runs in';
+      'Your email address is used solely to issue you an AppMap license. Your source code and';
     const expectedText2 =
-      'your code editor, so your AppMaps and your source code stay on your machine.';
+      'AppMaps stay on your machine when you use AppMap diagrams and runtime analysis. Consult';
 
-    const textElement = cy.get('[data-cy="your-data-text"]');
-    textElement.should('contain.text', expectedText1);
-    textElement.should('contain.text', expectedText2);
+    cy.get('[data-cy="your-data-text"]')
+      .should('contain.text', expectedText1)
+      .should('contain.text', expectedText2);
+  });
+
+  it('shows an error message for an invalid email', () => {
+    cy.intercept('POST', 'https://getappmap.com/api/activations*', {
+      statusCode: 422,
+    });
+    cy.get('[data-cy="email-activation-button"]').click();
+    cy.get('.error').should('contain.text', INVALID_EMAIL_MSG);
+  });
+
+  it('shows an error message for a server error', () => {
+    cy.intercept('POST', 'https://getappmap.com/api/activations*', {
+      statusCode: 500,
+    });
+    cy.get('[data-cy="email-activation-button"]').click();
+    cy.get('.error').should('contain.text', GENERIC_ERROR_MSG);
+  });
+
+  it('shows verification code page upon successful email submission', () => {
+    cy.intercept('POST', 'https://getappmap.com/api/activations*', {
+      statusCode: 201,
+    });
+    cy.get('#email-input').type(TEST_EMAIL);
+    cy.get('[data-cy="email-activation-button"]').click();
+    cy.get('.finish-activation').should('exist');
+  });
+
+  it('allows the user to retry from the verification code page', () => {
+    cy.intercept('POST', 'https://getappmap.com/api/activations*', {
+      statusCode: 201,
+    });
+    cy.get('#email-input').type(TEST_EMAIL).type('{enter}');
+    cy.get('.no-email .link').click();
+    cy.get('#email-input').should('have.value', '');
+  });
+
+  it('shows an error message with incorrect verification code', () => {
+    cy.intercept('POST', 'https://getappmap.com/api/activations*', {
+      statusCode: 201,
+    });
+    cy.get('#email-input').type(TEST_EMAIL);
+    cy.get('[data-cy="email-activation-button"]').click();
+    cy.intercept('POST', 'https://getappmap.com/api/activations/verify*', {
+      statusCode: 422,
+    });
+    cy.get('#verification-code-input').type('1234').type('{enter}');
+    cy.get('.error').should('contain.text', 'Invalid verification code, please try again.');
   });
 });
