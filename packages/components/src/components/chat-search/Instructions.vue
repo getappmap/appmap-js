0fb59d5f4eaee32c0d7496a5b86a2c78d83b3772
Adam Trotta <ahtrotta@gmail.com>
2024-03-11 15:39:50 -0400

    refactor: Move Navie context status into separate component

--- packages/components/src/components/chat-search/Instructions.vue
+++ packages/components/src/components/chat-search/Instructions.vue
@@ -2,81 +2,7 @@
   <div class="instructions">
     <div class="instructions__container">
       <div class="instructions__alerts">
-        <v-alert-box v-if="!appmapYmlPresent" level="error" data-cy="alert-no-config">
-          <div class="instructions__stats__header">
-            No AppMap configuration is present in this workspace.
-          </div>
-          <div class="instructions__stats__sub-header">
-            <p>
-              This typically means that the recording agent is not installed or has not yet been run
-              for the first time.
-            </p>
-            <p class="mb-0">
-              To resolve this,
-              <a href="#" data-cy="install-guide" @click.prevent="openInstallInstructions">
-                refer to the instructions in the installation guide.
-              </a>
-            </p>
-          </div>
-        </v-alert-box>
-
-        <v-alert-box
-          v-if="appmapStats && appmapStats.numAppMaps"
-          level="info"
-          data-cy="alert-success"
-        >
-          <div class="instructions__stats__header">
-            There are {{ appmapStats.numAppMaps }} AppMap recordings in this workspace.
-          </div>
-          <div class="instructions__stats__sub-header">
-            From these recordings, AppMap has observed:
-          </div>
-          <div class="instructions__stats__details" data-cy="stats">
-            <div class="instructions__stats__details-row">
-              <div class="stats-number">
-                <strong>{{ appmapStats.packages.length }}</strong>
-              </div>
-              packages
-            </div>
-            <div class="instructions__stats__details-row">
-              <div class="stats-number">
-                <strong>{{ appmapStats.classes.length }}</strong>
-              </div>
-              classes
-            </div>
-            <div class="instructions__stats__details-row" v-if="appmapStats.routes.length">
-              <div class="stats-number">
-                <strong>{{ appmapStats.routes.length }}</strong>
-              </div>
-              distinct HTTP routes
-            </div>
-            <div class="instructions__stats__details-row" v-if="appmapStats.tables.length">
-              <div class="stats-number">
-                <strong>{{ appmapStats.tables.length }}</strong>
-              </div>
-              distinct SQL tables
-            </div>
-          </div>
-          <div>
-            Navie improves with every runtime recording. To learn more about the different recording
-            methods available, refer to the
-            <a href="#" @click.prevent="openRecordInstructions" data-cy="record-guide">
-              recording guide.
-            </a>
-          </div>
-        </v-alert-box>
-
-        <v-alert-box v-else-if="appmapYmlPresent" level="warning" data-cy="alert-no-data">
-          <div class="instructions__stats__header">
-            No AppMap recordings were found in this workspace.
-          </div>
-          <div class="instructions__stats__sub-header">
-            To resolve this,
-            <a href="#" @click.prevent="openRecordInstructions" data-cy="record-guide">
-              refer to the instructions in the recording guide.
-            </a>
-          </div>
-        </v-alert-box>
+        <v-context-status :appmap-stats="appmapStats" :appmap-yml-present="appmapYmlPresent" />
       </div>
 
       <div class="instructions__appmaps">
@@ -120,7 +46,7 @@
 // @ts-nocheck
 import Vue, { PropType } from 'vue';
 import VAppmapListItem from '@/components/AppmapListItem.vue';
-import VAlertBox from '@/components/AlertBox.vue';
+import VContextStatus from '@/components/chat-search/ContextStatus.vue';
 
 type AppmapStats = {
   numAppMaps: number;
@@ -143,7 +69,7 @@ export default Vue.extend({
 
   components: {
     VAppmapListItem,
-    VAlertBox,
+    VContextStatus,
   },
 
   props: {
@@ -153,12 +79,6 @@ export default Vue.extend({
   },
 
   methods: {
-    openInstallInstructions(): void {
-      this.$root.$emit('open-install-instructions');
-    },
-    openRecordInstructions(): void {
-      this.$root.$emit('open-record-instructions');
-    },
     showAppMapTree(): void {
       this.$root.$emit('show-appmap-tree');
     },
@@ -203,8 +123,6 @@ export default Vue.extend({
     display: flex;
     flex-direction: column;
     border-radius: $border-radius;
-    // min-height: 1rem;
-    // overflow: hidden;
 
     & > :nth-child(odd) {
       background-color: rgba(128, 128, 255, 0.1);
@@ -214,49 +132,6 @@ export default Vue.extend({
     }
     & > *:last-child {
       border-bottom: none;
-    }
-  }
-
-  &__stats {
-    background-color: #181c28;
-    padding: 1.75rem;
-
-    h2 {
-      margin-bottom: 1.25rem;
-      margin-top: 0;
-    }
-
-    &__header {
-      margin-bottom: 1.25rem;
-      font-weight: 600;
-    }
-
-    &__sub-header {
-      .divider {
-        margin: 0 0.75rem;
-      }
-    }
-
-    &__details {
-      width: fit-content;
-      padding: 1rem 0;
-
-      &-row {
-        display: flex;
-        padding: 0.2rem 12rem 0.2rem 0.75rem;
-
-        .stats-number {
-          width: 30px;
-        }
-
-        &:nth-child(odd) {
-          background-color: rgba(128, 128, 255, 0.1);
-        }
-      }
-    }
-
-    &__visualizations-info {
-      margin-top: 1.5rem;
     }
   }
 
