95a74c87d87cbdc30c9f7518c634087847558e72
Dustin Byrne <dustinsbyrne@gmail.com>
2024-03-12 16:06:41 -0400

    feat: Update AppMap config and stats prompts
    They now reference multiple projects within a workspace.

--- packages/navie/src/project-info.ts
+++ packages/navie/src/project-info.ts
@@ -19,24 +19,8 @@ export type ProjectInfo = {
   appmapStats: AppMapStats;
 };
 
-export type ProjectInfoV1 = ProjectInfo;
-export type ProjectInfoV2 = { [key: string]: ProjectInfo };
-export type ProjectInfoResponse = ProjectInfoV1 | ProjectInfoV2;
+export type ProjectInfoResponse = ProjectInfo | Array<ProjectInfo>;
 
 export type ProjectInfoProvider = (
   request: ProjectInfoRequest
 ) => Promise<ProjectInfoResponse | undefined>;
-
-export function isV2ProjectInfoResponse(
-  projectInfo: ProjectInfoResponse
-): projectInfo is ProjectInfoV2 {
-  return Object.values(projectInfo).every((info) => 'appmapStats' in info);
-}
-
-export function toV2ProjectInfoResponse(projectInfo: ProjectInfoResponse): ProjectInfoV2 {
-  if (isV2ProjectInfoResponse(projectInfo)) {
-    return projectInfo;
-  }
-
-  return { project: projectInfo };
-}
