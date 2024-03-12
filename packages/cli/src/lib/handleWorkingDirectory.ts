df5fb6fe17c1bedc9bb674ffdda18f0a12caaa93
Dustin Byrne <dustinsbyrne@gmail.com>
2024-03-12 11:50:38 -0400

    refactor: prettier

--- packages/cli/src/lib/handleWorkingDirectory.ts
+++ packages/cli/src/lib/handleWorkingDirectory.ts
@@ -1,5 +1,5 @@
-import { join } from "path";
-import { setConfiguration } from "../rpc/configuration";
+import { join } from 'path';
+import { setConfiguration } from '../rpc/configuration';
 
 export function handleWorkingDirectory(directory?: string) {
   if (directory) process.chdir(directory);
