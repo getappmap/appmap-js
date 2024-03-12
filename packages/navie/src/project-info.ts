ad11a3a084c35ab0df51b1508b42db31355d2612
Dustin Byrne <dustinsbyrne@gmail.com>
2024-03-12 16:05:52 -0400

    feat: Support multi-project ProjectInfo
    This change is backward compatible with previous definitions of
    ProjectInfo where only a single project is expected.

--- packages/navie/src/project-info.ts
+++ packages/navie/src/project-info.ts
@@ -14,11 +14,29 @@ export type AppMapStats = {
   numAppMaps: number;
 };
 
-export type ProjectInfoResponse = {
-  appmapConfig: AppMapConfig;
+export type ProjectInfo = {
+  appmapConfig?: AppMapConfig;
   appmapStats: AppMapStats;
 };
+
+export type ProjectInfoV1 = ProjectInfo;
+export type ProjectInfoV2 = { [key: string]: ProjectInfo };
+export type ProjectInfoResponse = ProjectInfoV1 | ProjectInfoV2;
 
 export type ProjectInfoProvider = (
   request: ProjectInfoRequest
 ) => Promise<ProjectInfoResponse | undefined>;
+
+export function isV2ProjectInfoResponse(
+  projectInfo: ProjectInfoResponse
+): projectInfo is ProjectInfoV2 {
+  return Object.values(projectInfo).every((info) => 'appmapStats' in info);
+}
+
+export function toV2ProjectInfoResponse(projectInfo: ProjectInfoResponse): ProjectInfoV2 {
+  if (isV2ProjectInfoResponse(projectInfo)) {
+    return projectInfo;
+  }
+
+  return { project: projectInfo };
+}
