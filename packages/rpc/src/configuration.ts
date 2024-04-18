1f38aa48348ba5eae35a59fb9a8c3c2ab853d404
Kevin Gilpin <kgilpin@gmail.com>
2024-04-18 15:12:33 -0400

    feat: Define v2.configuration.get+set

--- packages/rpc/src/configuration.ts
+++ packages/rpc/src/configuration.ts
@@ -16,4 +16,22 @@ export namespace ConfigurationRpc {
       };
     }
   }
+
+  export namespace V2 {
+    export namespace Set {
+      export const Method = 'v2.configuration.set';
+      export type Params = V1.Set.Params & {
+        projectDirectories: string[];
+      };
+      export type Response = void;
+    }
+
+    export namespace Get {
+      export const Method = 'v2.configuration.get';
+      export type Params = undefined;
+      export type Response = V1.Get.Response & {
+        projectDirectories: string[];
+      };
+    }
+  }
 }
