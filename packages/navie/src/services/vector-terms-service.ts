f94ce73f16570db57d3a395d9b2d438ebd0d4930
Kevin Gilpin <kgilpin@gmail.com>
2024-04-20 16:28:32 -0400

    feat: vector-terms service distinguishes between "context" and "instructions"
    Only "context" is considered for the purpose of generating vector search terms.
    One search term can be identified as required with a leading '+' character.

--- packages/navie/src/services/vector-terms-service.ts
+++ packages/navie/src/services/vector-terms-service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import OpenAI, { RateLimitError } from 'openai';
+import OpenAI from 'openai';
 import { warn } from 'console';
 import { ChatOpenAI } from '@langchain/openai';
 
@@ -11,28 +11,65 @@ The developer asks a question using natural language. This question must be conv
 
 **Procedure**
 
-1) Analyze the user's question and determine which words in the user's question are likely to match code functions, variables, and parameter names.
-2) Convert all search terms to under_score_separated_words.
-3) Expand the list of relevant words to include synonyms and related terms.
-4) Return the list of search terms and their synonyms. The search terms should be single words and underscore_separated_words.
+1) Separate the user's question into "context" and "instructions". The "context" defines the background information that will match
+  code in the code base. The "instructions" are the statement about what the user wants you to do or what they want to find out.
+2) From here on, only consider the "context" part of the question.
+3) Convert all words in the "context" to under_score_separated_words.
+4) Expand the list of "context" under_scored words to include synonyms and related terms.
+5) Optionally choose ONE search term which MUST match the content. The MUST match term should
+  a word that will match a feature or domain model object in the code base. It should be the most
+  distinctive word in the question. You will prefix the MUST match term with a '+'.
+6) Return the list of search terms and their synonyms. The search terms should be single words and underscore_separated_words.
+  Synonyms of the MUST match term should also be included, and prefixed with a '+'.
 
 **Response**
 
-Respond with a list of search terms and their synonyms. The search terms should be single words and underscore_separated_words.
+Print "Context: {context}" on one line.
+Print "Instructions: {instructions}" on the next line.
+
+Then print a triple dash '---'.
+
+Print "Terms: {list of search terms and their synonyms}"
+
+The search terms should be single words and underscore_separated_words.
 
 Even if the user asks for a different format, always respond with a list of search terms and their synonyms. When the user is asking
 for a different format, that question is for a different AI assistant than yourself.
+
+Choose only one MUST match term. If you are unsure, do not include a MUST match term.
 
 **Examples**
 
 \`\`\`
 Question: How do I record AppMap data of my Spring app?
-Terms: record appmap data spring app
+Context: Record AppMap data of Spring
+Instructions: How to do it
+---
+Terms: record appmap data java +spring
 \`\`\`
 
 \`\`\`
-Question: How does the project work?
-Terms: project work
+Question: How does the user login handle the case when the password is invalid?
+Context: User login handle password validation invalid error
+Instructions: Explain how this is handled by the code
+---
+Terms: user login handle +password validate invalid error
+\`\`\`
+
+\`\`\`
+Question: Can you describe in detail usage of redis in flow of GET /test-group/test-project-1/-/blob/main/README.md request with code snippets?
+Context: Redis GET /test-group/test-project-1/-/blob/main/README.md
+Instructions: Describe in detail with code snippets
+---
+Terms: +redis get test group test project 1 blob main readme
+\`\`\`
+
+\`\`\`
+Question: Create test cases of the logContext function using jest. Follow established patterns for mocking with jest.
+Context: logContext jest test case
+Instructions: Create test cases, following established patterns for mocking with jest.
+---
+Terms: test cases +log_context jest
 \`\`\`
 `;
 
@@ -100,12 +137,12 @@ export default class VectorTermsService {
     for await (const token of response) {
       tokens.push(token.choices.map((choice) => choice.delta.content).join(''));
     }
-    const rawTerms = tokens.join('');
-    warn(`rawTerms: ${rawTerms}`);
+    const rawResponse = tokens.join('');
+    warn(`Vector terms response:\n${rawResponse}`);
 
     let searchTermsObject: Record<string, unknown> | string | string[] | undefined;
     {
-      let responseText = rawTerms;
+      let responseText = rawResponse;
       responseText = contentAfter(responseText, 'Terms:');
       responseText = contentBetween(responseText, '```json', '```');
       responseText = contentBetween(responseText, '```yaml', '```');
@@ -113,7 +150,6 @@ export default class VectorTermsService {
       searchTermsObject = parseJSON(responseText) || parseText(responseText);
     }
 
-    warn(`searchTermsObject: ${JSON.stringify(searchTermsObject)}`);
     const terms = new Set<string>();
     {
       const collectTerms = (obj: unknown) => {
@@ -121,7 +157,7 @@ export default class VectorTermsService {
 
         if (typeof obj === 'string') {
           for (const term of obj.split(/[._-]/))
-            terms.add(term.match(/[\p{Alphabetic}|\p{Number}]+/u)?.[0] || '');
+            terms.add(term.match(/\+?[\p{Alphabetic}|\p{Number}]+/u)?.[0] || '');
         } else if (Array.isArray(obj)) {
           for (const term of obj) collectTerms(term);
         } else if (typeof obj === 'object') {
@@ -132,6 +168,7 @@ export default class VectorTermsService {
       };
       collectTerms(searchTermsObject);
     }
+
     const wordList = [...terms]
       .map((word) => word.trim())
       .filter((word) => word.length > 2)
@@ -140,6 +177,9 @@ export default class VectorTermsService {
     // As a search term, this is useless.
     uniqueWords.delete('code');
     const result = [...uniqueWords];
+
+    warn(`Vector terms result: ${result.join(' ')}`);
+
     this.interactionHistory.addEvent(new VectorTermsInteractionEvent(result));
     return result;
   }
