3a12c6897f855a5805f34341a60a0a4dd2ac3794
Dustin Byrne <dustinsbyrne@gmail.com>
2024-05-07 13:46:15 -0400

    fix: Prevent SIGPIPE in large projects
    I'm not totally sure what the cause was, but when dealing with a lot of
    output the exec'd process would be killed via SIGPIPE. When listing git
    project files, buffering is now handled directly by exec.

--- packages/cli/tests/unit/fulltext/listGitProjectFiles.spec.ts
+++ packages/cli/tests/unit/fulltext/listGitProjectFiles.spec.ts
@@ -1,9 +1,10 @@
-import * as executeCommandModule from '../../../src/lib/executeCommand';
 import * as utilsModule from '../../../src/utils';
 import listGitProjectFiles from '../../../src/fulltext/listGitProjectFIles';
+import * as childProcess from 'node:child_process';
+import type { ChildProcess } from 'node:child_process';
 
 // Mock dependencies
-jest.mock('../../../src/lib/executeCommand');
+jest.mock('node:child_process');
 jest.mock('../../../src/utils', () => ({
   ...jest.requireActual('../../../src/utils'),
   isFile: jest.fn(),
@@ -16,14 +17,19 @@ describe('listGitProjectFiles', () => {
     // Clear all mocks before each test
     jest.clearAllMocks();
     // Setup default return values for mocked functions
-    jest.mocked(executeCommandModule.executeCommand).mockImplementation(async (cmd) => {
+    jest.mocked(childProcess.exec).mockImplementation((cmd: string, _options, callback) => {
+      const cb = callback as (_: unknown, __: unknown) => void;
       if (cmd.includes('ls-files')) {
-        return 'file1.js\nfile2.png';
+        cb(null, { stdout: 'file1.js\nfile2.png' });
+      } else if (cmd.includes('status --porcelain')) {
+        cb(null, {
+          stdout: '?? newFile.ts\nM modifiedFile.js',
+        });
+      } else {
+        cb(null, { stdout: '' });
       }
-      if (cmd.includes('status --porcelain')) {
-        return '?? newFile.ts\nM modifiedFile.js';
-      }
-      return '';
+
+      return {} as ChildProcess;
     });
     jest.mocked(utilsModule.isFile).mockResolvedValue(true);
   });
