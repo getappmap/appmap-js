66f5aa669f7b956cb86987499c5efd54a4ca8d3e
Rafał Rzepecki <divided.mind@gmail.com>
2024-05-10 19:22:34 +0200

    feat: Add 'navie' CLI command for one-shot Navie interactions

--- packages/cli/src/cmds/index/rpc.ts
+++ packages/cli/src/cmds/index/rpc.ts
@@ -1,25 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { warn } from 'node:console';
+
 import yargs from 'yargs';
-import chalk from 'chalk';
-import { ContextV2, Help, ProjectInfo, Agents } from '@appland/navie';
+
 import { loadConfiguration } from '@appland/client';
 
 import { verbose } from '../../utils';
-import { log } from 'console';
 import { search } from '../../rpc/search/search';
 import appmapFilter from '../../rpc/appmap/filter';
 import { RpcHandler } from '../../rpc/rpc';
 import metadata from '../../rpc/appmap/metadata';
 import sequenceDiagram from '../../rpc/appmap/sequenceDiagram';
 import { explainHandler, explainStatusHandler } from '../../rpc/explain/explain';
+import { buildNavieProvider, commonNavieArgsBuilder as navieBuilder } from '../navie';
 import RPCServer from './rpcServer';
 import appmapData from '../../rpc/appmap/data';
 import { appmapStatsV1, appmapStatsV2 } from '../../rpc/appmap/stats';
-import LocalNavie from '../../rpc/explain/navie/navie-local';
-import RemoteNavie from '../../rpc/explain/navie/navie-remote';
-import { InteractionEvent } from '@appland/navie/dist/interaction-history';
 import { configureRpcDirectories } from '../../lib/handleWorkingDirectory';
 import {
   getConfigurationV1,
@@ -28,162 +23,39 @@ import {
   setConfigurationV2,
 } from '../../rpc/configuration';
 import detectCodeEditor from '../../lib/detectCodeEditor';
-import detectAIEnvVar, { AI_KEY_ENV_VARS } from './aiEnvVar';
 
 export const command = 'rpc';
 export const describe = 'Run AppMap JSON-RPC server';
 
-export const builder = (args: yargs.Argv) => {
-  args.option('directory', {
-    describe: 'program working directory',
-    type: 'string',
-    alias: 'd',
-    array: true,
-  });
-  args.option('port', {
-    describe:
-      'port to listen on for JSON-RPC requests. Use port 0 to let the OS choose a port. The port number will be printed to stdout on startup.',
-    type: 'number',
-    alias: 'p',
-  });
-  args.option('navie-provider', {
-    describe: 'navie provider to use',
-    type: 'string',
-    choices: ['local', 'remote'],
-  });
-  args.option('log-navie', {
-    describe: 'Log Navie events to stderr',
-    boolean: true,
-    default: false,
-  });
-  args.option('ai-option', {
-    describe:
-      'Specify an extended option to the AI provider, in the form of a key=value pair. May be repeated.',
-    type: 'string',
-    array: true,
-  });
-
-  args.option('agent-mode', {
-    describe: `Agent mode which to run the Navie AI. The agent can also be controlled by starting the question with '@<agent> '.`,
-    choices: Object.values(Agents).map((agent) => agent.toLowerCase()),
-  });
-
-  args.option('code-editor', {
-    describe:
-      'Active code editor. This information is used to tune the @help responses. If unspecified, the code editor may be picked up from environment variables APPMAP_CODE_EDITOR, TERM_PROGRAM and TERMINAL_EMULATOR.',
-    type: 'string',
-    // Allow this to be any string. The code editor brand name may be a clue to the language
-    // in use, or the user's intent.
-  });
-
-  return args.strict();
+export const builder = <T>(args: yargs.Argv<T>) => {
+  return navieBuilder(args)
+    .option('port', {
+      describe:
+        'port to listen on for JSON-RPC requests. Use port 0 to let the OS choose a port. The port number will be printed to stdout on startup.',
+      type: 'number',
+      alias: 'p',
+      default: 0,
+    })
+    .strict();
 };
 
-export const handler = async (argv) => {
+type GetArgv<T> = T extends yargs.Argv<infer A> ? A : never;
+type HandlerArguments = yargs.ArgumentsCamelCase<
+  GetArgv<ReturnType<typeof builder>> & { verbose?: boolean }
+>;
+
+export const handler = async (argv: HandlerArguments) => {
   verbose(argv.verbose);
 
-  const directories: string[] = [];
-  if (argv.directory) {
-    Array.isArray(argv.directory)
-      ? directories.push(...argv.directory)
-      : directories.push(argv.directory);
-  }
-
-  const { port, logNavie } = argv;
-  let aiOptions: string[] | undefined = argv.aiOption;
-  if (aiOptions) {
-    aiOptions = Array.isArray(aiOptions) ? aiOptions : [aiOptions];
-  }
-  const agentModeStr: string | undefined = argv.explainMode;
-  let agentMode: Agents | undefined;
-  if (agentModeStr) agentMode = agentModeStr as Agents;
-
+  const navie = buildNavieProvider(argv);
   let codeEditor: string | undefined = argv.codeEditor;
   if (!codeEditor) {
     codeEditor = detectCodeEditor();
-    if (codeEditor) log(`Detected code editor: ${codeEditor}`);
+    if (codeEditor) warn(`Detected code editor: ${codeEditor}`);
   }
 
-  const useLocalNavie = () => {
-    if (argv.navieProvider === 'local') {
-      log(`Using local Navie provider due to explicit --navie-provider=local option`);
-      return true;
-    }
-
-    if (argv.navieProvider === 'remote') {
-      log(`Using remote Navie provider due to explicit --navie-provider=remote option`);
-      return false;
-    }
-
-    const aiEnvVar = detectAIEnvVar();
-    if (aiEnvVar) {
-      log(`Using local Navie provider due to presence of environment variable ${aiEnvVar}`);
-      return true;
-    }
-
-    log(
-      `--navie-provider option not provided, and none of ${AI_KEY_ENV_VARS.join(
-        ' '
-      )} are available. Using remote Navie provider.`
-    );
-    return false;
-  };
-
-  const applyAIOptions = (navie: LocalNavie | RemoteNavie) => {
-    if (aiOptions) {
-      for (const option of aiOptions) {
-        const [key, value] = option.split('=');
-        if (key && value) {
-          navie.setOption(key, value);
-        }
-      }
-    }
-    if (agentMode) {
-      navie.setOption('explainMode', agentMode);
-    }
-  };
-
-  const buildLocalNavie = (
-    threadId: string | undefined,
-    contextProvider: ContextV2.ContextProvider,
-    projectInfoProvider: ProjectInfo.ProjectInfoProvider,
-    helpProvider: Help.HelpProvider
-  ) => {
-    const navie = new LocalNavie(threadId, contextProvider, projectInfoProvider, helpProvider);
-    applyAIOptions(navie);
-
-    let START: number | undefined;
-    const logEvent = (event: InteractionEvent) => {
-      if (!logNavie) return;
-
-      if (!START) START = Date.now();
-
-      const elapsed = Date.now() - START;
-      process.stderr.write(chalk.gray(`${elapsed}ms `));
-      process.stderr.write(chalk.gray(event.message));
-      process.stderr.write(chalk.gray('\n'));
-    };
-
-    navie.on('event', logEvent);
-    return navie;
-  };
-
-  const buildRemoteNavie = (
-    threadId: string | undefined,
-    contextProvider: ContextV2.ContextProvider,
-    projectInfoProvider: ProjectInfo.ProjectInfoProvider,
-    helpProvider: Help.HelpProvider
-  ) => {
-    loadConfiguration(false);
-    const navie = new RemoteNavie(threadId, contextProvider, projectInfoProvider, helpProvider);
-    applyAIOptions(navie);
-    return navie;
-  };
-
-  const navieProvider = useLocalNavie() ? buildLocalNavie : buildRemoteNavie;
-
   loadConfiguration(false);
-  await configureRpcDirectories(directories);
+  await configureRpcDirectories(argv.directory);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const rpcMethods: RpcHandler<any, any>[] = [
@@ -194,13 +66,13 @@ export const handler = async (argv) => {
     appmapData(),
     metadata(),
     sequenceDiagram(),
-    explainHandler(navieProvider, codeEditor),
+    explainHandler(navie, codeEditor),
     explainStatusHandler(),
     setConfigurationV1(),
     getConfigurationV1(),
     setConfigurationV2(),
     getConfigurationV2(),
   ];
-  const rpcServer = new RPCServer(port, rpcMethods);
+  const rpcServer = new RPCServer(argv.port, rpcMethods);
   rpcServer.start();
 };
