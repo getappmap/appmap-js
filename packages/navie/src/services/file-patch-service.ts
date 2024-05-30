22fdd3eb1304def6d063ea03f9e560d83b3f470b
Kevin Gilpin <kgilpin@gmail.com>
2024-05-30 18:03:40 -0400

    feat: Make @apply more effective
    Look for the most relevant chunk within the existing code.
    Use the LLM to apply the desired code update to that chunk.
    
    Rev the file patch service, which generates a patch file. But,
    it's not currently used because patching is so fragile.

--- packages/navie/src/services/file-patch-service.ts
+++ packages/navie/src/services/file-patch-service.ts
@@ -2,74 +2,99 @@ import { ChatOpenAI } from '@langchain/openai';
 import { warn } from 'console';
 import { readFile } from 'fs/promises';
 import OpenAI from 'openai';
-import { createPatch } from 'diff';
 
 import InteractionHistory from '../interaction-history';
-import trimFences from '../lib/trim-fences';
 
 export type FileUpdate = {
   file: string;
   content: string;
 };
 
-const SYSTEM_PROMPT = `**File Change Applier**
+const SYSTEM_PROMPT = `**File Patch Generator**
 
-Your job is to apply a code change to an existing file.
+Your job is to generate a patch to an existing file.
 
 **Inputs**
 
-- **existing-code**: The existing code in the file.
 - **suggested-change**: The code change to apply.
+- **existing-code**: The existing code in the file.
 
 **Output**
 
-Respond with the segment of existing code to which the suggested change should be applied.
+Respond with a patch in unified diff format that can be applied to the existing code to apply the suggested change.
 
-The existing code segment should be surrounded by context lines that are unchanged. The context lines
-should be the same as the original code.
+Emit:
 
-Try to include 3 lines of context at the beginning and end of the output code segment.
+* **Start line:** The line number where the change should be made.
+* **Existing line count:** The number of lines in the existing code (without removed lines).
+* **Suggested line count:** The number of lines in the suggested change (with added lines).
+* **Diff:** The diff of the existing code and the suggested change.
+
+Include 5 lines of context before and after the change.
 
 **Example**
 
 Input:
 <existing-code>
-class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
-
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @microposts = Rails.cache.fetch(@user.microposts_cache_key) do
-      @user.microposts.paginate(page: params[:page])
-    end
-  end
+1class MicropostsController < ApplicationController
+2  before_action :logged_in_user, only: [:create, :destroy]
+3  before_action :correct_user, only: :destroy
+4
+5  def create
+6    @micropost = current_user.microposts.build(micropost_params)
+7    @micropost.image.attach(params[:micropost][:image])
+8    if @micropost.save
+9      flash[:success] = "Micropost created!"
+10      redirect_to root_url
+11    else
+12      @feed_items = current_user.feed.paginate(page: params[:page])
+13      render "static_pages/home", status: :unprocessable_entity
+14    end
+15  end
+16
+17  def destroy
+18    @micropost.destroy
+19    flash[:success] = "Micropost deleted"
+25  end
+26
+27  private
+28
+29  def micropost_params
+30    params.require(:micropost).permit(:content, :image)
+31  end
 </existing-code>
 
 <suggested-change>
-  def index
-    @users = User.paginate(page: params[:page])
-    total_microposts = Micropost.count
-    Rails.logger.info "Total number of microposts available: #{total_microposts}"
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    if request.referrer.nil?
+      redirect_to root_url, status: :see_other
+    else
+      redirect_to request.referrer, status: :see_other
+    end
   end
+
+  private
 </suggested-change>
 
 Output:
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
-
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
+\`\`\`diff
+--- a/app/controllers/microposts_controller.rb
++++ b/app/controllers/microposts_controller.rb
+@@ -17,6 +17,11 @@ class MicropostsController < ApplicationController
+   def destroy
+     @micropost.destroy
+     flash[:success] = "Micropost deleted"
++    if request.referrer.nil?
++      redirect_to root_url, status: :see_other
++    else
++      redirect_to request.referrer, status: :see_other
++    end
+   end
+ 
+   private
+\`\`\`
 `;
 
 export default class FilePatchService {
@@ -79,7 +104,7 @@ export default class FilePatchService {
     public temperature: number
   ) {}
 
-  async apply(fileUpdate: FileUpdate): Promise<string[] | undefined> {
+  async generatePatch(fileUpdate: FileUpdate): Promise<string | undefined> {
     const fileContents = await readFile(fileUpdate.file, 'utf8');
 
     const openAI: ChatOpenAI = new ChatOpenAI({
@@ -87,13 +112,18 @@ export default class FilePatchService {
       temperature: this.temperature,
     });
 
+    const fileWithNumberedLines = fileContents
+      .split('\n')
+      .map((line, index) => `${index + 1}${line}`)
+      .join('\n');
+
     const messages: OpenAI.ChatCompletionMessageParam[] = [
       {
         content: SYSTEM_PROMPT,
         role: 'system',
       },
       {
-        content: `<existing-code>${fileContents}</existing-code>`,
+        content: `<existing-code>${fileWithNumberedLines}</existing-code>`,
         role: 'user',
       },
       {
@@ -115,23 +145,6 @@ export default class FilePatchService {
     }
     const rawResponse = tokens.join('');
     warn(`Code change response:\n${rawResponse}`);
-    const codeResponse = trimFences(rawResponse);
-
-    if (!codeResponse) return [`Failed to extract code response from file contents.`];
-
-    const startIndex = fileContents.indexOf(codeResponse);
-    if (startIndex === -1) return [`Failed to find code response in file contents.`];
-
-    let patch: string;
-    try {
-      patch = createPatch(fileUpdate.file, codeResponse, fileUpdate.content);
-      warn(`Patch:\n${patch}`);
-    } catch (err: any) {
-      if (err instanceof Error) return [`Failed to create patch: ${err.name} ${err.message}`];
-
-      return [`Failed to create patch`];
-    }
-
-    return [patch];
+    return rawResponse;
   }
 }
