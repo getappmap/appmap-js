5da727c9538afe67c8564de4e6ad0607b316a7b2
Kevin Gilpin <kgilpin@gmail.com>
2024-06-02 19:02:22 -0400

    feat: Make the LLM make assertions about the output
    Making it assert the correctness of the output may/should
    improve the actual correctness.

--- packages/cli/bin/solve.py
+++ packages/cli/bin/solve.py
@@ -214,10 +214,18 @@ Wrap these tags with a <change> tag that also includes a <file> tag with the fil
 The <original> tag should contain the original code that you want to change. Do not abbreviate
 existing code using ellipses or similar. 
 
-The <original> code should be at least 5 lines long.
+Always include an attribute "no-ellipsis" with the value "true" in the <original> tag.
+This should be a true statement about the tag.
 
-Do not output the entire original code if you only want to change a part of it. Output
-only the part that you want to change.
+The <original> code should contain an attribute that indicates about how many lines of context
+it contains. You should plan for this context to contain the code that should be modified, plus 
+three lines before and after it.
+
+Do not output the entire original code, or long functions, if you only want to change a part of it.
+Plan to output only the part that you want to change.
+
+If you need to make multiple changes to the same file, output multiple <change> tags.
+In the change, indicate the number of the change that this is, starting from 1.
 
 The <modified> tag should contain the modified code that you want to replace the original code with.
 Do not abbreviate the modified code using ellipses or similar. You must place the exact modified code
@@ -225,6 +233,9 @@ in the <modified> tag.
 
 You do not need to output the entire modified code if you only want to change a part of it. Output
 only the part that you want to change.
+
+Always include an attribute "no-ellipsis" with the value "true" in the <modified> tag.
+This should be a true statement about the tag.
 
 Both the original code and the output code must contain the proper indentation and formatting.
 For example, if the original code has 4 spaces of indentation, the output code must also have 4
@@ -236,19 +247,37 @@ The <original> and <modified> content should be wrapped in a CDATA section to av
 ## Example output
 
 <change>
-    <file>sympy/physics/vector/point.py</file>
-    <original><![CDATA[
-                except Exception:
-                    raise ValueError('Velocity of point ' + self.name + ' has not been'
-                                    ' defined in ReferenceFrame ' + frame.name)
-    ]]></original>
-    <modified><![CDATA[
-                    # Attempt to calculate the velocity based on position's time derivative
-                    return self.pos_from(self).dt(frame)
-                except Exception:
-                    raise ValueError('Velocity of point ' + self.name + ' has not been'
-                                    ' defined in ReferenceFrame ' + frame.name)
-    ]]></modified>
+    <file change-number-for-this-file="1">src/main/java/org/springframework/samples/petclinic/vet/Vet.java</file>
+    <original line-count="13" no-ellipsis="true"><![CDATA[
+    @JoinTable(
+        name = "vet_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
+    private Set<Specialty> specialties;
+
+    protected Set<Specialty> getSpecialtiesInternal() {
+        if (this.specialties == null) {
+            this.specialties = new HashSet<>();
+        }
+        return this.specialties;
+    }]]></original>
+    <modified no-ellipsis="true"><![CDATA[
+    @JoinTable(
+        name = "vet_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
+    private Set<Specialty> specialties;
+
+    private String address;
+
+    protected Set<Specialty> getSpecialtiesInternal() {
+        if (this.specialties == null) {
+            this.specialties = new HashSet<>();
+        }
+        return this.specialties;
+    }]]></modified>
 </change>
 """
             )
