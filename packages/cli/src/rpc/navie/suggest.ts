f47b19ba50e840f9922d4b6d9c88323f92eca100
Dustin Byrne <dustinsbyrne@gmail.com>
2024-08-19 16:11:42 -0400

    feat: Add an implementation of `v1.navie.suggest`

---
+++ packages/cli/src/rpc/navie/suggest.ts
@@ -0,0 +1,31 @@
+import { NavieRpc } from '@appland/rpc';
+import { RpcHandler } from '../rpc';
+import { INavieProvider } from '../explain/navie/inavie';
+
+// We don't want to support context lookups
+const NOP = () => Promise.resolve([]);
+
+function getSuggestions(
+  navieProvider: INavieProvider,
+  threadId: string
+): Promise<NavieRpc.V1.Suggest.Response> {
+  const navie = navieProvider(NOP, NOP, NOP);
+  return new Promise((resolve, reject) => {
+    navie
+      .on('token', (response: string) =>
+        resolve(JSON.parse(response) as NavieRpc.V1.Suggest.Response)
+      )
+      .on('error', reject)
+      .ask(threadId, '@suggest /nocontext /nohelp', undefined, undefined)
+      .catch(reject);
+  });
+}
+
+export function navieSuggestHandlerV1(
+  navieProvider: INavieProvider
+): RpcHandler<NavieRpc.V1.Suggest.Params, NavieRpc.V1.Suggest.Response> {
+  return {
+    name: NavieRpc.V1.Suggest.Method,
+    handler: (params) => getSuggestions(navieProvider, params.threadId),
+  };
+}
