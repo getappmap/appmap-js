8f5eede24639650e8d1c0a8b18d5bdb8095e390a
Kevin Gilpin <kgilpin@gmail.com>
2024-09-16 09:19:44 -0400

    fix: Prompt for fences in mixed content
    Sonnet is emitting a combination of text and
    JSON, without fencing the JSON. Prompt for fences when emitting
    JSON, and try and match simple JSON arrays and objects even
    without fences.

--- packages/navie/src/lib/parse-json.ts
+++ packages/navie/src/lib/parse-json.ts
@@ -1,20 +1,53 @@
 import { warn } from 'console';
 import trimFences from './trim-fences';
 
+/**
+ * Look within the text for opening and closing square brackets.
+ * If found, return the text within the brackets. This is a fallback
+ * in case proper JSON cannot be found within trimmed fences.
+ */
+function findArray(text: string): string | undefined {
+  const array = text.match(/\[.*\]/s);
+  return array ? array[0] : undefined;
+}
+
+/**
+ * Look within the text for opening and closing curly braces.
+ * If found, return the text within the braces. This is a fallback
+ * in case proper JSON cannot be found within trimmed fences.
+ */
+function findObject(text: string): string | undefined {
+  const object = text.match(/\{.*\}/s);
+  return object ? object[0] : undefined;
+}
+
 export default function parseJSON<T>(
   text: string,
   warnOnError: boolean,
   warning?: string | undefined
 ): T | undefined {
   const sanitizedTerms = trimFences(text);
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return JSON.parse(sanitizedTerms);
-  } catch (err) {
-    if (warnOnError) {
-      warn(warning || 'Failed to parse JSON');
-      warn(err);
+
+  const parse = (chunk: string): T | undefined => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return JSON.parse(chunk);
+    } catch (err) {
+      if (warnOnError) {
+        warn(warning || 'Failed to parse JSON');
+        warn(text);
+        warn(err);
+      }
+      return undefined;
     }
-    return undefined;
+  };
+
+  let result = parse(sanitizedTerms);
+  if (result === undefined) {
+    if (warnOnError) warn('Failed to parse JSON, attempting to parse as array or object.');
+
+    const chunk = findArray(sanitizedTerms) || findObject(sanitizedTerms);
+    if (chunk) result = parse(chunk);
   }
+  return result;
 }
