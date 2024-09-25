fc4dabf1f5de1a9ff3695f3397729a19fb299c95
Rafał Rzepecki <divided.mind@gmail.com>
2024-09-24 22:40:38 -0400

    feat: Support Gemini via Vertex AI
    Note: I needed to use a forked version of web-auth-library due to
    the latter only supporting ESM upstream.

--- packages/cli/src/cmds/index/aiEnvVar.ts
+++ packages/cli/src/cmds/index/aiEnvVar.ts
@@ -1,4 +1,9 @@
-export const AI_KEY_ENV_VARS = ['OPENAI_API_KEY', 'AZURE_OPENAI_API_KEY', 'ANTHROPIC_API_KEY'];
+export const AI_KEY_ENV_VARS = [
+  'GOOGLE_WEB_CREDENTIALS',
+  'OPENAI_API_KEY',
+  'AZURE_OPENAI_API_KEY',
+  'ANTHROPIC_API_KEY',
+];
 
 export default function detectAIEnvVar(): string | undefined {
   return Object.keys(process.env).find((key) => AI_KEY_ENV_VARS.includes(key));
