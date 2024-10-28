316a41ffe838084bbb0aae2c853cd72af152d63d
Kevin Gilpin <kgilpin@gmail.com>
2024-10-27 22:25:50 -0400

    chore: Update eslint rules

--- packages/search/eslint.config.mjs
+++ packages/search/eslint.config.mjs
@@ -1,68 +1,10 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import eslintComments from 'eslint-plugin-eslint-comments';
-import jest from 'eslint-plugin-jest';
-import promise from 'eslint-plugin-promise';
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-  ...compat.extends(
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:eslint-comments/recommended',
-    'plugin:jest/recommended',
-    'plugin:promise/recommended',
-    'plugin:prettier/recommended'
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      'eslint-comments': eslintComments,
-      jest,
-      promise,
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-        ...globals.jest,
-      },
-
-      ecmaVersion: 5,
-      sourceType: 'commonjs',
-
-      parserOptions: {
-        project: '/home/db/dev/applandinc/appmap-js/packages/search/tsconfig.json',
-      },
-    },
-
-    rules: {
-      'no-param-reassign': [
-        'error',
-        {
-          props: false,
-        },
-      ],
-
-      'no-console': 'off',
-      'no-debugger': 'off',
-      '@typescript-eslint/lines-between-class-members': 'off',
-      'no-restricted-syntax': 'off',
-      'import/no-cycle': 'off',
-      'prettier/prettier': ['error'],
-      'max-classes-per-file': 'off',
-    },
-  },
+  { ignores: ['built/', 'node_modules/', 'jest.config.js', 'test/fixtures'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
 ];
