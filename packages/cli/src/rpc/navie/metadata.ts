5e627d20ed0bc19a92938203641848f338ac1e37
Dustin Byrne <dustinsbyrne@gmail.com>
2024-12-05 15:18:30 -0500

    feat: Add welcome message completions
    The welcome message will now reflect the current task by looking at
    changes on the current branch and within the worktree.

--- packages/cli/src/rpc/navie/metadata.ts
+++ packages/cli/src/rpc/navie/metadata.ts
@@ -1,15 +1,117 @@
 import { NavieRpc } from '@appland/rpc';
 import { RpcHandler } from '../rpc';
+import configuration from '../configuration';
+import { getDiffLog, getWorkingDiff } from '../../lib/git';
+import { INavieProvider } from '../explain/navie/inavie';
+import { UserContext } from '@appland/navie';
 
-export function navieMetadataV1(): RpcHandler<
-  NavieRpc.V1.Metadata.Params,
-  NavieRpc.V1.Metadata.Response
-> {
+interface WelcomeSuggestion {
+  activity: string;
+  suggestions: string[];
+}
+
+// We don't want to support context lookups
+const NOP = () => Promise.resolve([]);
+
+function parseWelcomeSuggestion(response: string): WelcomeSuggestion | undefined {
+  try {
+    // The only time the JSON is parsable is if Navie has sent the welcome suggestion.
+    // We don't need to validate the object.
+    return JSON.parse(response) as unknown as WelcomeSuggestion;
+  } catch {
+    return undefined;
+  }
+}
+
+async function getWelcomeSuggestion(
+  navieProvider: INavieProvider,
+  diffs: string[]
+): Promise<WelcomeSuggestion | undefined> {
+  const navie = navieProvider(NOP, NOP, NOP);
+  const userContext: UserContext.ContextItem[] = diffs.map((diff) => ({
+    type: 'code-selection',
+    content: diff,
+  }));
+  return new Promise((resolve, reject) => {
+    let buffer = '';
+    navie
+      .on('token', (token: string) => (buffer += token))
+      .on('complete', () => resolve(parseWelcomeSuggestion(buffer)))
+      .on('error', reject)
+      .ask(undefined, '@welcome', userContext, undefined)
+      .catch(reject);
+  });
+}
+
+async function getChangeDiffs(projectDirectories: string[]): Promise<string[]> {
+  const diffs = await Promise.allSettled(
+    [
+      projectDirectories.map((d) => getDiffLog('HEAD', undefined, d)),
+      projectDirectories.map((d) => getWorkingDiff(d)),
+    ].flat()
+  );
+  return diffs
+    .filter((result): result is PromiseFulfilledResult<string> => result.status === 'fulfilled')
+    .map((result) => result.value);
+}
+
+async function getWelcomeMessage(navieProvider: INavieProvider): Promise<string> {
+  const result: string[] = ["### Hi, I'm Navie!", ''];
+  const { projectDirectories } = configuration();
+
+  // Case 1: No open project directories
+  if (projectDirectories.length === 0) {
+    result.push(
+      "It looks like there's no project open in your code editor. To get started, try opening a project so I can help you more effectively.",
+      '',
+      'I can help you answer questions about your codebase, create diagrams, plan solutions, generate code and tests, and review code changes. Type `@` to see a list of commands.'
+    );
+    return result.join('\n');
+  }
+
+  // Case 2: Check for uncommitted changes or diffs on the current branch
+  const diffs = await getChangeDiffs(projectDirectories);
+  const hasChanges = diffs.some((diff) => diff.length > 0);
+
+  // Case 2a: No uncommitted changes or diff on this branch
+  if (!hasChanges) {
+    result.push(
+      "It looks like you haven't started working on a task yet, as there are no changes to your workspace or branch. To begin, you can check out a different branch or start editing files. I'll keep track of your progress, so you can return anytime for help with your task.",
+      '',
+      'In the meantime, I can help you answer questions about your codebase, create diagrams, plan solutions and generate code. Type `@` to see a list of commands.'
+    );
+    return result.join('\n');
+  }
+
+  // Case 2b: Changes in progress
+  const welcomeSuggestion = await getWelcomeSuggestion(navieProvider, diffs);
+  if (!welcomeSuggestion) {
+    // This shouldn't really ever happen, but it's possible if the LLM fails to generate a
+    // structured response.
+    result.push(
+      'I can help you answer questions about your codebase, create diagrams, plan solutions, generate code and tests, and review code changes.',
+      '',
+      'Type `@` to see a list of commands.'
+    );
+  } else {
+    result.push(
+      `It looks like you're ${welcomeSuggestion.activity}.`,
+      '',
+      'Here are some questions you might want to try:',
+      ...welcomeSuggestion.suggestions.map((suggestion) => `- ${suggestion}`)
+    );
+  }
+
+  return result.join('\n');
+}
+
+export function navieMetadataV1(
+  navieProvider: INavieProvider
+): RpcHandler<NavieRpc.V1.Metadata.Params, NavieRpc.V1.Metadata.Response> {
   return {
     name: NavieRpc.V1.Metadata.Method,
-    handler: () => ({
-      welcomeMessage:
-        "### Hi, I'm Navie!\n\nI can help you answer questions about your codebase, plan solutions, create diagrams, and generate code. Enter `@` to see a list of commands.",
+    handler: async () => ({
+      welcomeMessage: await getWelcomeMessage(navieProvider),
       inputPlaceholder: "Ask a question or enter '@' for commands",
       commands: [
         {
