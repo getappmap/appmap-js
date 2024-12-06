4d6861ae75067f6674fa15c52be395897e5e655a
Rafał Rzepecki <divided.mind@gmail.com>
2024-12-06 21:42:45 +0100

    fix: Don't allow location lookup to go outside of source directories

--- packages/cli/tests/unit/rpc/explain/collect-location-context.spec.ts
+++ packages/cli/tests/unit/rpc/explain/collect-location-context.spec.ts
@@ -28,28 +28,27 @@ describe('collectLocationContext', () => {
     const locations: Location[] = [
       { path: 'file1.js', snippet: (contents: string) => contents.slice(0, 10) },
       { path: '/src/file2.js', snippet: (contents: string) => contents.slice(0, 10) },
-      { path: '/other/file3.js', snippet: (contents: string) => contents.slice(0, 10) },
+      { path: '/other/file3.js', snippet: (contents: string) => contents.slice(0, 10) }, // This should be skipped
     ];
 
     const collect = async () => collectLocationContext(sourceDirectories, locations);
 
-    it('handles valid locations', async () => {
+    it('handles valid locations and skips those outside source directories', async () => {
       jest.spyOn(utils, 'exists').mockResolvedValue(true);
       jest.spyOn(utils, 'isFile').mockResolvedValue(true);
       jest.spyOn(fs, 'readFile').mockResolvedValue('file contents');
 
       const result = await collect();
-      expect(result.length).toBe(4);
+      expect(result.length).toBe(3);
       expect(result[0].content).toBe('file conte');
       expect(result[1].content).toBe('file conte');
       expect(result[2].content).toBe('file conte');
-      expect(result[3].content).toBe('file conte');
 
-      expect(utils.exists).toHaveBeenCalledTimes(4);
+      expect(utils.exists).toHaveBeenCalledTimes(3);
       expect(utils.exists).toHaveBeenCalledWith('/src/file1.js');
       expect(utils.exists).toHaveBeenCalledWith('/lib/file1.js');
       expect(utils.exists).toHaveBeenCalledWith('/src/file2.js');
-      expect(utils.exists).toHaveBeenCalledWith('/other/file3.js');
+      // note file in /other is skipped
     });
 
     it('handles non-file locations', async () => {
