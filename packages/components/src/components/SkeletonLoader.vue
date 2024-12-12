e90259c2a978c95ba73c6c1b8b8d4c73f83dc091
Dustin Byrne <dustinsbyrne@gmail.com>
2024-12-12 15:25:27 -0500

    Move SkeletonLoader into its own component

---
+++ packages/components/src/components/SkeletonLoader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="skeleton-loader" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'v-skeleton-loader',
+});
+</script>
+
+<style lang="scss" scoped>
+@keyframes skeleton {
+  0% {
+    background-position: -100% 0%;
+  }
+  100% {
+    background-position: 100% 0%;
+  }
+}
+.skeleton-loader {
+  $alpha: 0.075;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(black, 0.1);
+  background: linear-gradient(
+    90deg,
+    rgba(black, $alpha) 0%,
+    rgba(white, $alpha) 50%,
+    rgba(black, $alpha) 100%
+  );
+  background-size: 200% 100%;
+  animation: skeleton 3s linear infinite;
+}
+</style>
