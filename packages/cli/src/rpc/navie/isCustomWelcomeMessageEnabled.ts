10e227c337c0c6708276c46e26c6bf489effea3a
Dustin Byrne <dustinsbyrne@gmail.com>
2024-12-19 15:13:31 -0500

    fix: Default the custom welcome message to ON

--- packages/cli/src/rpc/navie/isCustomWelcomeMessageEnabled.ts
+++ packages/cli/src/rpc/navie/isCustomWelcomeMessageEnabled.ts
@@ -1,15 +1,5 @@
-import INavie from '../explain/navie/inavie';
-
-export default function isCustomWelcomeMessageEnabled(_navie: INavie): boolean {
-  const disabledByEnvironmentVariable = () => process.env.NAVIE_CUSTOM_WELCOME_MESSAGE === 'false';
-  const enabledByEnvironmentVariable = () => process.env.NAVIE_CUSTOM_WELCOME_MESSAGE === 'true';
-  // TODO: This can be used to enable an incremental rollout of the feature.
-  // For now, we will go with the environment variables only.
-  // const disabledByUseOfLocalNavie = () => navie instanceof LocalNavie;
-  if (disabledByEnvironmentVariable()) return false;
-
-  if (enabledByEnvironmentVariable()) return true;
-
-  // return !disabledByUseOfLocalNavie();
-  return false;
+export default function isCustomWelcomeMessageEnabled(): boolean {
+  // This behavior is DEFAULT ON, but can be disabled by setting the environment variable
+  // NAVIE_CUSTOM_WELCOME_MESSAGE=false. Any other value will be treated as true.
+  return process.env.NAVIE_CUSTOM_WELCOME_MESSAGE !== 'false';
 }
