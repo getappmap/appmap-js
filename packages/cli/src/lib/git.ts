52298a7f7b2b6c0340288ec625504df962e45da8
Kevin Gilpin <kgilpin@gmail.com>
2025-01-23 09:49:53 -0500

    refactor: Remove unused code
    Locating the git repository - can't find any references to this any more

--- packages/cli/src/lib/git.ts
+++ packages/cli/src/lib/git.ts
@@ -1,76 +1,8 @@
-import gitconfig from 'gitconfiglocal';
-import assert from 'node:assert';
-import { pathToFileURL } from 'node:url';
-import { promisify, types } from 'util';
 import { execute } from './executeCommand';
 import { warn } from 'node:console';
 import { verbose } from '../utils';
 
 const DEV_NULL = process.platform === 'win32' ? 'NUL' : '/dev/null';
-
-// Attempt to parse special syntax allowed by git:
-// - scp-style urls ([user@]host.xz:path/to/repo.git/),
-// - bare file paths.
-function parseSpecial(url: string): URL | undefined {
-  // From git-push(1):
-  // > This syntax is only recognized if there are no slashes before the first colon.
-  // > This helps differentiate a local path that contains a colon.
-  // Note {2,} is to avoid recognizing d:/windows style paths.
-  if (url.match(/^[^/]{2,}:/)) return new URL('ssh://' + url.replace(':', '/'));
-  else return pathToFileURL(url);
-}
-
-// Utility type. This is just so the type system can
-// verify we're not leaking unsanitized uris anywhere.
-class SanitizedUri {
-  uri: URL;
-
-  constructor(uri: string) {
-    try {
-      this.uri = new URL(uri);
-    } catch (err) {
-      assert(types.isNativeError(err));
-      if (err.name === 'TypeError') {
-        const special = parseSpecial(uri);
-        if (!special) throw err;
-        this.uri = special;
-      } else throw err;
-    }
-    this.uri.username = '';
-    this.uri.password = '';
-  }
-
-  toString() {
-    return this.uri.toString();
-  }
-}
-
-function hasUrl(remote: unknown): remote is { url: string } {
-  return remote?.['url'];
-}
-
-async function findAndSanitizeRepository(dir: string): Promise<SanitizedUri | void> {
-  try {
-    const config = await promisify(gitconfig)(dir);
-    const remote: unknown = config.remote;
-    if (!remote || typeof remote !== 'object') return;
-
-    const origin = remote['origin'];
-    if (hasUrl(origin)) return new SanitizedUri(origin.url);
-
-    const first = Object.values(remote).find(hasUrl);
-    if (!first) return;
-    return new SanitizedUri(first.url);
-  } catch (err) {
-    assert(types.isNativeError(err));
-    if (err.message.includes('no gitconfig')) return;
-    throw err;
-  }
-}
-
-export async function findRepository(dir: string): Promise<string | undefined> {
-  return (await findAndSanitizeRepository(dir))?.toString();
-}
 
 /**
  * Find the distance between the base commit and the head commit.
@@ -229,7 +161,12 @@ export async function getDiffLog(
   const base = baseCommit ?? (await findBaseCommit(headCommit, cwd));
   if (!base) return '';
 
-  return execute('git', ['--no-pager', 'log', '-p', '--full-diff', `${base}..${headCommit}`], {
-    cwd,
-  });
+  const result = await execute(
+    'git',
+    ['--no-pager', 'log', '-p', '--full-diff', `${base}..${headCommit}`],
+    {
+      cwd,
+    }
+  );
+  return result;
 }
