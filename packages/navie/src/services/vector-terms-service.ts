ebbc1d9e772a8e35fcb121d17be02d1ccbf31502
Dustin Byrne <dustinsbyrne@gmail.com>
2025-02-10 20:45:19 -0500

    fix: Reword 'question' to 'inquiry' in vector terms system prompt
    Some LLMs may fail to generate vector terms if the user did not form a
    proper question.

--- packages/navie/src/services/vector-terms-service.ts
+++ packages/navie/src/services/vector-terms-service.ts
@@ -11,18 +11,18 @@ const debug = makeDebug('navie:vector-terms');
 
 const SYSTEM_PROMPT = `You are assisting a developer to search a code base.
 
-The developer asks a question using natural language. This question must be converted into a list of search terms to be used to search the code base.
+The developer submits an inquiry using natural language. This inquiry must be converted into a list of search terms to be used to search the code base.
 
 **Procedure**
 
-1) Separate the user's question into "context" and "instructions". The "context" defines the background information that will match
+1) Separate the user's inquiry into "context" and "instructions". The "context" defines the background information that will match
   code in the code base. The "instructions" are the statement about what the user wants you to do or what they want to find out.
   Be brief in the "instructions" part.
-2) From here on, only consider the "context" part of the question.
+2) From here on, only consider the "context" part of the inquiry.
 3) Expand the list of "context" words to include synonyms and related terms.
 4) Optionally choose a small number of search terms which are MOST SELECTIVE. The MOST SELECTIVE match terms should
   be words that will match a feature or domain model object in the code base. They should be the most
-  distinctive words in the question. You will prefix the MOST SELECTIVE terms with a '+'.
+  distinctive words in the inquiry. You will prefix the MOST SELECTIVE terms with a '+'.
 5) In order for keywords to match optimally, compound words MUST additionally be segmented into individual search terms. This is additionally very important for compound words containing acronyms.
   E.g., "httpserver" would become "http", "server", "httpserver". "xmlserializer" -> "xml", "serializer", "xmlserializer". "jsonserializer" -> "json", "serializer", "jsonserializer". etc.
 
