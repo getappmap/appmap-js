85084c0e3f35f23a0700d4e9721c7386f7d3b206
Dustin Byrne <dustinsbyrne@gmail.com>
2025-04-07 16:07:28 -0400

    feat: Add backwards compatibility for unsupported thread log front ends

--- packages/cli/tests/unit/rpc/explain/LocalNavie.spec.ts
+++ packages/cli/tests/unit/rpc/explain/LocalNavie.spec.ts
@@ -1,11 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/require-await */
-import EventEmitter from 'events';
-import { navie as navieFn, Navie } from '@appland/navie';
-
 import LocalNavie from '../../../../src/rpc/explain/navie/navie-local';
+import LegacyHistory from '../../../../src/rpc/navie/legacy/history';
+import { container } from 'tsyringe';
+import ThreadService from '../../../../src/rpc/navie/services/threadService';
 
-jest.mock('@appland/navie');
+jest.mock('@appland/navie', () => ({
+  ...jest.requireActual('@appland/navie'),
+  navie: jest
+    .fn()
+    .mockReturnValue({
+      on: jest.fn().mockReturnThis(),
+      execute: jest.fn().mockImplementation(function* () {}),
+    }),
+}));
+jest.mock('@appland/client', () => ({
+  AI: {
+    createUserMessage: jest.fn().mockResolvedValue({ id: 'user-message' }),
+    createAgentMessage: jest.fn().mockResolvedValue({ id: 'assistant-message' }),
+  },
+}));
 
 describe('LocalNavie', () => {
   let navie: LocalNavie;
@@ -35,6 +47,44 @@ describe('LocalNavie', () => {
       expect(() => navie.setOption('unsupported', 'value')).toThrowError(
         "LocalNavie does not support option 'unsupported'"
       );
+    });
+  });
+
+  describe('backwards compatibility', () => {
+    let threadService: ThreadService;
+    let originalEnv: NodeJS.ProcessEnv;
+    let navie: LocalNavie;
+
+    beforeEach(() => {
+      navie = new LocalNavie(jest.fn(), jest.fn(), jest.fn());
+      originalEnv = { ...process.env };
+      process.env.APPMAP_TELEMETRY_DISABLED = '1';
+      LegacyHistory.initialize = jest.fn();
+      container.reset();
+      threadService = {
+        getThread: jest.fn().mockResolvedValue({ getChatHistory: jest.fn().mockReturnValue([]) }),
+      } as unknown as ThreadService;
+      container.registerInstance(ThreadService, threadService);
+    });
+
+    afterEach(() => {
+      process.env = originalEnv;
+    });
+
+    describe('ask', () => {
+      it('uses LegacyHistory if `APPMAP_NAVIE_THREAD_LOG` is unset', async () => {
+        delete process.env.APPMAP_NAVIE_THREAD_LOG;
+        await navie.ask('threadId', 'question');
+        expect(LegacyHistory.initialize).toHaveBeenCalled();
+        expect(threadService.getThread).not.toHaveBeenCalled();
+      });
+
+      it('uses thread logs if `APPMAP_NAVIE_THREAD_LOG` is set', async () => {
+        process.env.APPMAP_NAVIE_THREAD_LOG = '1';
+        await navie.ask('threadId', 'question');
+        expect(LegacyHistory.initialize).not.toHaveBeenCalled();
+        expect(threadService.getThread).toHaveBeenCalled();
+      });
     });
   });
 });
