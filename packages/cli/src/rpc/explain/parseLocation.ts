871139a659115725ffb06276f8f2941456b9ed25
Rafał Rzepecki <divided.mind@gmail.com>
2025-05-16 15:32:06 +0200

    fix: Update parseLocation to handle negative line numbers correctly
    It seems that sometimes appmaps contain -1 as a placeholder for unknown line number. Handle this case gracefully.
    
    Fixes #2304

--- packages/cli/src/rpc/explain/parseLocation.ts
+++ packages/cli/src/rpc/explain/parseLocation.ts
@@ -4,13 +4,14 @@ import { warn } from 'console';
 export default function parseLocation(location: string): [string, number | undefined] | undefined {
   if (!location.includes(':')) return [location, undefined];
 
-  const locationTest = /([^:]+):(\d+)$/.exec(location);
+  const locationTest = /([^:]+):(-?\d+)$/.exec(location);
   if (!locationTest) {
     warn(chalk.gray(`Invalid location format: ${location}. Skipping file lookup.`));
     return;
   }
 
   const [requestedFileName, lineNoStr] = locationTest.slice(1);
-  const lineNoReturned = lineNoStr ? parseInt(lineNoStr, 10) : undefined;
+  const lineNoReturned =
+    lineNoStr && !lineNoStr.startsWith('-') ? parseInt(lineNoStr, 10) : undefined;
   return [requestedFileName, lineNoReturned];
 }
