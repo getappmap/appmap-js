02e667c7300502b1271948a3d8d7abb56b9138be
Rafał Rzepecki <divided.mind@gmail.com>
2025-06-20 16:22:01 +0200

    Add gradual loading for suggestions in Review component

--- packages/components/src/stories/Review.stories.js
+++ packages/components/src/stories/Review.stories.js
@@ -18,3 +18,41 @@ export const Review = (args, { argTypes }) => ({
   components: { VReview },
   template: '<v-review v-bind="$props" ref="vsCode" />',
 });
+
+const chunkSize = 3;
+const delay = 1000;
+export function ReviewGradual(args, { argTypes }) {
+  return {
+    components: { VReview },
+    data() {
+      return {
+        features: [],
+        suggestions: [],
+      };
+    },
+    mounted() {
+      setTimeout(() => {
+        this.features = features;
+        this.nextChunk();
+      }, delay);
+    },
+    methods: {
+      nextChunk() {
+        if (this.suggestions.length < suggestions.length) {
+          this.suggestions = suggestions.slice(0, this.suggestions.length + chunkSize);
+          setTimeout(() => {
+            this.nextChunk();
+          }, delay);
+        }
+      },
+    },
+    computed: {
+      loading() {
+        return (
+          this.suggestions.length < suggestions.length || this.features.length < features.length
+        );
+      },
+    },
+    template: '<v-review v-bind="$data" :loading="loading" />',
+  };
+}
