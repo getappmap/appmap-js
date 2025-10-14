9f697765496c890c8f3cd1f50e58d30ca4adb642
Rafał Rzepecki <divided.mind@gmail.com>
2025-10-14 15:33:07 +0200

    fix: Don't crash on licence check errors
    When checking licence, if the licence check is non-mandatory, don't
    throw error (and crash) when there's a problem contacting the server.

--- packages/client/src/licenseKey.ts
+++ packages/client/src/licenseKey.ts
@@ -9,11 +9,12 @@ import verbose from './verbose';
 export default {
   async check(licenseKey: string, retryOptions: RetryOptions = {}): Promise<boolean> {
     const commandDescription = `Checking if API key is valid`;
+    const requestPath = ['api', 'api_keys', 'check'].join('/');
+    const request = buildRequest(requestPath, { requireApiKey: false });
+    request.headers.authorization = `Bearer ${licenseKey}`;
+
     const makeRequest = async (): Promise<IncomingMessage> => {
       const retrier = retry(commandDescription, retryOptions, makeRequest);
-      const requestPath = ['api', 'api_keys', 'check'].join('/');
-      const request = buildRequest(requestPath, { requireApiKey: false });
-      request.headers.authorization = `Bearer ${licenseKey}`;
       return new Promise<IncomingMessage>((resolve, reject) => {
         const interaction = request.requestFunction(
           request.url,
@@ -37,7 +38,9 @@ export default {
       response.on('end', () => {});
 
       if (!response.statusCode) {
-        throw new Error('No status code was provided by the server');
+        throw new Error(
+          `License key check failed for ${request.url.toString()}: No status code was provided by the server`
+        );
       }
       if (response.statusCode === 404) {
         return false;
@@ -45,7 +48,9 @@ export default {
       if (response.statusCode < 300) {
         return true;
       }
-      throw new Error(`Unexpected status code: ${response.statusCode}`);
+      throw new Error(
+        `License key check failed: Unexpected status code ${response.statusCode} from ${request.url.toString()}`
+      );
     });
   },
 };
